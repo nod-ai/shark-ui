af13b8c9495c14c882148927a5d208e4364f95de
Jacob Gordon <jacob.gordon@amd.com>
2025-03-17 19:20:36 +0000

    preliminary: adds `Ref` to vue reactivity facade

--- src/library/vue/reactivity.ts
+++ src/library/vue/reactivity.ts
@@ -1,11 +1,19 @@
 import {
   ref as _ref,
   unref,
-  type Ref,
+  type Ref as _Ref,
 } from 'vue';
 
 /** Wraps an instance to make it reactive */
 export const ref = _ref;
+
+export type Ref<
+  GetterResult,
+  SetterParameter = GetterResult,
+> = _Ref<
+  GetterResult,
+  SetterParameter
+>;
 
 /** Unwraps a reactive instance */
 export const get = unref;
