8a18003f3da148b687c74ae2691de57d11354b5d
Jacob Gordon <jacob.gordon@amd.com>
2025-03-18 20:54:44 +0000

    types: avoids naming imports as "StabilityAIClient"
    - avoids potential collision with `StabilityAIClient` class

--- src/models/ImageGenerationPromptWeight.ts
+++ src/models/ImageGenerationPromptWeight.ts
@@ -1,11 +1,13 @@
-import * as StabilityAIClient from 'stabilityai-client-typescript/models/components';
+import type {
+  TextPrompt,
+} from 'stabilityai-client-typescript/models/components';
 
 enum ImageGenerationPromptWeight {
   positive,
   negative,
 }
 
-export const quantitative = (givenQualitativeWeight: ImageGenerationPromptWeight): StabilityAIClient.TextPrompt['weight'] => {
+export const quantitative = (givenQualitativeWeight: ImageGenerationPromptWeight): TextPrompt['weight'] => {
   switch (givenQualitativeWeight) {
     case ImageGenerationPromptWeight.positive: return +1;
     case ImageGenerationPromptWeight.negative: return -1;
