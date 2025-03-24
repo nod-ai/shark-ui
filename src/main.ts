4e29337c5b48c762f12d6ec8532775fe8b154731
Jacob Gordon <jacob.gordon@amd.com>
2025-03-24 15:37:00 +0000

    feature: points global error handler to GitHub page for new issues

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,25 @@ const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
+
+window.onunhandledrejection = (someEvent) => {
+  const windowShouldOpenNewGitHubIssue = window.confirm([
+    'Unexpected Error:',
+    `"${String(someEvent.reason)}"`,
+    '',
+    'Proceed to file an issue?',
+  ].join('\n'));
+
+  if (windowShouldOpenNewGitHubIssue) {
+    const quotedErrorMessage = `Error Message:\n${String(someEvent.reason)}`.replace('\n', '\n> ');
+    const newGitHubIssue = new URL('https://github.com/nod-ai/shark-ui/issues/new');
+    const referencedParameters = newGitHubIssue.searchParams;
+    referencedParameters.set('title', `[Unexpected Error]: can't <some task> when <some context>`);
+    referencedParameters.set('body', quotedErrorMessage);
+    referencedParameters.set('labels', ['bug'].join());
+    referencedParameters.set('type', 'Bug');
+    window.open(newGitHubIssue);
+  };
+
+  someEvent.preventDefault();
+};
