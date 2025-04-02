b1b01eae4c67edc4bb946d70f00f987e4a30f504
Jacob Gordon <jacob.gordon@amd.com>
2025-04-02 21:24:03 +0000

    moves vuetify.ts to plugins/

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import {
 } from 'vue';
 
 import App from './App.vue';
+import vuetify from './plugins/vuetify.ts';
 import router from './router';
-import vuetify from './vuetify.ts';
 
 const app = createApp(App);
 app.use(router);
