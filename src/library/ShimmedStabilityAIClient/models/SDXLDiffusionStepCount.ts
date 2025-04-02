bb453d6777d2807db368e23bc886ba3f254004c4
Jacob Gordon <jacob.gordon@amd.com>
2025-04-02 16:57:52 +0000

    preliminary: adds `SDXLDiffusionStepCount` model

---
+++ src/library/ShimmedStabilityAIClient/models/SDXLDiffusionStepCount.ts
@@ -0,0 +1,14 @@
+import DiscreteRange from '@/library/Range/DiscreteRange.ts';
+
+/**
+ * Source from the [StabilityAI OpenAPI spec](https://github.com/nod-ai/StabilityAI-client-typescript/blob/HEAD/openapi.json)
+ *
+ * Defined at "components.schemas.Steps"
+ */
+export default abstract class SDXLDiffusionStepCount { // eslint-disable-line @typescript-eslint/no-extraneous-class
+  public static range = DiscreteRange.spanning({
+    from: 10,
+    to  : 50,
+    by  : 1,
+  });
+};
