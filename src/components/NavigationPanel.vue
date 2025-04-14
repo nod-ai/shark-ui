2fbbcaf2d1019b297702563d901e4d95f68f2828
Jacob Gordon <jacob.gordon@amd.com>
2025-04-14 16:25:34 +0000

    preliminary: adds `NavigationPanel` component

---
+++ src/components/NavigationPanel.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import {
+  reactive,
+  type Reactive,
+} from 'vue';
+
+import {
+  VNavigationDrawer,
+} from 'vuetify/components/VNavigationDrawer';
+import {
+  useDisplay,
+} from 'vuetify/framework';
+
+const currentDisplay = reactive(useDisplay());
+
+type Display = Reactive<ReturnType<typeof useDisplay>>;
+
+const PanelWidth = {
+  default: 256,
+  scaleFor(
+    given: Display,
+  ) {
+    const {
+      md,
+      lg,
+      xl,
+    } = given.thresholds;
+
+    /**/ if (given.width <= md) return 1.00;
+    else if (given.width <= lg) return 1.50;
+    else if (given.width <= xl) return 2.00;
+
+    return 2.50;
+  },
+  for(
+    givenDisplay: Display,
+  ) {
+    return this.scaleFor(givenDisplay) * this.default;
+  },
+};
+
+</script>
+
+<template>
+  <VNavigationDrawer
+    permanent
+    class="pa-4"
+    v-bind="currentDisplay.smAndUp
+      ? {
+        width: PanelWidth.for(currentDisplay),
+      }
+      : {
+        location: 'top',
+      }
+    "
+  >
+    <slot />
+  </VNavigationDrawer>
+</template>
