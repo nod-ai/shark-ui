2ee0da03435b438ed7478b384f27bac254cdd2e1
Jacob Gordon <jacob.gordon@amd.com>
2025-04-23 15:43:11 -0500

    preliminary: adds `TextToImageOutput` type

---
+++ src/features/TextToImage/types/Output.ts
@@ -0,0 +1,9 @@
+import type ImageURI from '@/library/customTypes/UniformResourceIdentifier/Data/Image';
+
+/** The resulting information after a text-to-image model has ingested some input with some configuration */
+export interface TextToImageOutput {
+  image: {
+    uri: ImageURI;
+    description: string;
+  };
+}
--- src/features/TextToImage/types/index.ts
+++ src/features/TextToImage/types/index.ts
@@ -1,3 +1,6 @@
 export type {
   TextToImageInput as Input,
 } from './Input.ts';
+export type {
+  TextToImageOutput as Output,
+} from './Output.ts';
