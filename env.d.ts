e70c05cd9093a778d468b1e62b7025f0745a3c85
Jacob Gordon <jacob.gordon@amd.com>
2025-04-25 10:08:19 -0500

    build: makes namespaces in environment variable easier to read

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TEXT_TO_IMAGE_API_SERVER_ORIGIN: string;
+  readonly VITE__TEXT_TO_IMAGE__API__SERVER__ORIGIN: string;
 }
 
 interface ImportMeta {
