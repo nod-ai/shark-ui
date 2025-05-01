0628c7be2cc05322b07c99a0be23e34b2c7d4143
Jacob Gordon <jacob.gordon@amd.com>
2025-05-01 11:39:57 -0500

    fix: catches errors that originate from Vue components during post-build execution (#175)

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import {
   promptUserToReport,
 } from './features/reporting.ts';
 
+import {
+  asError,
+} from './library/utilitiesByType/error.ts';
+
 import vuetify from './plugins/vuetify.ts';
 import router from './router';
 
@@ -20,4 +24,10 @@ window.onunhandledrejection = (someEvent) => {
   const rejectionReason = String(someEvent.reason);
   promptUserToReport(rejectionReason);
   someEvent.preventDefault();
+};
+
+// In production version (post build/bundling), errors that originate from Vue components bypass the listeners on the current `Window` instance
+app.config.errorHandler = (someException) => {
+  const someError = asError(someException);
+  promptUserToReport(someError.message);
 };
