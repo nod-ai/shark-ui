7e58e655a7d3285dca159ed4291f547054481a74
Jacob Gordon <jacob.gordon@amd.com>
2025-05-14 08:36:51 -0500

    preliminary: defines sanctioned way to ignore actionable errors

--- src/library/Attempt/error/ActionableError.ts
+++ src/library/Attempt/error/ActionableError.ts
@@ -2,11 +2,24 @@ import type {
   Branded,
 } from '@/library/typeUtilities/Branded';
 
+import {
+  NonActionableError,
+} from '.';
+
 /** Extend this class to describe errors from which callers ought to recover */
 abstract class ActionableError<SomeBrand extends string>
   extends Error
   implements Branded<SomeBrand> {
   public readonly brand!: SomeBrand;
+
+  public throwAnyway(): never {
+    throw new NonActionableError(
+      'Implementation not specified for actionable error',
+      {
+        cause: this,
+      },
+    );
+  }
 }
 
 export default ActionableError;
