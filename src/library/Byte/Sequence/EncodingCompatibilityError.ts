nod-ai/shark-ui

commit 0fe7437322574a15101ae38af09b8fe74b472ced

fix: emphasizes overlooked "actionable" errors (#220)
Follow up to #217

src/library/Byte/Sequence/EncodingCompatibilityError.ts
@@ -1,0 +1,11 @@
+import * as Byte from '@/library/Byte';
+
+class Byte_Sequence_EncodingCompatibilityError extends Error {
+  public constructor(givenBitWidth: number) {
+    const byteCofactor = Byte.cofactorTo(givenBitWidth);
+    super(`Character count for ${givenBitWidth.toString()}-bit sequence must be a multiple of ${byteCofactor.toString()} to be byte encodable`);
+    this.name = 'Byte_Sequence_CompatibilityError';
+  }
+}
+
+export default Byte_Sequence_EncodingCompatibilityError;

src/features/TextToImage/config/utilities/DynamicConfig/EndpointResponseError.ts
@@ -1,0 +1,15 @@
+import type {
+  URLPath,
+} from '@/library/customTypes/URLComponent';
+
+class DynamicConfig_EndpointResponseError extends Error {
+  public constructor(given: {
+    endpoint: URLPath;
+    response: Response;
+  }) {
+    super(`Expected JSON response from "${given.endpoint.toString()}", but received content-type: ${given.response.headers.get('Content-Type') ?? ''}`);
+    this.name = 'DynamicConfig_EndpointResponseError';
+  }
+}
+
+export default DynamicConfig_EndpointResponseError;

src/features/TextToImage/config/utilities/DynamicConfig/FetchingError.ts
@@ -2,11 +2,11 @@ import type {
   URLPath,
 } from '@/library/customTypes/URLComponent';
 
-class DynamicConfigFetchingError extends Error {
+class DynamicConfig_FetchingError extends Error {
   public constructor(givenEndpoint: URLPath) {
     super(`Failed to fetch text-to-image config from endpoint: ${givenEndpoint.toString()}`);
-    this.name = 'DynamicConfigFetchError';
+    this.name = 'DynamicConfig_FetchingError';
   }
 }
 
-export default DynamicConfigFetchingError;
+export default DynamicConfig_FetchingError;

src/features/TextToImage/config/utilities/DynamicConfig/index.ts
@@ -7,7 +7,8 @@ import {
   ConfigSchema,
 } from '../../types';
 
-import DynamicConfigFetchingError from './DynamicConfigFetchError';
+import DynamicConfig_EndpointResponseError from './EndpointResponseError';
+import DynamicConfig_FetchingError from './FetchingError';
 
 const contentIsJSONIn = (givenResponse: Response): boolean => {
   const contentType = givenResponse.headers.get('Content-Type');
@@ -24,11 +25,14 @@ const forciblyFetchConfig = async (): Promise<Config> => {
 
   if (
     !endpointResponse.ok
-  ) throw new DynamicConfigFetchingError(configEndpoint);
+  ) throw new DynamicConfig_FetchingError(configEndpoint);
 
   if (
     !contentIsJSONIn(endpointResponse)
-  ) throw new Error('Dynamic config: expected JSON response but received non-JSON content.');
+  ) throw new DynamicConfig_EndpointResponseError({
+    endpoint: configEndpoint,
+    response: endpointResponse,
+  });
 
   const unparsedSchema = await endpointResponse.json() as unknown;
   return ConfigSchema.parse(unparsedSchema);
@@ -37,5 +41,6 @@ const forciblyFetchConfig = async (): Promise<Config> => {
 export {
   configEndpoint as endpoint,
   forciblyFetchConfig as forciblyFetch,
-  DynamicConfigFetchingError as FetchingError,
+  DynamicConfig_FetchingError as FetchingError,
+  DynamicConfig_EndpointResponseError as EndpointResponseError,
 };

src/library/Base64/Alphabet.ts
@@ -1,0 +1,7 @@
+/** See [RFC 4648 Section 4](https://www.rfc-editor.org/rfc/rfc4648.html#section-4) for more information */
+const Base64_Alphabet = {
+  length : 64,
+  pattern: /A-Za-z\d\+\//,
+};
+
+export default Base64_Alphabet;

src/library/Base64/CharacterSequence/ConformanceError.ts
@@ -1,0 +1,10 @@
+import Base64_Alphabet from '../Alphabet';
+
+class Base64_CharacterSequence_ConformanceError extends Error {
+  public constructor() {
+    super(`Sequence contained 1+ character(s) outside of the Base64 Alphabet: ${Base64_Alphabet.pattern.toString()}`);
+    this.name = 'Base64_CharacterSequence_ConformanceError';
+  }
+}
+
+export default Base64_CharacterSequence_ConformanceError;

src/library/Base64/CharacterSequence/index.ts
@@ -1,0 +1,18 @@
+import Base64_Alphabet from '../Alphabet';
+
+import Base64_CharacterSequence_ConformanceError from './ConformanceError';
+
+const CharacterSequence_pattern = new RegExp(`^[${Base64_Alphabet.pattern.source}]+$`);
+
+const assertConformanceOf = (givenCharacterSequence: string): string => {
+  if (
+    !CharacterSequence_pattern.test(givenCharacterSequence)
+  ) throw new Base64_CharacterSequence_ConformanceError();
+
+  return givenCharacterSequence;
+};
+
+export {
+  assertConformanceOf,
+  Base64_CharacterSequence_ConformanceError as ConformanceError,
+};

src/library/Base64/index.ts
@@ -1,0 +1,10 @@
+import Base64_Alphabet from './Alphabet';
+import * as Base64_CharacterSequence from './CharacterSequence';
+
+const Base64_bitWidth = Math.log2(Base64_Alphabet.length);
+
+export {
+  Base64_Alphabet as Alphabet,
+  Base64_bitWidth as bitWidth,
+  Base64_CharacterSequence as CharacterSequence,
+};

commit 1f93bca46241e977c6793f436f9543880b0be340

fix(utility): avoids reference race in `Byte` (#221)
Introduced in #220

src/library/Byte/Sequence/EncodingCompatibilityError.ts
@@ -1,8 +1,10 @@
-import * as Byte from '@/library/Byte';
+import {
+  cofactorTo as Byte_cofactorTo,
+} from '..';
 
 class Byte_Sequence_EncodingCompatibilityError extends Error {
   public constructor(givenBitWidth: number) {
-    const byteCofactor = Byte.cofactorTo(givenBitWidth);
+    const byteCofactor = Byte_cofactorTo(givenBitWidth);
     super(`Character count for ${givenBitWidth.toString()}-bit sequence must be a multiple of ${byteCofactor.toString()} to be byte encodable`);
     this.name = 'Byte_Sequence_CompatibilityError';
   }
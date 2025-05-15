1f93bca46241e977c6793f436f9543880b0be340
Jacob Gordon <jacob.gordon@amd.com>
2025-05-15 11:31:46 -0500

    fix(utility): avoids reference race in `Byte` (#221)
    Introduced in #220

--- src/library/Byte/Sequence/EncodingCompatibilityError.ts
+++ src/library/Byte/Sequence/EncodingCompatibilityError.ts
@@ -1,8 +1,10 @@
-import * as Byte from '@/library/Byte';
+import {
+  cofactorTo as Byte_cofactorTo,
+} from '..';
 
 class Byte_Sequence_EncodingCompatibilityError extends Error {
   public constructor(givenBitWidth: number) {
-    const byteCofactor = Byte.cofactorTo(givenBitWidth);
+    const byteCofactor = Byte_cofactorTo(givenBitWidth);
     super(`Character count for ${givenBitWidth.toString()}-bit sequence must be a multiple of ${byteCofactor.toString()} to be byte encodable`);
     this.name = 'Byte_Sequence_CompatibilityError';
   }
