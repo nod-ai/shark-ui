c4b388befefad3b2e00a4cc8cc826bc79c3eed9d
Jacob Gordon <jacob.gordon@amd.com>
2025-05-22 13:53:41 -0500

    prefeature(library/Parser): defines `StringParser` utility with type-safe errors

---
+++ src/library/Parser/string/StaticStringParser.ts
@@ -0,0 +1,22 @@
+import type Static from '@/library/typeUtilities/Static';
+
+import type ParsingError from '../ParsingError';
+
+import type {
+  StringParser,
+} from './StringParser';
+
+type StaticStringParser<
+  Any,
+> =
+  & Static<
+    Any
+  >
+  & StringParser<
+    Any,
+    ParsingError<string>
+  >;
+
+export type {
+  StaticStringParser,
+};
