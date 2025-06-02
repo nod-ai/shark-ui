295f32a425c27f3fdb0451f160b958a2f3525c94
Jacob Gordon <jacob.gordon@amd.com>
2025-06-02 15:27:35 -0500

    refactor(text-to-image): extracts `TextToImageOutputImg` component from `TextToImagePage`

---
+++ src/features/TextToImage/components/TextToImageOutputImg.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {
+  VImg,
+} from 'vuetify/components/VImg';
+
+import * as TextToImage from '@/features/TextToImage';
+
+defineProps<{
+  modelValue: TextToImage.Output['image'];
+}>();
+</script>
+
+<template>
+  <VImg
+    :src="modelValue.uri.serialized"
+    :alt="modelValue.description"
+  />
+</template>
--- src/pages/TextToImagePage.vue
+++ src/pages/TextToImagePage.vue
@@ -18,9 +18,6 @@ import {
   VContainer,
 } from 'vuetify/components/VGrid';
 import {
-  VImg,
-} from 'vuetify/components/VImg';
-import {
   VMain,
 } from 'vuetify/components/VMain';
 import {
@@ -35,6 +32,7 @@ import DiscreteSlider from '@/components/DiscreteSlider.vue';
 import NavigationPanel from '@/components/NavigationPanel.vue';
 
 import TextToImageInputSection from '@/features/TextToImage/components/TextToImageInputSection.vue';
+import TextToImageOutputImg from '@/features/TextToImage/components/TextToImageOutputImg.vue';
 import * as TextToImage from '@/features/TextToImage/index.ts';
 
 const currentPrompt: Ref<TextToImage.Input['text'] | null> = ref(null);
@@ -118,10 +116,9 @@ const imageGeneration = useStatefulProcess(async () => {
       max-width="100vh"
       class="fill-height"
     >
-      <VImg
+      <TextToImageOutputImg
         v-if="imageGeneration.result !== null"
-        :src="imageGeneration.result.uri.serialized"
-        :alt="imageGeneration.result.description"
+        :model-value="imageGeneration.result"
       />
       <VSkeletonLoader
         v-else
