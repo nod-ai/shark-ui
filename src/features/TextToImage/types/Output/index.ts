a991304c06af7a528e2a69fe0381626e196e6c1f
Jacob Gordon <jacob.gordon@amd.com>
2025-06-02 15:22:46 -0500

    types(text-to-image): extracts `TextToImage_Output_Image` from `TextToImage_Output`
    - allows intellisense to display type name rather just its structure upon hover

--- src/features/TextToImage/types/Output/index.ts
+++ src/features/TextToImage/types/Output/index.ts
@@ -1,9 +1,6 @@
-import type ImageURI from '@/library/customTypes/UniformResourceIdentifier/Data/Image';
+import type TextToImage_Output_Image from './Image';
 
 /** The resulting information after a text-to-image model has ingested some input with some configuration */
 export interface TextToImage_Output {
-  image: {
-    uri: ImageURI;
-    description: string;
-  };
+  image: TextToImage_Output_Image;
 }
