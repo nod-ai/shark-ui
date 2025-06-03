334513e2fbbbb3421d4b27ceed5c644ca0cd7b99
Jacob Gordon <jacob.gordon@amd.com>
2025-06-03 13:40:43 -0500

    prefeature(library/vue): makes it more obvious that error UI is missing (#298)
    - replaces `useStatefulProcess` composable with error-safe version
    - the benefit: template-level "force unwrap" that just _begs_ for an error UI

---
+++ src/library/vue/statefulAttempt.ts
@@ -0,0 +1,60 @@
+import {
+  get,
+  ref,
+  set,
+  type Ref,
+} from '@/library/vue/reactivity.ts';
+
+import Attempt from '@/library/Attempt';
+
+interface StatefulAttempt<
+  SomeProduct,
+  SomeActionableError extends Attempt.ActionableError<string>,
+> {
+  initiate: () => Promise<void>;
+  isInProgress: boolean;
+  outcome: Attempt.Outcome<SomeProduct, SomeActionableError> | null;
+}
+
+/** Useful when state of UI is dependent on some async operation and the outcome upon completion */
+export const useStatefulAttemptThatEventually = <
+  SomeProduct,
+  SomeActionableError extends Attempt.ActionableError<string>,
+>(
+  retrieveOutcome: Attempt.EndRetriever<
+    Attempt.Outcome<SomeProduct, SomeActionableError>
+  >,
+): StatefulAttempt<SomeProduct, SomeActionableError> => {
+  const flagIsRaised = ref(false);
+
+  type CapturedOutcome = Attempt.Outcome<SomeProduct, SomeActionableError>;
+  const capturedOutcome: Ref<CapturedOutcome | null> = ref(null);
+
+  const captureOutcome = async (): Promise<void> => {
+    using cleanup = new DisposableStack();
+
+    set(capturedOutcome, null);
+    set(flagIsRaised, true);
+
+    cleanup.defer(() => {
+      set(flagIsRaised, false);
+    });
+
+    const retrievedOutcome = await Attempt.thatEventually(retrieveOutcome);
+    set(capturedOutcome, retrievedOutcome);
+  };
+
+  return {
+    initiate: captureOutcome,
+    get isInProgress() {
+      return get(flagIsRaised);
+    },
+    get outcome() {
+      if (
+        this.isInProgress
+      ) return null;
+
+      return get(capturedOutcome);
+    },
+  };
+};
--- src/pages/TextToImagePage.vue
+++ src/pages/TextToImagePage.vue
@@ -5,8 +5,8 @@ import {
   type Ref,
 } from '@/library/vue/reactivity.ts';
 import {
-  useStatefulProcess,
-} from '@/library/vue/statefulProcess.ts';
+  useStatefulAttemptThatEventually,
+} from '@/library/vue/statefulAttempt';
 
 import {
   VBtn,
@@ -23,8 +23,6 @@ import {
 import {
   VSkeletonLoader,
 } from 'vuetify/components/VSkeletonLoader';
-
-import Attempt from '@/library/Attempt';
 
 import SDXLDiffusionStepCount from '@/library/ShimmedStabilityAIClient/models/SDXLDiffusionStepCount.ts';
 
@@ -43,12 +41,12 @@ const {
 
 const currentNumberOfDiffusionSteps = ref<number>(range.midpoint);
 
-const imageGeneration = useStatefulProcess(async () => {
+const imageGeneration = useStatefulAttemptThatEventually(async (ends) => {
   const proposedPrompt = get(currentPrompt);
 
   if (
     proposedPrompt === null
-  ) return Attempt.abandon('Prompt was not set before submission');
+  ) return ends.inFlamesBecause('Prompt was not set before submission');
 
   const outcomeOfGeneratingOutput = await TextToImage.Client.SDXL.generateOutputFrom({
     textToImageRequestBody: {
@@ -62,7 +60,7 @@ const imageGeneration = useStatefulProcess(async () => {
   });
 
   const generatedOutput = outcomeOfGeneratingOutput.forciblyUnwrap();
-  return generatedOutput.image;
+  return ends.inSuccessWith(generatedOutput.image);
 });
 </script>
 
@@ -117,7 +115,7 @@ const imageGeneration = useStatefulProcess(async () => {
       class="fill-height"
     >
       <VSkeletonLoader
-        v-if="imageGeneration.result === null"
+        v-if="imageGeneration.outcome === null"
         :boilerplate="!imageGeneration.isInProgress"
         width="100vh"
         :style="{
@@ -126,7 +124,7 @@ const imageGeneration = useStatefulProcess(async () => {
       />
       <TextToImageOutputImg
         v-else
-        :model-value="imageGeneration.result"
+        :model-value="imageGeneration.outcome.forciblyUnwrap()"
       />
     </VContainer>
   </VMain>
