974f0bd0eb99f6b155421e82d5e79c8c5bdcd9ce
Jacob Gordon <jacob.gordon@amd.com>
2025-06-05 10:25:03 -0500

    refactor(utilities/Repository): calls out labels concern in `draftIssueFor`

--- src/utilities/Repository.ts
+++ src/utilities/Repository.ts
@@ -1,5 +1,11 @@
+type Repository_Issue_Label =
+  | 'bug'
+  | 'enhancement'
+  | 'documentation';
+
 type Repository_Issue_title_ = string;
 type Repository_Issue_body_ = string;
+type Repository_Issue_labels_ = Repository_Issue_Label[];
 
 const Repository = {
   get emptyDraftOfNewIssue(): URL {
@@ -14,10 +20,11 @@ const Repository = {
 
     const errorIssue_title: Repository_Issue_title_ = `[Unexpected Error]: can't <some task> when <some context>`;
     const errorIssue_body: Repository_Issue_body_ = `Error Message:\n${givenErrorMessage}`.replace('\n', '\n> ');
+    const errorIssue_labels: Repository_Issue_labels_ = ['bug'];
 
     referencedParameters.set('title', errorIssue_title);
     referencedParameters.set('body', errorIssue_body);
-    referencedParameters.set('labels', ['bug'].join());
+    referencedParameters.set('labels', errorIssue_labels.join());
     referencedParameters.set('type', 'Bug');
 
     return mutableDraft;
