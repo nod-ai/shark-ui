5c84924b72f4a7f8701dab58f538a9366dd8aa59
Jacob Gordon <jacob.gordon@amd.com>
2025-06-05 11:02:28 -0500

    refactor(utilities/Repository): makes it more obvious that error dialog dictates repo issue (#314)
    See commits in PR for refactor ops

--- src/utilities/Repository.ts
+++ src/utilities/Repository.ts
@@ -1,19 +1,35 @@
+type Repository_Issue_Label =
+  | 'bug'
+  | 'enhancement'
+  | 'documentation';
+
+type Repository_Issue_Type =
+  | 'Feature'
+  | 'Bug'
+  | 'Task';
+
+interface Repository_Issue {
+  title: string;
+  body: string;
+  labels: Repository_Issue_Label[];
+  type: Repository_Issue_Type;
+}
+
 const Repository = {
   get emptyDraftOfNewIssue(): URL {
     return new URL('https://github.com/nod-ai/shark-ui/issues/new');
   },
   /** Creates a URL that drafts a new issue with pre-populated fields  */
-  draftIssueFor(givenErrorMessage: string): URL {
+  draftIssue(
+    given: Repository_Issue,
+  ): URL {
     const mutableDraft = this.emptyDraftOfNewIssue;
-
     const referencedParameters = mutableDraft.searchParams;
 
-    referencedParameters.set('title', `[Unexpected Error]: can't <some task> when <some context>`);
-    const quotedErrorMessage = `Error Message:\n${givenErrorMessage}`.replace('\n', '\n> ');
-
-    referencedParameters.set('body', quotedErrorMessage);
-    referencedParameters.set('labels', ['bug'].join());
-    referencedParameters.set('type', 'Bug');
+    referencedParameters.set('title', given.title);
+    referencedParameters.set('body', given.body);
+    referencedParameters.set('labels', given.labels.join());
+    referencedParameters.set('type', given.type);
 
     return mutableDraft;
   },
