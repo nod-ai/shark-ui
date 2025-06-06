b1850adf2953a89fd9f75c1172e65d1b89e09915
Jacob Gordon <jacob.gordon@amd.com>
2025-06-06 17:02:08 +0000

    refactor(text-to-image): throws within template of TextToImageServerSpecificationAlert

--- src/features/TextToImage/components/TextToImageServerSpecificationAlert.vue
+++ src/features/TextToImage/components/TextToImageServerSpecificationAlert.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import * as TextToImage from '@/features/TextToImage';
 
-const given = defineProps<{
+defineProps<{
   error: TextToImage.Server.SpecificationError;
 }>();
-
-(() => {
-  return given.error.throwAnyway([
-    'No text-to-image server was specified!',
-    'Either:',
-    `a) supply it's corresponding environment variable named \`${given.error.environmentKey}\` and rebuild`,
-    `b) specify it within ${given.error.file.toString()}`,
-    'OR',
-    `c) specify it within the response from ${given.error.endpoint.toString()}`,
-  ].join('\n'));
-})();
 </script>
+
+<template>
+  {{
+    error.throwAnyway([
+      'No text-to-image server was specified!',
+      'Either:',
+      `a) supply it's corresponding environment variable named \`${error.environmentKey}\` and rebuild`,
+      `b) specify it within ${error.file.toString()}`,
+      'OR',
+      `c) specify it within the response from ${error.endpoint.toString()}`,
+    ].join('\n'))
+  }}
+</template>
