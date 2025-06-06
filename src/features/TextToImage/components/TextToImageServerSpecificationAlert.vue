362e7c330d386c0bfaca1f974c1856ffbcd29d34
Jacob Gordon <jacob.gordon@amd.com>
2025-06-06 17:00:23 +0000

    refactor(text-to-image): inlines `userFacingMessages` in TextToImageServerSpecificationAlert

--- src/features/TextToImage/components/TextToImageServerSpecificationAlert.vue
+++ src/features/TextToImage/components/TextToImageServerSpecificationAlert.vue
@@ -6,15 +6,13 @@ const given = defineProps<{
 }>();
 
 (() => {
-  const userFacingMessage = [
+  return given.error.throwAnyway([
     'No text-to-image server was specified!',
     'Either:',
     `a) supply it's corresponding environment variable named \`${given.error.environmentKey}\` and rebuild`,
     `b) specify it within ${given.error.file.toString()}`,
     'OR',
     `c) specify it within the response from ${given.error.endpoint.toString()}`,
-  ].join('\n');
-
-  return given.error.throwAnyway(userFacingMessage);
+  ].join('\n'));
 })();
 </script>
