17d9960c618803ece778b363f8c8aa52c5b193e4
Jacob Gordon <jacob.gordon@amd.com>
2025-06-12 20:26:11 +0000

    refactor(text-to-image): extracts failure from interpretation

--- src/features/TextToImage/models/SDXL/client/index.ts
+++ src/features/TextToImage/models/SDXL/client/index.ts
@@ -97,8 +97,11 @@ const generateOutputFrom = async (
       }
       else {
         interpretedError = new Server.ConnectionError(shimmedStabilityAIClient.origin);
-        return ends.inFailureDueTo(interpretedError);
       }
+
+      if (
+        interpretedError !== null
+      ) return ends.inFailureDueTo(interpretedError);
 
       return Attempt.NonActionableError.rethrow(someError, {
         message: 'Text-to-image client failed to generate image due to an unexpected error',
