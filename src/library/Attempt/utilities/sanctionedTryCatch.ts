b66a8871fc00f12015a1ec534da364ecda684786
Jacob Gordon <jacob.gordon@amd.com>
2025-06-13 21:59:22 +0000

    refactor(library/Attempt): leverages sanctioned catch in `sanctionedAsync`

--- src/library/Attempt/utilities/sanctionedTryCatch.ts
+++ src/library/Attempt/utilities/sanctionedTryCatch.ts
@@ -44,14 +44,19 @@ const sanctionedAsync = async <
     catch: ($0: AppropriatelyThrown<Error>) => CatchBlockOutput;
   },
 ): Promise<TryBlockOutput | CatchBlockOutput> => {
+  const sanction = (whateverThatWasThrown: unknown) => sanctioned({
+    try: () => {
+      throw whateverThatWasThrown; // eslint-disable-line no-restricted-syntax -- puts the error back through the sanctioned catch
+    },
+    catch: catchBlockOutputFor,
+  });
+
   // eslint-disable-next-line no-restricted-syntax -- this is the implementation designed to help avoid use of raw try/catch
   try {
     return await retrieveTryBlockOutput();
   }
   catch (whateverThatWasThrown) {
-    const someError = asError(whateverThatWasThrown);
-    const someAppropriatelyThrownError = assertAppropriatelyThrown(someError);
-    return catchBlockOutputFor(someAppropriatelyThrownError);
+    return sanction(whateverThatWasThrown);
   }
 };
 
