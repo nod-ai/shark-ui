b4f35024f32da0b3f3444753d2c6b7c4e71486fe
Jacob Gordon <jacob.gordon@amd.com>
2025-06-17 17:43:28 -0500

    fix(library/Range): avoids repeated bounds for `inclusiveSteps` in `DiscreteRange` (#376)

--- src/library/Range/DiscreteRange.ts
+++ src/library/Range/DiscreteRange.ts
@@ -59,17 +59,25 @@ class DiscreteRange extends Range {
     }
   }
 
+  private* generateInclusiveSteps() {
+    yield this.lowerBound;
+
+    if (
+      this.lowerBound === this.upperBound
+    ) return;
+
+    yield* this.generateExclusiveSteps();
+    yield this.upperBound;
+  }
+
   public get exclusiveSteps(): number[] {
     const generatedSteps = this.generateExclusiveSteps();
     return Array.from(generatedSteps);
   }
 
   public get inclusiveSteps(): number[] {
-    return [
-      this.lowerBound,
-      ...this.exclusiveSteps,
-      this.upperBound,
-    ];
+    const generatedSteps = this.generateInclusiveSteps();
+    return Array.from(generatedSteps);
   }
 }
 
