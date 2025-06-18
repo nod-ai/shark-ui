3edc728afae2308f60edb46c59b050ee82c14210
Jacob Gordon <jacob.gordon@amd.com>
2025-06-18 21:12:43 +0000

    refactor(library/HTTP): prefers `HTTP_Response` internally

--- src/library/HTTP/HTTPResponseError.ts
+++ src/library/HTTP/HTTPResponseError.ts
@@ -1,11 +1,13 @@
 import Attempt from '@/library/Attempt';
 
-import type * as HTTPResponse from './HTTPResponse';
+import type {
+  HTTP_Response,
+} from './Response';
 
 class HTTPResponseError extends Attempt.ActionableError<'HTTPResponseError'> {
-  public readonly status: HTTPResponse.ErrorStatusCode;
+  public readonly status: HTTP_Response.StatusCode.Error.Any;
 
-  public constructor(givenMessage: string, givenStatus: HTTPResponse.ErrorStatusCode) {
+  public constructor(givenMessage: string, givenStatus: HTTP_Response.StatusCode.Error.Any) {
     super(givenMessage);
     this.name = 'HTTPResponseError';
     this.status = givenStatus;
