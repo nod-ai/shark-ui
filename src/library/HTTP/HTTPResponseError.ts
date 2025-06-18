e723268779914bc23ed91506aad4bfced85c7a3c
Jacob Gordon <jacob.gordon@amd.com>
2025-06-18 19:07:26 +0000

    refactor(library/HTTP): renames `HTTPResponseError` to `HTTP_Endpoint_ResponseError`

--- src/library/HTTP/HTTPResponseError.ts
+++ src/library/HTTP/HTTPResponseError.ts
@@ -4,16 +4,16 @@ import type {
   HTTP_Response,
 } from './Response';
 
-class HTTPResponseError extends Attempt.ActionableError<'HTTPResponseError'> {
+class HTTP_Endpoint_ResponseError extends Attempt.ActionableError<'HTTP_Endpoint_ResponseError'> {
   public readonly status: HTTP_Response.StatusCode.Error.Any;
 
   public constructor(givenMessage: string, givenStatus: HTTP_Response.StatusCode.Error.Any) {
     super(givenMessage);
-    this.name = 'HTTPResponseError';
+    this.name = 'HTTP_Endpoint_ResponseError';
     this.status = givenStatus;
   }
 }
 
 export {
-  HTTPResponseError as default,
+  HTTP_Endpoint_ResponseError,
 };
