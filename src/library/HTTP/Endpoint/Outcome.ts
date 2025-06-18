9fd30681dca12daea8e7c0e36abce3a2e3cdfa28
Jacob Gordon <jacob.gordon@amd.com>
2025-06-18 19:14:36 +0000

    preliminary(library/HTTP): defines `HTTP.Endpoint.RequestError`

--- src/library/HTTP/Endpoint/Outcome.ts
+++ src/library/HTTP/Endpoint/Outcome.ts
@@ -1,12 +1,16 @@
 import type Attempt from '@/library/Attempt';
 
 import type {
+  HTTP_Endpoint_RequestError,
+} from './RequestError';
+
+import type {
   HTTP_Endpoint_ResponseError,
 } from './ResponseError';
 
 type HTTP_Endpoint_Outcome = Attempt.Outcome<
   unknown,
-  HTTP_Endpoint_ResponseError
+  HTTP_Endpoint_RequestError | HTTP_Endpoint_ResponseError
 >;
 
 export type {
