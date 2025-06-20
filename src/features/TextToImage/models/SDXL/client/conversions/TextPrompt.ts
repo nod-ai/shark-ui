4fd530862ecec279d0f52333e7537b0cc57ecdea
Jacob Gordon <jacob.gordon@amd.com>
2025-06-20 12:46:36 -0500

    refactor(text-to-image): extracts conversions from SDXL client (#390)
    - pointed out by #389, but the overlapping levels of abstractions was the actual source of the diff noise rather than style

---
+++ src/features/TextToImage/models/SDXL/client/conversions/TextPrompt.ts
@@ -0,0 +1,31 @@
+import type {
+  TextPrompt,
+} from 'stabilityai-client-typescript/models/components';
+
+const isDefault = (
+  givenWeight: TextPrompt['weight'],
+) => {
+  return (givenWeight === undefined) || (givenWeight === 1);
+};
+
+const serialized = (
+  givenPrompt: TextPrompt,
+): string => {
+  if (
+    isDefault(givenPrompt.weight)
+  ) return givenPrompt.text;
+
+  return `(${givenPrompt.text}: ${givenPrompt.weight.toString()})`;
+};
+
+const allSerialized = (
+  givenPrompts: TextPrompt[],
+): string => {
+  return givenPrompts
+    .map(serialized)
+    .join(', ');
+};
+
+export {
+  allSerialized,
+};
