a3e4030cbcdb625aa73f0627248f2f1175922ae3
Jacob Gordon <jacob.gordon@amd.com>
2025-06-23 22:15:09 +0000

    refactor(library/Attempt): extracts "Outcome/Success.ts" from "Outcome/index.ts"

---
+++ src/library/Attempt/Outcome/Success.ts
@@ -0,0 +1,62 @@
+// cspell:words sugarfree discriminable
+
+import type {
+  DiscriminableOutcome,
+} from './Discriminable';
+
+interface SemanticallySugarfreeSuccess<SomeProduct> extends DiscriminableOutcome<SomeProduct> {
+  readonly case: 'success';
+  readonly product: SomeProduct;
+}
+
+interface Attempt_Success<SomeProduct> extends SemanticallySugarfreeSuccess<SomeProduct> {
+  /**
+   * Access the product nested within a successful outcome.
+   *
+   * Adds a guarded parallel to the `optionallyUnwrap` and `forciblyUnwrapped` methods:
+   * ```ts
+   * function doRiskyThingUnsafely(
+   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
+   * ): void {
+   *   console.log(givenOutcome.forciblyUnwrap());
+   * }
+   *
+   * function doRiskyThingSafelyWhileIgnoringErrors(
+   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
+   * ): void {
+   *   console.log(givenOutcome.optionallyUnwrap());
+   * }
+   *
+   * function doRiskyThingSafelyWhileHandlingErrors(
+   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
+   *   recoverFrom: (expectedError: CustomError) => void,
+   * ): void {
+   *   if (
+   *     givenOutcome.isFailure
+   *   ) recoverFrom(givenOutcome.causeOfFailure);
+   *
+   *   console.log(givenOutcome.unwrapped);
+   * }
+   * ```
+   */
+  readonly unwrapped: this['product'];
+}
+
+const successThatYielded = <
+  SomeProduct,
+>(
+  givenProduct: SomeProduct,
+): Attempt_Success<SomeProduct> => ({
+  case            : 'success',
+  product         : givenProduct,
+  isSuccess       : true,
+  isFailure       : false,
+  optionallyUnwrap: () => givenProduct,
+  forciblyUnwrap  : () => givenProduct,
+  unwrapped       : givenProduct,
+});
+
+export {
+  type Attempt_Success,
+  successThatYielded,
+};
--- src/library/Attempt/Outcome/index.ts
+++ src/library/Attempt/Outcome/index.ts
@@ -8,49 +8,16 @@ import type {
   DiscriminableOutcome,
 } from './Discriminable';
 
-interface SemanticallySugarfreeSuccess<SomeProduct> extends DiscriminableOutcome<SomeProduct> {
-  readonly case: 'success';
-  readonly product: SomeProduct;
-}
+import {
+  type Attempt_Success,
+  successThatYielded,
+} from './Success';
 
 interface SemanticallySugarfreeFailure<
   SomeActionableError extends ActionableError<string>,
 > extends DiscriminableOutcome<unknown> {
   readonly case: 'failure';
   readonly cause: SomeActionableError;
-}
-
-interface Attempt_Success<SomeProduct> extends SemanticallySugarfreeSuccess<SomeProduct> {
-  /**
-   * Access the product nested within a successful outcome.
-   *
-   * Adds a guarded parallel to the `optionallyUnwrap` and `forciblyUnwrapped` methods:
-   * ```ts
-   * function doRiskyThingUnsafely(
-   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
-   * ): void {
-   *   console.log(givenOutcome.forciblyUnwrap());
-   * }
-   *
-   * function doRiskyThingSafelyWhileIgnoringErrors(
-   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
-   * ): void {
-   *   console.log(givenOutcome.optionallyUnwrap());
-   * }
-   *
-   * function doRiskyThingSafelyWhileHandlingErrors(
-   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
-   *   recoverFrom: (expectedError: CustomError) => void,
-   * ): void {
-   *   if (
-   *     givenOutcome.isFailure
-   *   ) recoverFrom(givenOutcome.causeOfFailure);
-   *
-   *   console.log(givenOutcome.unwrapped);
-   * }
-   * ```
-   */
-  readonly unwrapped: this['product'];
 }
 
 interface Attempt_Failure<
@@ -75,20 +42,6 @@ interface Attempt_Failure<
    */
   readonly causeOfFailure: this['cause'];
 }
-
-const successThatYielded = <
-  SomeProduct,
->(
-  givenProduct: SomeProduct,
-): Attempt_Success<SomeProduct> => ({
-  case            : 'success',
-  product         : givenProduct,
-  isSuccess       : true,
-  isFailure       : false,
-  optionallyUnwrap: () => givenProduct,
-  forciblyUnwrap  : () => givenProduct,
-  unwrapped       : givenProduct,
-});
 
 const failureDueTo = <
   SomeActionableError extends ActionableError<string>,
