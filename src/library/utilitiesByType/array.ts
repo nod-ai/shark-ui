2e7e5e05a2c664607c44cb3dc0dbeee57442b104
Jacob Gordon <jacob.gordon@amd.com>
2025-06-25 09:17:41 -0500

    refactor(text-to-image): separates response conversion concerns from main logic in SDXL client (#412)
    - see PR commits for refactor operations
    - facilitates #408

--- src/library/utilitiesByType/array.ts
+++ src/library/utilitiesByType/array.ts
@@ -1,7 +1,16 @@
-const isEmpty = (givenSubject: unknown[]): givenSubject is [] => {
+const isEmpty = (
+  givenSubject: unknown[],
+): givenSubject is [] => {
   return givenSubject.length === 0;
+};
+
+const hasAtLeastOne = <Element>(
+  givenElements: Element[],
+): givenElements is [Element, ...Element[]] => {
+  return !isEmpty(givenElements);
 };
 
 export {
   isEmpty,
+  hasAtLeastOne as hasAtLeastOne,
 };
