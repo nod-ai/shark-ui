a2c699acdb26a3459426673cca5766a992ca2248
Jacob Gordon <jacob.gordon@amd.com>
2025-06-27 22:49:59 +0000

    refactor(library/URI): extracts `prefixedKeyValuePairs` in `MediaType`

--- src/library/customTypes/UniformResourceIdentifier/Data/MediaType/index.ts
+++ src/library/customTypes/UniformResourceIdentifier/Data/MediaType/index.ts
@@ -69,11 +69,11 @@ implements StringForciblyParsable<
       this.parameters === null
     ) return null;
 
-    const serializedKeyValuePairs = Object.entries(this.parameters)
+    const prefixedKeyValuePairs = Object.entries(this.parameters)
       .map($0 => $0.join(MediaType.parameterKeyValueDelimiter))
-      .map($0 => MediaType.parameterPrefix.concat($0))
-      .join('');
+      .map($0 => MediaType.parameterPrefix.concat($0));
 
+    const serializedKeyValuePairs = prefixedKeyValuePairs.join('');
     return serializedKeyValuePairs;
   }
 
