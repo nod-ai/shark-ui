85fecf9163abed789c6ac92d4a08dc1698f9227d
Jacob Gordon <jacob.gordon@amd.com>
2025-07-01 21:45:00 +0000

    refactor(library/URI): renames `format` to `parsedFormat` in `ImageURI`

--- src/library/customTypes/UniformResourceIdentifier/Data/Image/index.ts
+++ src/library/customTypes/UniformResourceIdentifier/Data/Image/index.ts
@@ -58,14 +58,14 @@ class ImageURI
       parsedURI.mediaType.fileType !== ImageURI.fileType
     ) return new ImageURI_ParsingError(`Expected media type starting with ${ImageURI.fileType}`).throwAnyway('To be converted to `Attempt` failure');
 
-    const format = allImageURIFormats.find($0 => $0 === parsedURI.mediaType.fileSubtype);
+    const parsedFormat = allImageURIFormats.find($0 => $0 === parsedURI.mediaType.fileSubtype);
 
     if (
-      format === undefined
+      parsedFormat === undefined
     ) return new ImageURI_ParsingError(`Expected format to be one of ${allImageURIFormats.toString()}`).throwAnyway('To be converted to `Attempt` failure');
 
     const parsedImageURI = new ImageURI(
-      format,
+      parsedFormat,
       parsedURI.encoding,
       parsedURI.data,
     );
