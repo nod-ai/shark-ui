dbbc39896c4336da46ae5740b7affff1c1a17ebe
Jacob Gordon <jacob.gordon@amd.com>
2025-07-02 15:27:04 +0000

    linter(cypress): extracts details from "eslint.config.ts"

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,13 +5,13 @@ import {
   vueTsConfigs,
 } from '@vue/eslint-config-typescript';
 
-import pluginCypress from 'eslint-plugin-cypress';
 import pluginVue from 'eslint-plugin-vue';
 
 import tseslint, {
   type ConfigWithExtends,
 } from 'typescript-eslint';
 
+import pluginCypress from './cypress/eslint.config';
 import pluginImport from './eslint.import';
 import pluginStylistic from './eslint.stylistic';
 
@@ -104,13 +104,7 @@ const configWithVueTS = defineConfigWithVueTs(
     files: ['src/**/__tests__/*'],
   },
 
-  {
-    ...pluginCypress.configs.recommended,
-    files: [
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}',
-    ],
-  },
+  ...pluginCypress,
 );
 
 const completeConfig = tseslint.config([
