d66c16bb70db1cef8f6af00e651c8aeb16102561
Jacob Gordon <jacob.gordon@amd.com>
2025-07-02 19:03:49 +0000

    refactor(library/URI): extracts `outcomeOfParsingMediaType` in `DataURI`

--- src/library/customTypes/UniformResourceIdentifier/Data/index.ts
+++ src/library/customTypes/UniformResourceIdentifier/Data/index.ts
@@ -118,7 +118,8 @@ class DataURI
       rawMediaType === undefined
     ) return new DataURI_ParsingError('Expected `mediaType` portion to be defined').throwAnyway('To be converted to `Attempt` failure');
 
-    const parsedMediaType = MediaType.parsedFrom(rawMediaType).forciblyUnwrap(/* TODO: move to guard statement */);
+    const outcomeOfParsingMediaType = MediaType.parsedFrom(rawMediaType);
+    const parsedMediaType = outcomeOfParsingMediaType.forciblyUnwrap(/* TODO: move to guard statement */);
     const parsedEncoding = allDataURIBinaryEncodings.find($0 => $0 === rawEncoding);
 
     if (
