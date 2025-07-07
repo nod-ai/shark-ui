0d1ee06fa6a3346996990c5f9b2273785ef38234
Jacob Gordon <jacob.gordon@amd.com>
2025-07-07 19:55:22 +0000

    refactor(library/URI): replaces `DataURIBinaryEncoding` with `DataURI_EncodingIdentifier`

---
+++ src/library/customTypes/UniformResourceIdentifier/Data/EncodingIdentifier/exports.ts
@@ -0,0 +1,7 @@
+export {
+  DataURI_EncodingIdentifier_all as all,
+} from './all';
+
+export type {
+  DataURI_EncodingIdentifier_Any as Any,
+} from './Any';
--- src/library/customTypes/UniformResourceIdentifier/Data/Image/index.ts
+++ src/library/customTypes/UniformResourceIdentifier/Data/Image/index.ts
@@ -1,8 +1,8 @@
 import type Base64CharacterEncodedByteSequence from '@/library/Base64CharacterEncodedByteSequence';
 
 import type {
-  DataURIBinaryEncoding,
-} from '../DataURIBinaryEncoding.ts';
+  DataURI_EncodingIdentifier,
+} from '../EncodingIdentifier';
 
 import MediaType from '../MediaType';
 import DataURI from '../index.ts';
@@ -19,7 +19,7 @@ class ImageURI
 
   public constructor(
     givenFormat: ImageURI_Format.Any,
-    givenEncoding: DataURIBinaryEncoding,
+    givenEncoding: DataURI_EncodingIdentifier.Any,
     givenData: Base64CharacterEncodedByteSequence,
   ) {
     super(
