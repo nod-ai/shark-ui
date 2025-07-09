670db932904e9cf67edd12ba54ee3e09ebacf640
Jacob Gordon <jacob.gordon@amd.com>
2025-07-09 08:58:51 -0500

    style: ensures multiline parameters in constructors (#497)
    - minimizes diffs for refactored constructors
    - facilitates #491

--- src/library/Byte/Sequence/EncodingCompatibilityError.ts
+++ src/library/Byte/Sequence/EncodingCompatibilityError.ts
@@ -8,7 +8,9 @@ class Byte_Sequence_EncodingCompatibilityError
   extends Attempt.ActionableError<
   'Byte_Sequence_EncodingCompatibilityError'
 > {
-  public constructor(givenBitWidth: number) {
+  public constructor(
+    givenBitWidth: number,
+  ) {
     const byteCofactor = Byte_cofactorTo(givenBitWidth);
     super(`Character count for ${givenBitWidth.toString()}-bit sequence must be a multiple of ${byteCofactor.toString()} to be byte encodable`);
     this.name = 'Byte_Sequence_CompatibilityError';
