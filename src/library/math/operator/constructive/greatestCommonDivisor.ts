3dced2d8b733aad1f93f3fd24655e5cf53028a6a
Jacob Gordon <jacob.gordon@amd.com>
2025-07-10 15:20:10 +0000

    refactor(library/math): moves `greatestCommonDivisor`

--- src/library/math/operator/constructive/greatestCommonDivisor.ts
+++ src/library/math/operator/constructive/greatestCommonDivisor.ts
@@ -1,6 +1,18 @@
-import {
-  greatestCommonDivisor,
-} from '../../../math/operators';
+const greatestCommonDivisor = (
+  leftHandOperand: number,
+  rightHandOperand: number,
+): number => {
+  let nextDividend = leftHandOperand;
+  let previousRemainder = rightHandOperand;
+
+  while (previousRemainder !== 0) {
+    const eachDivisor = previousRemainder;
+    previousRemainder = nextDividend % eachDivisor;
+    nextDividend = eachDivisor;
+  }
+
+  return nextDividend;
+};
 
 export {
   greatestCommonDivisor,
--- src/library/math/operators.ts
+++ src/library/math/operators.ts
@@ -1,16 +1,2 @@
-const greatestCommonDivisor = (a: number, b: number): number => {
-  let nextDividend = a;
-  let previousRemainder = b;
-
-  while (previousRemainder !== 0) {
-    const eachDivisor = previousRemainder;
-    previousRemainder = nextDividend % eachDivisor;
-    nextDividend = eachDivisor;
-  }
-
-  return nextDividend;
-};
-
 export {
-  greatestCommonDivisor,
 };
