6f5425a493149149294097a8c789db06507654b0
Jacob Gordon <jacob.gordon@amd.com>
2025-07-11 13:25:06 +0000

    refactor(library/math): moves `sumOf`

--- src/library/math/aggregator/sumOf.ts
+++ src/library/math/aggregator/sumOf.ts
@@ -1,6 +1,10 @@
-import {
-  sumOf,
-} from '../aggregators';
+import type {
+  Aggregator,
+} from './Aggregator';
+
+const sumOf: Aggregator = (
+  ...givenAddends
+) => givenAddends.reduce(($0, $1) => $0 + $1);
 
 export {
   sumOf,
--- src/library/math/aggregators.ts
+++ src/library/math/aggregators.ts
@@ -1,10 +1,5 @@
 type Aggregator = (...givenValues: [number, ...number[]]) => number;
 
-const sumOf: Aggregator = (
-  ...givenAddends
-) => givenAddends.reduce(($0, $1) => $0 + $1);
-
 export {
   type Aggregator,
-  sumOf,
 };
