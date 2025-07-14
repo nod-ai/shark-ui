1bf0d6d310ebfb46bab07d4a9eea78543cdfd90a
Jacob Gordon <jacob.gordon@amd.com>
2025-07-14 21:45:47 +0000

    tests(library/math): specs general concrete answers for `greatestCommonDivisor`

--- src/library/math/operator/constructive/greatestCommonDivisor.test.ts
+++ src/library/math/operator/constructive/greatestCommonDivisor.test.ts
@@ -211,6 +211,33 @@ describe(greatestCommonDivisor, () => {
       ).toBe(identityFactor);
     });
 
-    it.todo('should extract a common factor applied to two distinct prime numbers');
+    const combosOfDistinctNonComposites = pairCombos({
+      left  : primeA,
+      right : primeB,
+      filler: identityFactor,
+    });
+
+    describe.each(combosOfDistinctNonComposites)('when applied to two distinct prime numbers', (...eachComboOfNonComposites) => {
+      const smallSquare = squared(primeA);
+      const largeSquare = squared(primeC);
+
+      const commonFactors = [
+        identityFactor,
+        primeC,
+        smallSquare,
+        largeSquare,
+      ];
+
+      it.each(commonFactors)('should extract a common factor', (eachCommonFactor) => {
+        expect.assertions(1);
+
+        expect(
+          greatestCommonDivisor(
+            eachCommonFactor * eachComboOfNonComposites[0],
+            eachCommonFactor * eachComboOfNonComposites[1],
+          ),
+        ).toBe(eachCommonFactor);
+      });
+    });
   });
 });
