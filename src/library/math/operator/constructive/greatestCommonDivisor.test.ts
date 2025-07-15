bf18579cd5e931780394fc8c99710ac033e893d4
Jacob Gordon <jacob.gordon@amd.com>
2025-07-15 21:22:57 +0000

    tests(library/math): stubs coverage for "happy" outcomes in `greatestCommonDivisor`

--- src/library/math/operator/constructive/greatestCommonDivisor.test.ts
+++ src/library/math/operator/constructive/greatestCommonDivisor.test.ts
@@ -26,5 +26,21 @@ describe(greatestCommonDivisor, () => {
     });
   });
 
-  describe.todo('the happy outcomes');
+  describe('the happy outcomes', () => {
+    it.todo('should accept valid operands');
+
+    it.todo('should return the same answer regardless of the sign of each operand');
+
+    it.todo('should be commutative');
+
+    it.todo('should be associative');
+
+    it.todo('should return an operable answer when zero is an operand');
+
+    it.todo('should preserve the identity of matching operands');
+
+    it.todo('should return identity factor for co-prime operands');
+
+    it.todo('should extract a common factor applied to two distinct prime numbers');
+  });
 });
