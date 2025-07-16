759e279431398bea82cc8cf0d75304a6356fe305
Jacob Gordon <jacob.gordon@amd.com>
2025-07-16 15:04:11 -0500

    tests(library/math): stubs complete coverage for `greatestCommonDivisor` (#532)
    - polishes a documentation introduced in #533 
        - "inputs" is an umbrella term that really means "the term used to refer to the inputs in this specific context"
        - i.e. if it's a mathematical operation, then the preferred term would be "operands"

--- src/library/math/operator/constructive/greatestCommonDivisor.test.ts
+++ src/library/math/operator/constructive/greatestCommonDivisor.test.ts
@@ -1,5 +1,6 @@
 import {
   describe,
+  it,
 } from 'vitest';
 
 import {
@@ -8,10 +9,38 @@ import {
 
 describe(greatestCommonDivisor, () => {
   describe('the sad outcomes', () => {
-    describe.todo('when delegated');
+    describe('when delegated', () => {
+      it.todo('should reject inoperable operands');
 
-    describe.todo('when generated');
+      it.todo('should reject infinite operands');
+    });
+
+    describe('when generated', () => {
+      describe('due to fractional operands', () => {
+        it.todo('should reject them');
+
+        it.todo('should safely propagate the error');
+
+        it.todo('should communicate clearly with developers');
+      });
+    });
   });
 
-  describe.todo('the happy outcomes');
+  describe('the happy outcomes', () => {
+    it.todo('should accept valid operands');
+
+    it.todo('should return the same answer regardless of the sign of each operand');
+
+    it.todo('should be commutative');
+
+    it.todo('should be associative');
+
+    it.todo('should return an operable answer when zero is an operand');
+
+    it.todo('should preserve the identity of matching operands');
+
+    it.todo('should return identity factor for co-prime operands');
+
+    it.todo('should extract a common factor applied to two distinct prime numbers');
+  });
 });
