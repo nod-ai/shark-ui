5b47692a8b3f400d55a51ee20680ebccfd6085bb
Jacob Gordon <jacob.gordon@amd.com>
2025-07-17 10:11:09 -0500

    refactor(library/math): separates concerns of "number taxonomy" from `isOperable` test suite (#539)
    - facilitates #538

--- src/library/math/operator/predicate/isOperable.test.ts
+++ src/library/math/operator/predicate/isOperable.test.ts
@@ -5,45 +5,27 @@ import {
 } from 'vitest';
 
 import {
+  numberTaxonomy,
+} from '../../numberTaxonomy';
+
+import {
   isOperable,
 } from './isOperable';
 
-const numericCategory = {
-  signed: {
-    negative: /**/-1,
-    positive: /**/+1,
-  },
-  fractional: {
-    rational      : 1 / 2,
-    irrational    : Math.PI,
-    transcendental: Math.E,
-  },
-  infinite: {
-    positive: Infinity,
-    negative: -Infinity,
-  },
-  runtime: {
-    min: Number.MIN_VALUE,
-    max: Number.MAX_VALUE,
-  },
-  origin   : 0,
-  undefined: NaN,
-} as const;
-
 describe(isOperable, () => {
-  const theSoleInoperableNumber = numericCategory.undefined;
+  const theSoleInoperableNumber = numberTaxonomy.undefined;
 
   const operableNumbers = [
-    numericCategory.origin,
-    numericCategory.signed.negative,
-    numericCategory.signed.positive,
-    numericCategory.fractional.rational,
-    numericCategory.fractional.irrational,
-    numericCategory.fractional.transcendental,
-    numericCategory.infinite.positive,
-    numericCategory.infinite.negative,
-    numericCategory.runtime.min,
-    numericCategory.runtime.max,
+    numberTaxonomy.origin,
+    numberTaxonomy.signed.negative,
+    numberTaxonomy.signed.positive,
+    numberTaxonomy.fractional.rational,
+    numberTaxonomy.fractional.irrational,
+    numberTaxonomy.fractional.transcendental,
+    numberTaxonomy.infinite.positive,
+    numberTaxonomy.infinite.negative,
+    numberTaxonomy.runtime.min,
+    numberTaxonomy.runtime.max,
   ];
 
   describe('the guaranteed behavior', () => {
