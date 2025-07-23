9c1a086e07f8f730617007daccc4f361d50427f8
Jacob Gordon <jacob.gordon@amd.com>
2025-07-23 21:57:07 +0000

    refactor(library/math): adds common abbreviations for "greatest common divisor"

--- src/library/math/operator/constructive/greatestCommonDivisor.ts
+++ src/library/math/operator/constructive/greatestCommonDivisor.ts
@@ -42,10 +42,18 @@ const highestCommonDivisor = greatestCommonDivisor;
 /** Alias for {@link greatestCommonDivisor} */
 const highestCommonFactor = greatestCommonDivisor;
 
+/** Abbreviation for {@link greatestCommonDivisor} */
+const gcd = greatestCommonDivisor;
+
+/** Abbreviation for {@link greatestCommonFactor} */
+const gcf = greatestCommonFactor;
+
 export {
   greatestCommonDivisor as default,
   greatestCommonDivisor,
   greatestCommonFactor,
   highestCommonDivisor,
   highestCommonFactor,
+  gcd,
+  gcf,
 };
