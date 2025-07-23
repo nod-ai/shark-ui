3f191f69db8236bbd7df411939667e9c5b02d1c4
Jacob Gordon <jacob.gordon@amd.com>
2025-07-23 21:56:45 +0000

    refactor(library/math): adds common aliases for "greatest common divisor"

--- src/library/math/operator/constructive/greatestCommonDivisor.ts
+++ src/library/math/operator/constructive/greatestCommonDivisor.ts
@@ -33,7 +33,19 @@ const greatestCommonDivisor = (
   return leftHandMagnitude;
 };
 
+/** Alias for {@link greatestCommonDivisor} */
+const greatestCommonFactor = greatestCommonDivisor;
+
+/** Alias for {@link greatestCommonDivisor} */
+const highestCommonDivisor = greatestCommonDivisor;
+
+/** Alias for {@link greatestCommonDivisor} */
+const highestCommonFactor = greatestCommonDivisor;
+
 export {
   greatestCommonDivisor as default,
   greatestCommonDivisor,
+  greatestCommonFactor,
+  highestCommonDivisor,
+  highestCommonFactor,
 };
