7317c127a9b86904741a691ce86a20f332c41596
Jacob Gordon <jacob.gordon@amd.com>
2025-07-25 16:57:22 +0000

    refactor: renames "index.ts" to "exports.ts" in "features/TextToImage/utilities/config/StaticConfig"

--- src/features/TextToImage/config/exports.ts
+++ src/features/TextToImage/config/exports.ts
@@ -1,3 +1,3 @@
 export * as DynamicConfig from './utilities/DynamicConfig/exports';
-export * as StaticConfig from './utilities/StaticConfig';
+export * as StaticConfig from './utilities/StaticConfig/exports';
 export * from './utilities/emptyConfig';
