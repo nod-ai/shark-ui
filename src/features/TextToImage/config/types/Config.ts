c95157520ac854ef9d3d74d4f3e79ebd7da40578
Jacob Gordon <jacob.gordon@amd.com>
2025-08-04 19:58:25 +0000

    preliminary: conforms candidates to `Parsable`

--- src/features/TextToImage/config/types/Config.ts
+++ src/features/TextToImage/config/types/Config.ts
@@ -1,8 +1,23 @@
+import type Attempt from '@/library/Attempt';
+
+import {
+  type Parsable,
+  Parse_instanceFrom,
+} from '@/library/Parser';
+
 import Schema from '@/library/Schema';
 import * as WebAPI from '@/library/WebAPI';
 
+import {
+  TextToImage_Config_ParsingError,
+} from './Config_ParsingError';
+
 /** The user-provided settings for the text-to-image feature */
-class TextToImage_Config {
+class TextToImage_Config
+implements Parsable<
+  typeof TextToImage_Config,
+  /*  */ TextToImage_Config_ParsingError
+> {
   public constructor(
     /** The details of the server that's providing text-to-image generation */
     public readonly server: WebAPI.Server | null,
@@ -17,6 +32,20 @@ class TextToImage_Config {
     .transform($0 => new TextToImage_Config(
       $0.server,
     ));
+
+  public static parsedFrom(
+    givenSubject: unknown,
+  ): Attempt.Outcome<
+    TextToImage_Config,
+    TextToImage_Config_ParsingError
+  > {
+    const parsedConfig = Parse_instanceFrom(givenSubject, {
+      using      : this.Schema,
+      failingWith: TextToImage_Config_ParsingError,
+    });
+
+    return parsedConfig;
+  }
 }
 
 export {
