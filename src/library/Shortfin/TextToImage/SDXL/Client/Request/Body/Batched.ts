4d58b2e05ed34f19d0a07c418d806e7221a78ed5
Jacob Gordon <jacob.gordon@amd.com>
2025-08-05 19:34:29 +0000

    style(library/Shortfin): aligns delimiters in `Shortfin_TextToImage_SDXL_Client_Request_Body_Batched`

--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
@@ -2,13 +2,13 @@ type UnsignedInteger = number;
 type FloatingPoint = number;
 
 interface Shortfin_TextToImage_SDXL_Client_Request_Body_Batched {
-  prompt: /*        */ string[];
-  neg_prompt: /*    */ string[];
-  height: /*        */ UnsignedInteger[];
-  width: /*         */ UnsignedInteger[];
-  steps: /*         */ UnsignedInteger[];
-  guidance_scale: /**/ FloatingPoint[];
-  seed: /*          */ UnsignedInteger[];
+  prompt/*    */: string[];
+  neg_prompt/**/: string[];
+  height/*    */: UnsignedInteger[];
+  width/*     */: UnsignedInteger[];
+  steps/*     */: UnsignedInteger[];
+  guidance_scale: FloatingPoint[];
+  seed/*      */: UnsignedInteger[];
 }
 
 export type {
