4c6cfbd1e92426e7664ec769f8ad184a9130f95a
Jacob Gordon <jacob.gordon@amd.com>
2025-08-05 16:28:29 +0000

    refactor(library/ShimmedStabilityAIClient): prefers "Batched" as final namespace
    - leaves room for more conventional "non-batched" request

--- src/library/ShimmedStabilityAIClient/definition.ts
+++ src/library/ShimmedStabilityAIClient/definition.ts
@@ -24,7 +24,7 @@ import {
 type UnsignedInteger = number;
 type FloatingPoint = number;
 
-interface Shortfin_TextToImage_SDXL_Client_BatchRequest_Body {
+interface Shortfin_TextToImage_SDXL_Client_Request_Body_Batched {
   prompt: /*        */ string[];
   neg_prompt: /*    */ string[];
   height: /*        */ UnsignedInteger[];
@@ -48,7 +48,7 @@ const toSerializedPromptValue = (
     .join(',');
 };
 
-const emptyBatchGenerationRequest: Shortfin_TextToImage_SDXL_Client_BatchRequest_Body = {
+const emptyBatchGenerationRequest: Shortfin_TextToImage_SDXL_Client_Request_Body_Batched = {
   prompt        : [],
   neg_prompt    : [],
   height        : [],
@@ -60,9 +60,9 @@ const emptyBatchGenerationRequest: Shortfin_TextToImage_SDXL_Client_BatchRequest
 
 const toBatchGenerationRequestBody = (
   givenRequests: GenerateFromTextRequest['textToImageRequestBody'][],
-): Shortfin_TextToImage_SDXL_Client_BatchRequest_Body => {
+): Shortfin_TextToImage_SDXL_Client_Request_Body_Batched => {
   return givenRequests.reduce<
-    Shortfin_TextToImage_SDXL_Client_BatchRequest_Body
+    Shortfin_TextToImage_SDXL_Client_Request_Body_Batched
   >((runningBatchRequest, eachRequest) => {
     if (
       (eachRequest.height !== undefined)
