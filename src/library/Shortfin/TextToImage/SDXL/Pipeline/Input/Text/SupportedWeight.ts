6ab4dab5621d016c0dc1d8ba80e9a966ea0eb954
Jacob Gordon <jacob.gordon@amd.com>
2025-08-05 20:41:07 +0000

    prefeature(library/Shortfin): defines `Shortfin.TextToImage.SDXL.Pipeline.Input.Text.SupportedWeight`

---
+++ src/library/Shortfin/TextToImage/SDXL/Pipeline/Input/Text/SupportedWeight.ts
@@ -0,0 +1,5 @@
+type Shortfin_TextToImage_SDXL_Pipeline_Input_Text_SupportedWeight = 1 | -1;
+
+export type {
+  Shortfin_TextToImage_SDXL_Pipeline_Input_Text_SupportedWeight,
+};
---
+++ src/library/Shortfin/TextToImage/SDXL/Pipeline/index.ts
@@ -0,0 +1,1 @@
+export type * as Shortfin_TextToImage_SDXL_Pipeline from './exports.ts';
