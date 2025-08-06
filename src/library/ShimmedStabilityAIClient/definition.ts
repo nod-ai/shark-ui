e4f61b6b4c40a214e05fe931a19bfc7d55c83845
Jacob Gordon <jacob.gordon@amd.com>
2025-08-06 18:54:29 +0000

    refactor(library/ShimmedStabilityAIClient): adds "body" to symbol name for empty batched request body

--- src/library/ShimmedStabilityAIClient/definition.ts
+++ src/library/ShimmedStabilityAIClient/definition.ts
@@ -25,7 +25,7 @@ import {
   toShortfinRequestBodyPrompt,
 } from './conversions';
 
-const emptyBatchedGenerationRequest = new Shortfin.TextToImage.SDXL.Client.Request.Body.Batched();
+const emptyBatchedGenerationRequestBody = new Shortfin.TextToImage.SDXL.Client.Request.Body.Batched();
 
 const toShortfinBatchedGenerationRequestBody = (
   givenRequests: GenerateFromTextRequest['textToImageRequestBody'][],
@@ -56,7 +56,7 @@ const toShortfinBatchedGenerationRequestBody = (
     }
 
     return runningBatchedRequest;
-  }, cloneOf(emptyBatchedGenerationRequest));
+  }, cloneOf(emptyBatchedGenerationRequestBody));
 };
 
 const Shortfin_TextToImage_SDXL_Client_Response_Body = {
