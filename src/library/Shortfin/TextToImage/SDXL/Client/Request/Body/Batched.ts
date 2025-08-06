40ba15896d8c0dd3a2c6f83bf28c7f1ae4a43e17
Jacob Gordon <jacob.gordon@amd.com>
2025-08-06 10:25:28 -0500

    docs(library/Shortfin): clarifies context of members within batched request body (#634)
    - Although the `FloatingPoint` and `UnsignedInteger` type aliases do carry some context, they don't get surfaced via intellisense because they both resolve to a primitive `number`
    - A potential follow-up to this could be implementing true `FloatingPoint` and `UnsignedInteger` subclasses of `Number`
    
    Follows-up on #631

--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
@@ -1,21 +1,38 @@
-type UnsignedInteger = number;
-type FloatingPoint = number;
-
 interface Shortfin_TextToImage_SDXL_Client_Request_Body_Batched {
   /** The text that will be fed to the model. */
   prompt/*    */: string[];
   /** The text that will be fed to the model with an equal but opposite weight. */
   neg_prompt/**/: string[];
-  /** The vertical dimension of the output image. */
-  height/*    */: UnsignedInteger[];
-  /** The horizontal dimension of the output image. */
-  width/*     */: UnsignedInteger[];
-  /** The number of diffusion slices upon which model inference is performed. */
-  steps/*     */: UnsignedInteger[];
-  /** Configures the pipeline's classifier-free guidance scale for de-noising. */
-  guidance_scale: FloatingPoint[];
-  /** The seed for random latents generation. */
-  seed/*      */: UnsignedInteger[];
+  /**
+   * The vertical dimension of the output image.
+   *
+   * Must be an integer.
+   */
+  height/*    */: number[];
+  /**
+   * The horizontal dimension of the output image.
+   *
+   * Must be an integer.
+   */
+  width/*     */: number[];
+  /**
+   * The number of diffusion slices upon which model inference is performed.
+   *
+   * Must be an integer in the range 1 <= n <= 100.
+   */
+  steps/*     */: number[];
+  /**
+   * Configures the pipeline's classifier-free guidance scale for de-noising.
+   *
+   * Must be in the range 0 <= x <= 10.
+   */
+  guidance_scale: number[];
+  /**
+   * The seed for random latents generation.
+   *
+   * Must be a positive integer.
+   */
+  seed/*      */: number[];
 }
 
 export type {
