95a7a2e4f2430e8193336b7c3f2136e32741a15d
Jacob Gordon <jacob.gordon@amd.com>
2025-08-06 13:49:30 +0000

    style(library/Shortfin): preps relevant members for additional description in `Shortfin_TextToImage_SDXL_Client_Request_Body_Batched`

--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
@@ -6,15 +6,25 @@ interface Shortfin_TextToImage_SDXL_Client_Request_Body_Batched {
   prompt/*    */: string[];
   /** The text that will be fed to the model with an equal but opposite weight. */
   neg_prompt/**/: string[];
-  /** The vertical dimension of the output image. */
+  /**
+   * The vertical dimension of the output image.
+   */
   height/*    */: UnsignedInteger[];
-  /** The horizontal dimension of the output image. */
+  /**
+   * The horizontal dimension of the output image.
+   */
   width/*     */: UnsignedInteger[];
-  /** The number of diffusion slices upon which model inference is performed. */
+  /**
+   * The number of diffusion slices upon which model inference is performed.
+   */
   steps/*     */: UnsignedInteger[];
-  /** Configures the pipeline's classifier-free guidance scale for de-noising. */
+  /**
+   * Configures the pipeline's classifier-free guidance scale for de-noising.
+   */
   guidance_scale: FloatingPoint[];
-  /** The seed for random latents generation. */
+  /**
+   * The seed for random latents generation.
+   */
   seed/*      */: UnsignedInteger[];
 }
 
