2efe6d62fdee43f8feb7799f9cc11818eef7dbbe
Jacob Gordon <jacob.gordon@amd.com>
2025-08-06 08:59:18 -0500

    docs(library/Shortfin): describes members within batched request body (#635)
    Facilitates #634

--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
@@ -2,13 +2,20 @@ type UnsignedInteger = number;
 type FloatingPoint = number;
 
 interface Shortfin_TextToImage_SDXL_Client_Request_Body_Batched {
-  prompt: /*        */ string[];
-  neg_prompt: /*    */ string[];
-  height: /*        */ UnsignedInteger[];
-  width: /*         */ UnsignedInteger[];
-  steps: /*         */ UnsignedInteger[];
-  guidance_scale: /**/ FloatingPoint[];
-  seed: /*          */ UnsignedInteger[];
+  /** The text that will be fed to the model. */
+  prompt/*    */: string[];
+  /** The text that will be fed to the model with an equal but opposite weight. */
+  neg_prompt/**/: string[];
+  /** The vertical dimension of the output image. */
+  height/*    */: UnsignedInteger[];
+  /** The horizontal dimension of the output image. */
+  width/*     */: UnsignedInteger[];
+  /** The number of diffusion slices upon which model inference is performed. */
+  steps/*     */: UnsignedInteger[];
+  /** Configures the pipeline's classifier-free guidance scale for de-noising. */
+  guidance_scale: FloatingPoint[];
+  /** The seed for random latents generation. */
+  seed/*      */: UnsignedInteger[];
 }
 
 export type {
