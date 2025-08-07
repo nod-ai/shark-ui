f430898e8efe19ed4455bef3cc1b72f458254577
Jacob Gordon <jacob.gordon@amd.com>
2025-08-07 17:04:40 +0000

    refactor(library/Shortfin): adds true "definitionWithAugmentation"

---
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/definitionWithAugmentation.ts
@@ -0,0 +1,5 @@
+import './definitionAugmentation.ts';
+
+export type {
+  Shortfin_TextToImage_SDXL_Client_Request_Body,
+} from './definition.ts';
--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/exports.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/exports.ts
@@ -1,5 +1,3 @@
-import './definitionAugmentation.ts';
-
 export type {
   Shortfin_TextToImage_SDXL_Client_Request_Body,
-} from './definition.ts';
+} from './definitionWithAugmentation.ts';
