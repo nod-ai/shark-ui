2e9e0e9b4b0881e1e99fc1dcbf50f27431b062f1
Jacob Gordon <jacob.gordon@amd.com>
2025-08-07 15:33:51 -0500

    refactor(library/ShimmedStabilityAIClient): prefers "batched" to mean "member-wise batch" (#655)
    - pinning down the semantics helps communicate the role of each unit, making it easier to determine how they can be extracted and modularized
    - "batch request body" would mean an "a list of request bodies" which would just be "request bodies"
    - "batch**ed** request body" would mean "a request body whose members are batched", which more accurately represents the data structure

--- src/library/ShimmedStabilityAIClient/definition.ts
+++ src/library/ShimmedStabilityAIClient/definition.ts
@@ -25,12 +25,12 @@ import {
   toShortfinRequestBodyPrompt,
 } from './conversions';
 
-const emptyBatchGenerationRequest = new Shortfin.TextToImage.SDXL.Client.Request.Body.Batched();
+const emptyBatchedGenerationRequest = new Shortfin.TextToImage.SDXL.Client.Request.Body.Batched();
 
-const toShortfinBatchGenerationRequestBody = (
+const toShortfinBatchedGenerationRequestBody = (
   givenRequests: GenerateFromTextRequest['textToImageRequestBody'][],
 ): Shortfin.TextToImage.SDXL.Client.Request.Body.Batched => {
-  return givenRequests.reduce((runningBatchRequest, eachStabilityAIRequest) => {
+  return givenRequests.reduce((runningBatchedRequest, eachStabilityAIRequest) => {
     if (
       (eachStabilityAIRequest.height !== undefined)
       && (eachStabilityAIRequest.width !== undefined)
@@ -46,17 +46,17 @@ const toShortfinBatchGenerationRequestBody = (
         weight: -1,
       });
 
-      (runningBatchRequest.prompt/*    */).push(positiveTextPrompts/* */);
-      (runningBatchRequest.neg_prompt/**/).push(negativeTextPrompts/* */);
-      (runningBatchRequest.height/*    */).push(eachStabilityAIRequest.height/*  */);
-      (runningBatchRequest.width/*     */).push(eachStabilityAIRequest.width/*   */);
-      (runningBatchRequest.steps/*     */).push(eachStabilityAIRequest.steps/*   */);
-      (runningBatchRequest.guidance_scale).push(eachStabilityAIRequest.cfgScale/**/);
-      (runningBatchRequest.seed/*      */).push(eachStabilityAIRequest.seed/*    */);
+      (runningBatchedRequest.prompt/*    */).push(positiveTextPrompts/* */);
+      (runningBatchedRequest.neg_prompt/**/).push(negativeTextPrompts/* */);
+      (runningBatchedRequest.height/*    */).push(eachStabilityAIRequest.height/*  */);
+      (runningBatchedRequest.width/*     */).push(eachStabilityAIRequest.width/*   */);
+      (runningBatchedRequest.steps/*     */).push(eachStabilityAIRequest.steps/*   */);
+      (runningBatchedRequest.guidance_scale).push(eachStabilityAIRequest.cfgScale/**/);
+      (runningBatchedRequest.seed/*      */).push(eachStabilityAIRequest.seed/*    */);
     }
 
-    return runningBatchRequest;
-  }, cloneOf(emptyBatchGenerationRequest));
+    return runningBatchedRequest;
+  }, cloneOf(emptyBatchedGenerationRequest));
 };
 
 const Shortfin_TextToImage_SDXL_Client_Response_Body = {
@@ -82,12 +82,12 @@ class ImageClient
   public async forciblyGenerateFromText(
     givenRequest: GenerateFromTextRequest,
   ): Promise<GenerateFromTextResponse> {
-    const shimmedRequestBody = toShortfinBatchGenerationRequestBody([
+    const shimmedBatchedRequestBody = toShortfinBatchedGenerationRequestBody([
       givenRequest.textToImageRequestBody,
     ]);
 
     const outcomeOfSubmittingResource = await this.submitResource({
-      bySending: shimmedRequestBody,
+      bySending: shimmedBatchedRequestBody,
       to       : generationEndpoint,
     });
 
