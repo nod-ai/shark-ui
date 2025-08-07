0afc2a0dfed545aff80aefbdcbe77bf4fcaac230
Jacob Gordon <jacob.gordon@amd.com>
2025-08-07 19:49:26 +0000

    refactor(library/Shortfin): makes `Shortfin.TextToImage.SDXL.Client.Request.Body.Batched` run-time accessible

--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/definitionAugmentation.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/definitionAugmentation.ts
@@ -1,10 +1,16 @@
-import type {
+import {
   Shortfin_TextToImage_SDXL_Client_Request_Body_Batched,
 } from './Batched';
 
+import {
+  Shortfin_TextToImage_SDXL_Client_Request_Body,
+} from './definition.ts';
+
+Shortfin_TextToImage_SDXL_Client_Request_Body.Batched = Shortfin_TextToImage_SDXL_Client_Request_Body_Batched;
+
 declare module './definition.ts' {
   namespace Shortfin_TextToImage_SDXL_Client_Request_Body {
-    export type {
+    export {
       Shortfin_TextToImage_SDXL_Client_Request_Body_Batched as Batched,
     };
   }
