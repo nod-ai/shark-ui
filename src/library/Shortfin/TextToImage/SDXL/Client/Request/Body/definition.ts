cab8678026b04c8c42ad3a114a19ef4f46304a60
Jacob Gordon <jacob.gordon@amd.com>
2025-08-07 18:31:35 +0000

    refactor(library/Shortfin): makes `Shortfin.TextToImage.SDXL.Client.Request.Body` patchable

--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/definition.ts
@@ -1,40 +1,42 @@
-interface Shortfin_TextToImage_SDXL_Client_Request_Body {
-  /** The text that will be fed to the model. */
-  prompt/*    */: string;
-  /** The text that will be fed to the model with an equal but opposite weight. */
-  neg_prompt/**/: string;
-  /**
-   * The vertical dimension of the output image.
-   *
-   * Must be an integer.
-   */
-  height/*    */: number;
-  /**
-   * The horizontal dimension of the output image.
-   *
-   * Must be an integer.
-   */
-  width/*     */: number;
-  /**
-   * The number of diffusion slices upon which model inference is performed.
-   *
-   * Must be an integer in the range 1 <= n <= 100.
-   */
-  steps/*     */: number;
-  /**
-   * Configures the pipeline's classifier-free guidance scale for de-noising.
-   *
-   * Must be in the range 0 <= x <= 10.
-   */
-  guidance_scale: number;
-  /**
-   * The seed for random latents generation.
-   *
-   * Must be a positive integer.
-   */
-  seed/*      */: number;
+class Shortfin_TextToImage_SDXL_Client_Request_Body {
+  public constructor(
+    /** The text that will be fed to the model. */
+    public prompt/*    */: string,
+    /** The text that will be fed to the model with an equal but opposite weight. */
+    public neg_prompt/**/: string,
+    /**
+     * The vertical dimension of the output image.
+     *
+     * Must be an integer.
+     */
+    public height/*    */: number,
+    /**
+     * The horizontal dimension of the output image.
+     *
+     * Must be an integer.
+     */
+    public width/*     */: number,
+    /**
+     * The number of diffusion slices upon which model inference is performed.
+     *
+     * Must be an integer in the range 1 <= n <= 100.
+     */
+    public steps/*     */: number,
+    /**
+     * Configures the pipeline's classifier-free guidance scale for de-noising.
+     *
+     * Must be in the range 0 <= x <= 10.
+     */
+    public guidance_scale: number,
+    /**
+     * The seed for random latents generation.
+     *
+     * Must be a positive integer.
+     */
+    public seed/*      */: number,
+  ) {}
 }
 
-export type {
+export {
   Shortfin_TextToImage_SDXL_Client_Request_Body,
 };
