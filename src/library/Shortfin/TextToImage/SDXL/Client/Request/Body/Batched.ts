5ec385be0e730da12b9521d1abdcbe400d4e517b
Jacob Gordon <jacob.gordon@amd.com>
2025-08-07 11:04:26 -0500

    refactor(library/Shortfin): decouples `*.Response.Body` from `*.Response.Body.Batched` (#645)
    Pre-requisite for #638

---
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/Batched.ts
@@ -0,0 +1,15 @@
+import type {
+  Batched,
+} from '@/library/typeUtilities';
+
+import type {
+  Shortfin_TextToImage_SDXL_Client_Request_Body,
+} from './definition.ts';
+
+type Shortfin_TextToImage_SDXL_Client_Request_Body_Batched = Batched<
+  Shortfin_TextToImage_SDXL_Client_Request_Body
+>;
+
+export type {
+  Shortfin_TextToImage_SDXL_Client_Request_Body_Batched,
+};
--- src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Request/Body/definition.ts
@@ -1,40 +1,40 @@
-interface Shortfin_TextToImage_SDXL_Client_Request_Body_Batched {
+interface Shortfin_TextToImage_SDXL_Client_Request_Body {
   /** The text that will be fed to the model. */
-  prompt/*    */: string[];
+  prompt/*    */: string;
   /** The text that will be fed to the model with an equal but opposite weight. */
-  neg_prompt/**/: string[];
+  neg_prompt/**/: string;
   /**
    * The vertical dimension of the output image.
    *
    * Must be an integer.
    */
-  height/*    */: number[];
+  height/*    */: number;
   /**
    * The horizontal dimension of the output image.
    *
    * Must be an integer.
    */
-  width/*     */: number[];
+  width/*     */: number;
   /**
    * The number of diffusion slices upon which model inference is performed.
    *
    * Must be an integer in the range 1 <= n <= 100.
    */
-  steps/*     */: number[];
+  steps/*     */: number;
   /**
    * Configures the pipeline's classifier-free guidance scale for de-noising.
    *
    * Must be in the range 0 <= x <= 10.
    */
-  guidance_scale: number[];
+  guidance_scale: number;
   /**
    * The seed for random latents generation.
    *
    * Must be a positive integer.
    */
-  seed/*      */: number[];
+  seed/*      */: number;
 }
 
 export type {
-  Shortfin_TextToImage_SDXL_Client_Request_Body_Batched,
+  Shortfin_TextToImage_SDXL_Client_Request_Body,
 };
