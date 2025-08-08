a1fe365952841d9b210a33808a69ef3bb1b736e0
Jacob Gordon <jacob.gordon@amd.com>
2025-08-08 15:09:41 +0000

    checkpoint: preps for pipe under `derivedBatchedRequestBody ` within `toShortfinBatchedRequestBody`

--- src/library/ShimmedStabilityAIClient/definition.ts
+++ src/library/ShimmedStabilityAIClient/definition.ts
@@ -26,15 +26,16 @@ const toShortfinBatchedRequestBody = (
 ): Shortfin.TextToImage.SDXL.Client.Request.Body.Batched => {
   const emptyBatchedRequestBody = new Shortfin.TextToImage.SDXL.Client.Request.Body.Batched();
 
-  const derivedBatchedRequestBody = givenRequestBodies.reduce((runningBatchedRequestBody, eachStabilityAIRequestBody) => {
-    const eachShortfinRequestBody = toShortfinRequestBody(eachStabilityAIRequestBody);
+  const derivedBatchedRequestBody = givenRequestBodies
+    .reduce((runningBatchedRequestBody, eachStabilityAIRequestBody) => {
+      const eachShortfinRequestBody = toShortfinRequestBody(eachStabilityAIRequestBody);
 
-    if (
-      eachShortfinRequestBody !== null
-    ) runningBatchedRequestBody.append(eachShortfinRequestBody);
+      if (
+        eachShortfinRequestBody !== null
+      ) runningBatchedRequestBody.append(eachShortfinRequestBody);
 
-    return runningBatchedRequestBody;
-  }, emptyBatchedRequestBody);
+      return runningBatchedRequestBody;
+    }, emptyBatchedRequestBody);
 
   return derivedBatchedRequestBody;
 };
