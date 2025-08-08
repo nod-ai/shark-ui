5e5ae1c195dfca65d949be51418cf305c4970449
Jacob Gordon <jacob.gordon@amd.com>
2025-08-08 20:30:47 +0000

    checkpoint: highlights reuse of reference

--- src/library/ShimmedStabilityAIClient/definition.ts
+++ src/library/ShimmedStabilityAIClient/definition.ts
@@ -30,13 +30,13 @@ const toShortfinBatchedRequestBody = (
 
   const emptyBatchedRequestBody = new Shortfin.TextToImage.SDXL.Client.Request.Body.Batched();
 
-  const derivedBatchedRequestBody = derivedShortfinRequestBodies
+  const alsoDerivedBatchedRequestBody = derivedShortfinRequestBodies
     .reduce((runningBatchedRequestBody, eachShortfinRequestBody) => {
       runningBatchedRequestBody.append(eachShortfinRequestBody);
       return runningBatchedRequestBody;
     }, emptyBatchedRequestBody);
 
-  return derivedBatchedRequestBody;
+  return alsoDerivedBatchedRequestBody;
 };
 
 const Shortfin_TextToImage_SDXL_Client_Response_Body = {
