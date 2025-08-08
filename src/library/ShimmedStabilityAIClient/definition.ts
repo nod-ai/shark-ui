7a2ea3e70aab561a649d54613a24c742fb3c678f
Jacob Gordon <jacob.gordon@amd.com>
2025-08-08 15:01:33 +0000

    style(library/ShimmedStabilityAIClient): preps control flow to become guard

--- src/library/ShimmedStabilityAIClient/definition.ts
+++ src/library/ShimmedStabilityAIClient/definition.ts
@@ -29,7 +29,9 @@ const toShortfinBatchedRequestBody = (
   return givenRequestBodies.reduce((runningBatchedRequestBody, eachStabilityAIRequestBody) => {
     const eachShortfinRequestBody = toShortfinRequestBody(eachStabilityAIRequestBody);
 
-    if (eachShortfinRequestBody !== null) {
+    if (
+      eachShortfinRequestBody !== null
+    ) {
       (runningBatchedRequestBody.prompt/*    */).push(eachShortfinRequestBody.prompt/*    */);
       (runningBatchedRequestBody.neg_prompt/**/).push(eachShortfinRequestBody.neg_prompt/**/);
       (runningBatchedRequestBody.height/*    */).push(eachShortfinRequestBody.height/*    */);
