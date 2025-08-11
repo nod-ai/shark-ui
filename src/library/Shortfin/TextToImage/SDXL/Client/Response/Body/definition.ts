76031052add8fe86d04297025a571b6783bae4e5
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 19:04:13 +0000

    checkpoint: adds guard for success

--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
@@ -5,6 +5,11 @@ const Shortfin_TextToImage_SDXL_Client_Response_Body = {
   SchemaMember: {
     image: Schema.string().transform((someSubject) => {
       const outcomeOfParsingSubject = Base64CharacterEncodedByteSequence.parsedFrom(someSubject);
+
+      if (
+        outcomeOfParsingSubject.isSuccess
+      ) return outcomeOfParsingSubject.unwrapped;
+
       return outcomeOfParsingSubject.forciblyUnwrap(/* TODO: safely adapt errors so it can propagate to encompassing schemas */);
     }),
     get images() {
