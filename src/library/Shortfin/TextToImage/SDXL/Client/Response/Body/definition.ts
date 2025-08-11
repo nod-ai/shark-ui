09bc1e87f8981e204fecfb7600f38d1a21928e31
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 14:21:53 +0000

    refactor(library/Shortfin): converts `*.Client.Response.Body` to class

--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
@@ -1,7 +1,8 @@
 import Schema from '@/library/Schema';
 
-const Shortfin_TextToImage_SDXL_Client_Response_Body = {
-  Schema: Schema.object({
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+class Shortfin_TextToImage_SDXL_Client_Response_Body {
+  public static Schema = Schema.object({
     images: Schema
       .tuple([
         Schema.base64CharacterEncodedByteSequence(),
@@ -9,8 +10,8 @@ const Shortfin_TextToImage_SDXL_Client_Response_Body = {
       .rest(
         Schema.base64CharacterEncodedByteSequence(),
       ),
-  }),
-};
+  });
+}
 
 export {
   Shortfin_TextToImage_SDXL_Client_Response_Body,
