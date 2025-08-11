573292b59abdd04fd41021f6583f7374794c61fb
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 21:32:19 +0000

    style: prefers multi-line piping for `Schema` calls

--- src/library/WebAPI/Server_Schema.ts
+++ src/library/WebAPI/Server_Schema.ts
@@ -3,9 +3,11 @@ import Schema from '@/library/Schema';
 import Server from './Server';
 
 /** Defines how to parse into an instance of {@link Server} */
-const Server_Schema = Schema.object({
-  origin: Schema.string(),
-}).transform($0 => Server.from($0));
+const Server_Schema = Schema
+  .object({
+    origin: Schema.string(),
+  })
+  .transform($0 => Server.from($0));
 
 export {
   Server_Schema,
