1c54bbd65dbb140f0d5846e62aed6666a86f0f8e
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 17:17:11 +0000

    checkpoint: avoids invariable getter for `SchemaMember`

--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
@@ -2,15 +2,13 @@ import Schema from '@/library/Schema';
 
 const Shortfin_TextToImage_SDXL_Client_Response_Body = {
   SchemaMember: {
-    get images() {
-      return Schema
-        .tuple([
-          Schema.base64CharacterEncodedByteSequence(),
-        ])
-        .rest(
-          Schema.base64CharacterEncodedByteSequence(),
-        );
-    },
+    images: Schema
+      .tuple([
+        Schema.base64CharacterEncodedByteSequence(),
+      ])
+      .rest(
+        Schema.base64CharacterEncodedByteSequence(),
+      ),
   },
   get Schema() {
     return Schema.object({
