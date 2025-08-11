60334e8c9a9ff72933625b6a0bd71c4eb4f8eb7c
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 17:37:48 +0000

    checkpoint: preps `*.Client.Response.Body.Schema` for piping

--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
@@ -2,15 +2,16 @@ import Schema from '@/library/Schema';
 
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 class Shortfin_TextToImage_SDXL_Client_Response_Body {
-  public static Schema = Schema.object({
-    images: Schema
-      .tuple([
-        Schema.base64CharacterEncodedByteSequence(),
-      ])
-      .rest(
-        Schema.base64CharacterEncodedByteSequence(),
-      ),
-  });
+  public static Schema = Schema
+    .object({
+      images: Schema
+        .tuple([
+          Schema.base64CharacterEncodedByteSequence(),
+        ])
+        .rest(
+          Schema.base64CharacterEncodedByteSequence(),
+        ),
+    });
 }
 
 export {
