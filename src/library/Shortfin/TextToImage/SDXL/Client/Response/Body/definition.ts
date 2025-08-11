9a182c5286a1b13f8b5670b88be661e2b68198e1
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 14:46:52 -0500

    fix(library/Shortfin): ensures ` *.Client.Response.Body` is opaque in intellisense (#693)
    - before, the type signature would show up as the object literal no matter where it was used in the codebase:
       ```typescript
       {
         images: [Base64CharacterEncodedByteSequence, ...Base64CharacterEncodedByteSequence[]]
       }
       ```
      - this wasn't very semantically valuable
    - now, it shows up as a member of the namespace
       ```typescript
       Shortfin.TextToImage.SDXL.Client.Response.Body
       ```

--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
@@ -1,16 +1,28 @@
+import type Base64CharacterEncodedByteSequence from '@/library/Base64CharacterEncodedByteSequence';
 import Schema from '@/library/Schema';
 
-const Shortfin_TextToImage_SDXL_Client_Response_Body = {
-  Schema: Schema.object({
-    images: Schema
-      .tuple([
-        Schema.base64CharacterEncodedByteSequence(),
-      ])
-      .rest(
-        Schema.base64CharacterEncodedByteSequence(),
-      ),
-  }),
-};
+class Shortfin_TextToImage_SDXL_Client_Response_Body {
+  public constructor(
+    public images: [
+      Base64CharacterEncodedByteSequence,
+      ...Base64CharacterEncodedByteSequence[],
+    ],
+  ) {}
+
+  public static Schema = Schema
+    .object({
+      images: Schema
+        .tuple([
+          Schema.base64CharacterEncodedByteSequence(),
+        ])
+        .rest(
+          Schema.base64CharacterEncodedByteSequence(),
+        ),
+    })
+    .transform($0 => new Shortfin_TextToImage_SDXL_Client_Response_Body(
+      $0.images,
+    ));
+}
 
 export {
   Shortfin_TextToImage_SDXL_Client_Response_Body,
