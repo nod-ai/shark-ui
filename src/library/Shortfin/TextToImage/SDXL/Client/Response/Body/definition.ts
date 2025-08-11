a0f6f837c1f3375aca1b194660eee8fc6b2f1a39
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 19:02:01 +0000

    checkpoint: replaces rational for forcible unwrap with "TODO"

--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
@@ -4,7 +4,7 @@ import Schema from '@/library/Schema';
 const Shortfin_TextToImage_SDXL_Client_Response_Body = {
   SchemaMember: {
     image: Schema.string().transform((someSubject) => {
-      return Base64CharacterEncodedByteSequence.parsedFrom(someSubject).forciblyUnwrap(/* Zod can safely propagate errors */);
+      return Base64CharacterEncodedByteSequence.parsedFrom(someSubject).forciblyUnwrap(/* TODO: safely adapt errors so it can propagate to encompassing schemas */);
     }),
     get images() {
       return Schema.tuple([this.image]).rest(this.image);
