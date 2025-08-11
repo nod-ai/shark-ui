10443958b86120a80e0d534d02753ba539c70a8c
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 16:09:13 +0000

    refactor: prefers using `Schema.base64CharacterEncodedByteSequence(...)` where applicable

--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.ts
@@ -1,22 +1,8 @@
-import Base64CharacterEncodedByteSequence from '@/library/Base64CharacterEncodedByteSequence';
 import Schema from '@/library/Schema';
 
 const Shortfin_TextToImage_SDXL_Client_Response_Body = {
   SchemaMember: {
-    image: Schema.string().transform((someSubject, currentContext) => {
-      const outcomeOfParsingSubject = Base64CharacterEncodedByteSequence.parsedFrom(someSubject);
-
-      if (
-        outcomeOfParsingSubject.isSuccess
-      ) return outcomeOfParsingSubject.unwrapped;
-
-      currentContext.addIssue({
-        code   : 'custom',
-        message: outcomeOfParsingSubject.cause.message,
-      });
-
-      return Schema.NEVER;
-    }),
+    image: Schema.base64CharacterEncodedByteSequence(),
     get images() {
       return Schema.tuple([this.image]).rest(this.image);
     },
