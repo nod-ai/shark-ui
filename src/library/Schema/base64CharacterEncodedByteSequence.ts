d8b1886b28fe4079e5f21b21c14b964d4dfefc0a
Jacob Gordon <jacob.gordon@amd.com>
2025-08-11 14:25:55 -0500

    refactor: centralizes definition of schema for `Base64CharacterEncodedByteSequence` (#690)
    Follows up on #632

---
+++ src/library/Schema/base64CharacterEncodedByteSequence.ts
@@ -0,0 +1,26 @@
+import Base64CharacterEncodedByteSequence from '@/library/Base64CharacterEncodedByteSequence';
+
+import {
+  string as Schema_string,
+  NEVER as Schema_NEVER,
+} from './core';
+
+/** An alternative to `Schema.base64()` that avoids using the deprecated `atob` conversion under the hood */
+const Schema_base64CharacterEncodedByteSequence = () => Schema_string().transform((someSubject, currentContext) => {
+  const outcomeOfParsingSubject = Base64CharacterEncodedByteSequence.parsedFrom(someSubject);
+
+  if (
+    outcomeOfParsingSubject.isSuccess
+  ) return outcomeOfParsingSubject.unwrapped;
+
+  currentContext.addIssue({
+    code   : 'custom',
+    message: outcomeOfParsingSubject.cause.message,
+  });
+
+  return Schema_NEVER;
+});
+
+export {
+  Schema_base64CharacterEncodedByteSequence,
+};
