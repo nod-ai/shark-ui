2b228ba17e5ee4c9b7a66295e37f0ec36e5c583c
Jacob Gordon <jacob.gordon@amd.com>
2025-08-13 20:21:16 +0000

    prefeature(library/Shortfin): adds generation method to `Shortfin.TextToImage.SDXL.Client`

--- src/library/Shortfin/TextToImage/SDXL/Client/definition.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/definition.ts
@@ -1,7 +1,44 @@
+import Attempt from '@/library/Attempt';
 import HTTP from '@/library/HTTP';
+
+import {
+  URLPath,
+} from '@/library/URLComponent';
+
+import type {
+  Shortfin_TextToImage_SDXL_Client_Request,
+} from './Request';
+
+import {
+  Shortfin_TextToImage_SDXL_Client_Response,
+} from './Response';
 
 class Shortfin_TextToImage_SDXL_Client
   extends HTTP.Client {
+  public generateImageFrom(
+    givenBatchedRequestBody: Shortfin_TextToImage_SDXL_Client_Request.Body.Batched,
+  ): Promise<Shortfin_TextToImage_SDXL_Client_Request.Outcome> {
+    const generationEndpoint = URLPath.parsedFrom('/generate').forciblyUnwrap();
+
+    return Attempt.thatEventually(async (ends) => {
+      const outcomeOfSubmittingResource = await this.submitResource({
+        bySending: givenBatchedRequestBody,
+        to       : generationEndpoint,
+      });
+
+      if (
+        outcomeOfSubmittingResource.isFailure
+      ) return outcomeOfSubmittingResource;
+
+      const rawResource = outcomeOfSubmittingResource.unwrapped;
+
+      const parsedResource = Shortfin_TextToImage_SDXL_Client_Response.Body.parsedFrom(rawResource)
+        .forciblyUnwrap(/* Implementation must align with established contract. */);
+
+      const [soleGeneratedImage] = parsedResource.images;
+      return ends.inSuccessWith(soleGeneratedImage);
+    });
+  }
 }
 
 export {
