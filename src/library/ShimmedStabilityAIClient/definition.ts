36e24b87ae56c9b7fbd4901d3d9af1aa7efab9b6
Jacob Gordon <jacob.gordon@amd.com>
2025-08-14 13:16:18 +0000

    checkpoint: renames "Client_" to "ShimmedStabilityAIClient_"

--- src/library/ShimmedStabilityAIClient/definition.ts
+++ src/library/ShimmedStabilityAIClient/definition.ts
@@ -16,7 +16,7 @@ import {
 
 import toShortfin from './toShortfin';
 
-class Client_Version1_Image
+class ShimmedStabilityAIClient_Version1_Image
   extends HTTP.Client {
   public async forciblyGenerateFromText(
     givenRequest: GenerateFromTextRequest,
@@ -47,12 +47,12 @@ class Client_Version1_Image
   }
 }
 
-class Client_Version1
+class ShimmedStabilityAIClient_Version1
   extends HTTP.Client {
-  private cachedClient?: Client_Version1_Image;
+  private cachedClient?: ShimmedStabilityAIClient_Version1_Image;
 
-  public get image(): Client_Version1_Image {
-    this.cachedClient ??= new Client_Version1_Image(this.origin, this.headers);
+  public get image(): ShimmedStabilityAIClient_Version1_Image {
+    this.cachedClient ??= new ShimmedStabilityAIClient_Version1_Image(this.origin, this.headers);
     return this.cachedClient;
   }
 }
@@ -74,10 +74,10 @@ class ShimmedStabilityAIClient
     );
   }
 
-  private cachedClient?: Client_Version1;
+  private cachedClient?: ShimmedStabilityAIClient_Version1;
 
-  public get version1(): Client_Version1 {
-    this.cachedClient ??= new Client_Version1(this.origin, this.headers);
+  public get version1(): ShimmedStabilityAIClient_Version1 {
+    this.cachedClient ??= new ShimmedStabilityAIClient_Version1(this.origin, this.headers);
     return this.cachedClient;
   }
 }
