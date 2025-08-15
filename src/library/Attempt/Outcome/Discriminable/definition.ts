968c807b0802b5c46a14fedac3aefab9b4aa3d9c
Jacob Gordon <jacob.gordon@amd.com>
2025-08-15 14:17:22 +0000

    refactor(library/Attempt): extracts "/SyntacticallySugarfree.ts" from "/definition.ts" in "/Outcome/Discriminable"

--- src/library/Attempt/Outcome/Discriminable/definition.ts
+++ src/library/Attempt/Outcome/Discriminable/definition.ts
@@ -12,11 +12,9 @@ import type {
   Attempt_Outcome_Discriminant,
 } from '../Discriminant';
 
-interface Attempt_Outcome_Discriminable_SyntacticallySugarfree<
-  SomeDiscriminant extends Attempt_Outcome_Discriminant,
-> {
-  readonly discriminant: SomeDiscriminant;
-}
+import type {
+  Attempt_Outcome_Discriminable_SyntacticallySugarfree,
+} from './SyntacticallySugarfree';
 
 interface Attempt_Outcome_Discriminable<
   SomeDiscriminant extends Attempt_Outcome_Discriminant,
