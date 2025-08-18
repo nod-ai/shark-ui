164b71c9d285831ce08aa41d5ca5d34ab8e7484f
Jacob Gordon <jacob.gordon@amd.com>
2025-08-18 17:23:47 +0000

    refactor(library/Base64): extracts `Base64_bitWidth`

---
+++ src/library/Base64/bitWidth.ts
@@ -0,0 +1,9 @@
+import {
+  default as Base64_Alphabet,
+} from './Alphabet';
+
+const Base64_bitWidth = Math.log2(Base64_Alphabet.length);
+
+export {
+  Base64_bitWidth,
+};
--- src/library/Base64/exports.ts
+++ src/library/Base64/exports.ts
@@ -1,6 +1,8 @@
 import Base64_Alphabet from './Alphabet';
 
-const Base64_bitWidth = Math.log2(Base64_Alphabet.length);
+import {
+  Base64_bitWidth,
+} from './bitWidth';
 
 export {
   Base64_Alphabet as Alphabet,
