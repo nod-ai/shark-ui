f18151d36a287c69d14311fce5b43c8d85574fa1
Jacob Gordon <jacob.gordon@amd.com>
2025-08-19 15:32:30 +0000

    refactor(library/Base64): adds "definition" in terms of submodules

---
+++ src/library/Base64/definition.ts
@@ -0,0 +1,14 @@
+import Base64_Alphabet from './Alphabet';
+
+import {
+  Base64_bitWidth,
+} from './bitWidth';
+
+const Base64 = {
+  Alphabet: Base64_Alphabet,
+  bitWidth: Base64_bitWidth,
+};
+
+export {
+  Base64,
+};
