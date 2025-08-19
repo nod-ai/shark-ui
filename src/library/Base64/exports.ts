cfed7a3a2c6c9890065e4f45b39a2adfd73e5c49
Jacob Gordon <jacob.gordon@amd.com>
2025-08-19 14:47:37 +0000

    checkpoint(library/Base64): removes cross export of `Sequence`

--- src/library/Base64/exports.ts
+++ src/library/Base64/exports.ts
@@ -1,5 +1,3 @@
-import * as Base64_CharacterSequence from '../Sequence/Base64';
-
 import Base64_Alphabet from './Alphabet';
 
 const Base64_bitWidth = Math.log2(Base64_Alphabet.length);
@@ -7,5 +5,4 @@ const Base64_bitWidth = Math.log2(Base64_Alphabet.length);
 export {
   Base64_Alphabet as Alphabet,
   Base64_bitWidth as bitWidth,
-  Base64_CharacterSequence as CharacterSequence,
 };
