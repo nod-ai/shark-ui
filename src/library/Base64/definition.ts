c3a85ec097c5f66d3640feb97f9d92fb3fa629d2
Jacob Gordon <jacob.gordon@amd.com>
2025-08-19 15:37:29 +0000

    refactor(library/Base64): prefers using `this` in `Base64.bitWidth` getter

--- src/library/Base64/definition.ts
+++ src/library/Base64/definition.ts
@@ -3,7 +3,7 @@ import Base64_Alphabet from './Alphabet';
 const Base64 = {
   Alphabet: Base64_Alphabet,
   get bitWidth() {
-    return Math.log2(Base64_Alphabet.length);
+    return Math.log2(this.Alphabet.length);
   },
 };
 
