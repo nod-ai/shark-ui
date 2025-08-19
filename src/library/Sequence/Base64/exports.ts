30e3965da4015d74939b8733b59d4d2b0b2c185a
Jacob Gordon <jacob.gordon@amd.com>
2025-08-19 19:15:47 +0000

    checkpoint(library/Sequence): ensures "Base64_" as level 2 namespace

--- src/library/Sequence/Base64/exports.ts
+++ src/library/Sequence/Base64/exports.ts
@@ -3,19 +3,19 @@ import Base64_Alphabet from '@/library/Base64/Alphabet';
 
 import Sequence_Base64_Character_ConformanceError from './ConformanceError';
 
-const Sequence_Character_pattern = new RegExp(`^[${Base64_Alphabet.pattern.source}]+$`);
+const Sequence_Base64_Character_pattern = new RegExp(`^[${Base64_Alphabet.pattern.source}]+$`);
 
-const Sequence_ensureConformanceOf = (
+const Sequence_Base64_ensureConformanceOf = (
   givenCharacterSequence: string,
 ): Attempt.Outcome<string, Sequence_Base64_Character_ConformanceError> => Attempt.that((ends) => {
   if (
-    !Sequence_Character_pattern.test(givenCharacterSequence)
+    !Sequence_Base64_Character_pattern.test(givenCharacterSequence)
   ) return ends.inFailureDueTo(new Sequence_Base64_Character_ConformanceError());
 
   return ends.inSuccessWith(givenCharacterSequence);
 });
 
 export {
-  Sequence_ensureConformanceOf as ensureConformanceOf,
+  Sequence_Base64_ensureConformanceOf as ensureConformanceOf,
   Sequence_Base64_Character_ConformanceError as ConformanceError,
 };
