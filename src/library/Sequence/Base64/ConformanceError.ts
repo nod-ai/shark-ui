1dd04e9feaf2c984af5649c95e8b77cab6676fd8
Jacob Gordon <jacob.gordon@amd.com>
2025-08-19 14:54:36 +0000

    checkpoint(library/Sequence): removes "Character_" as a nested namespace

--- src/library/Sequence/Base64/ConformanceError.ts
+++ src/library/Sequence/Base64/ConformanceError.ts
@@ -1,11 +1,11 @@
 import Attempt from '@/library/Attempt';
 import Base64_Alphabet from '@/library/Base64/Alphabet';
 
-class Sequence_Base64_Character_ConformanceError
+class Sequence_Base64_ConformanceError
   extends Attempt.ActionableError<
-  'Sequence_Base64_Character_ConformanceError'
+  'Sequence_Base64_ConformanceError'
 > {
-  public override name = 'Sequence_Base64_Character_ConformanceError' as const;
+  public override name = 'Sequence_Base64_ConformanceError' as const;
 
   public constructor() {
     super(`Sequence contained 1+ character(s) outside of the Base64 Alphabet: ${Base64_Alphabet.pattern.toString()}`);
@@ -13,5 +13,5 @@ class Sequence_Base64_Character_ConformanceError
 }
 
 export {
-  Sequence_Base64_Character_ConformanceError as default,
+  Sequence_Base64_ConformanceError as default,
 };
--- src/library/Sequence/Base64/exports.ts
+++ src/library/Sequence/Base64/exports.ts
@@ -1,21 +1,21 @@
 import Attempt from '@/library/Attempt';
 import Base64_Alphabet from '@/library/Base64/Alphabet';
 
-import Sequence_Base64_Character_ConformanceError from './ConformanceError';
+import Sequence_Base64_ConformanceError from './ConformanceError';
 
-const Sequence_Base64_Character_pattern = new RegExp(`^[${Base64_Alphabet.pattern.source}]+$`);
+const Sequence_Base64_pattern = new RegExp(`^[${Base64_Alphabet.pattern.source}]+$`);
 
 const Sequence_Base64_ensureConformanceOf = (
   givenCharacterSequence: string,
-): Attempt.Outcome<string, Sequence_Base64_Character_ConformanceError> => Attempt.that((ends) => {
+): Attempt.Outcome<string, Sequence_Base64_ConformanceError> => Attempt.that((ends) => {
   if (
-    !Sequence_Base64_Character_pattern.test(givenCharacterSequence)
-  ) return ends.inFailureDueTo(new Sequence_Base64_Character_ConformanceError());
+    !Sequence_Base64_pattern.test(givenCharacterSequence)
+  ) return ends.inFailureDueTo(new Sequence_Base64_ConformanceError());
 
   return ends.inSuccessWith(givenCharacterSequence);
 });
 
 export {
   Sequence_Base64_ensureConformanceOf as ensureConformanceOf,
-  Sequence_Base64_Character_ConformanceError as ConformanceError,
+  Sequence_Base64_ConformanceError as ConformanceError,
 };
