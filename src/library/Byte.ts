3fcd29e4515300d858190602c2d94f15bad5e0c8
Jacob Gordon <jacob.gordon@amd.com>
2025-08-19 13:21:04 +0000

    checkpoint(library/Byte): converts `Byte_cofactorTo` to named function

--- src/library/Byte.ts
+++ src/library/Byte.ts
@@ -4,12 +4,12 @@ import {
 
 const Byte_bitWidth = 8 as const;
 
-const Byte_cofactorTo = (givenBitWidth: number): number => {
+function Byte_cofactorTo(givenBitWidth: number): number {
   return cofactor({
     to        : givenBitWidth,
     forLCMWith: Byte_bitWidth,
   });
-};
+}
 
 export {
   Byte_cofactorTo,
