1693e949a97bbeacaee4da1385363a580172caa4
Jacob Gordon <jacob.gordon@amd.com>
2025-08-20 18:31:22 +0000

    refactor: ensures top-level prefixes in "library/WebAPI"

--- src/features/TextToImage/Server/exports.ts
+++ src/features/TextToImage/Server/exports.ts
@@ -1,7 +1,7 @@
 import Attempt from '@/library/Attempt';
 
 import {
-  Server,
+  Server as WebAPI_Server,
 } from '@/library/WebAPI';
 
 import {
@@ -14,20 +14,20 @@ import TextToImage_Server_SpecificationError from './ServerSpecificationError';
 
 const environmentKeyForOriginOfTextToImageServer = 'VITE__TEXT_TO_IMAGE__API__SERVER__ORIGIN';
 
-const textToImageServerAccordingToEnvironment = ((): Server | null => {
+const textToImageServerAccordingToEnvironment = ((): WebAPI_Server | null => {
   const originAccordingToEnvironment = import.meta.env[environmentKeyForOriginOfTextToImageServer];
 
   if (
     originAccordingToEnvironment === undefined
   ) return null;
 
-  return Server.from({
+  return WebAPI_Server.from({
     origin: originAccordingToEnvironment,
   });
 })();
 
 const retrieveCurrentTextToImageServer = (): Promise<
-  Attempt.Outcome<Server, TextToImage_Server_SpecificationError>
+  Attempt.Outcome<WebAPI_Server, TextToImage_Server_SpecificationError>
 > => Attempt.thatEventually(async (ends) => {
   if (
     textToImageServerAccordingToEnvironment !== null
