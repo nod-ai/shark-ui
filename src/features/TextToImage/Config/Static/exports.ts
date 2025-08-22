542802368bf92d9e67357914bd1f01cf2bcd75eb
Jacob Gordon <jacob.gordon@amd.com>
2025-08-22 21:39:43 +0000

    refactor(text-to-image): ensures "Reading" declarations include level 4 namespaces in "TextToImage/Config/Static"

--- src/features/TextToImage/Config/Static/exports.ts
+++ src/features/TextToImage/Config/Static/exports.ts
@@ -8,21 +8,21 @@ import {
   TextToImage_Config,
 } from '../definition.ts';
 
-import TextToImage_Config_Static_ReadingError from './StaticConfigReadingError';
+import TextToImage_Config_Static_Reading_Error from './StaticConfigReadingError';
 
 const TextToImage_Config_Static_file = URLComponent_Path.parsedFrom('/config/text-to-image.json').forciblyUnwrap();
 
-type TextToImage_Config_Static_OutcomeOfReading = Attempt.Outcome<
+type TextToImage_Config_Static_Reading_Outcome = Attempt.Outcome<
   TextToImage_Config,
-  TextToImage_Config_Static_ReadingError
+  TextToImage_Config_Static_Reading_Error
 >;
 
-const TextToImage_Config_Static_read = (): Promise<TextToImage_Config_Static_OutcomeOfReading> => Attempt.thatEventually(async (ends) => {
+const TextToImage_Config_Static_read = (): Promise<TextToImage_Config_Static_Reading_Outcome> => Attempt.thatEventually(async (ends) => {
   const fileResponse = await fetch(TextToImage_Config_Static_file.toString());
 
   if (
     !fileResponse.ok
-  ) return ends.inFailureDueTo(new TextToImage_Config_Static_ReadingError(TextToImage_Config_Static_file, fileResponse));
+  ) return ends.inFailureDueTo(new TextToImage_Config_Static_Reading_Error(TextToImage_Config_Static_file, fileResponse));
 
   const rawConfig = await fileResponse.json() as unknown;
   const parsedConfig = TextToImage_Config.parsedFrom(rawConfig).forciblyUnwrap(/* Implementation must align with established contract. */);
@@ -32,5 +32,5 @@ const TextToImage_Config_Static_read = (): Promise<TextToImage_Config_Static_Out
 export {
   TextToImage_Config_Static_file as file,
   TextToImage_Config_Static_read as read,
-  TextToImage_Config_Static_ReadingError as ReadingError,
+  TextToImage_Config_Static_Reading_Error as Reading_Error,
 };
