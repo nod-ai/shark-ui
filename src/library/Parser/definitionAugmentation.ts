457d60b7f1befc1aa7482ff51fe9024727828fce
Jacob Gordon <jacob.gordon@amd.com>
2025-08-22 16:58:48 +0000

    checkpoint: avoids exposing `Parser.String_Static`

--- src/library/Parser/definitionAugmentation.ts
+++ src/library/Parser/definitionAugmentation.ts
@@ -3,10 +3,6 @@ import type {
 } from './Static';
 
 import type {
-  Parser_String_Static,
-} from './StaticString';
-
-import type {
   Parser_String,
 } from './String';
 
@@ -14,7 +10,6 @@ declare module './definition.ts' {
   namespace Parser {
     export type {
       Parser_Static as Static,
-      Parser_String_Static as String_Static,
       Parser_String as String,
     };
   }
