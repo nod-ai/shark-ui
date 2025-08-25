eccd7a493398cded315d32648de6b1e9b28dc29a
Jacob Gordon <jacob.gordon@amd.com>
2025-08-25 16:08:44 +0000

    checkpoint: moves "*/StaticConfigReadingError.ts" to "*/Reading/Error.ts"

--- src/features/TextToImage/Config/Static/Reading/exports.ts
+++ src/features/TextToImage/Config/Static/Reading/exports.ts
@@ -1,3 +1,3 @@
 export {
   default as Error,
-} from '../StaticConfigReadingError';
+} from './Error';
--- src/features/TextToImage/Config/Static/exports.ts
+++ src/features/TextToImage/Config/Static/exports.ts
@@ -12,7 +12,7 @@ import {
   TextToImage_Config_Static_Reading,
 } from './Reading';
 
-import type TextToImage_Config_Static_Reading_Error from './StaticConfigReadingError';
+import type TextToImage_Config_Static_Reading_Error from './Reading/Error';
 
 const TextToImage_Config_Static_file = URLComponent_Path.parsedFrom('/config/text-to-image.json').forciblyUnwrap();
 
