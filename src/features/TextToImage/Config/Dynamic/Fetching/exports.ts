429d583075ae14895081fcf60a83105da2aed54a
Jacob Gordon <jacob.gordon@amd.com>
2025-08-25 18:02:22 +0000

    checkpoint: removes `Error_*` aliases from ".../Fetching/exports.ts"

--- src/features/TextToImage/Config/Dynamic/Fetching/exports.ts
+++ src/features/TextToImage/Config/Dynamic/Fetching/exports.ts
@@ -1,11 +1,3 @@
 export {
   TextToImage_Config_Dynamic_Fetching_Error as Error,
 } from './Error';
-
-export {
-  default as Error_Request,
-} from './Error/Request';
-
-export {
-  default as Error_Response,
-} from './Error/Response';
