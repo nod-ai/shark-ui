e142631575497f47b41d978a9d2d7ed64ba7ec75
Jacob Gordon <jacob.gordon@amd.com>
2025-08-25 16:06:17 +0000

    checkpoint: switches non-peer internal consumers from `TextToImage_Config_Static_Reading_*` to `TextToImage_Config_Static_Reading.*`

--- src/features/TextToImage/Config/Static/exports.ts
+++ src/features/TextToImage/Config/Static/exports.ts
@@ -26,7 +26,7 @@ const TextToImage_Config_Static_read = (): Promise<TextToImage_Config_Static_Rea
 
   if (
     !fileResponse.ok
-  ) return ends.inFailureDueTo(new TextToImage_Config_Static_Reading_Error(TextToImage_Config_Static_file, fileResponse));
+  ) return ends.inFailureDueTo(new TextToImage_Config_Static_Reading.Error(TextToImage_Config_Static_file, fileResponse));
 
   const rawConfig = await fileResponse.json() as unknown;
   const parsedConfig = TextToImage_Config.parsedFrom(rawConfig).forciblyUnwrap(/* Implementation must align with established contract. */);
