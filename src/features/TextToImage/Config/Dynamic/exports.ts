24b861ed3b65567e1c27dcb7471aecafc8bca689
Jacob Gordon <jacob.gordon@amd.com>
2025-08-25 15:36:55 +0000

    checkpoint: switches non-peer internal consumers from `TextToImage_Config_Dynamic_Fetching_*` to `TextToImage_Config_Dynamic_Fetching.*`

--- src/features/TextToImage/Config/Dynamic/exports.ts
+++ src/features/TextToImage/Config/Dynamic/exports.ts
@@ -26,13 +26,13 @@ type TextToImage_Config_Dynamic_Fetching_Outcome = Attempt.Outcome<TextToImage_C
 
 const TextToImage_Config_Dynamic_fetch = (): Promise<TextToImage_Config_Dynamic_Fetching_Outcome> => Attempt.thatEventually(async (ends) => {
   const endpointResponse = await fetch(TextToImage_Config_Dynamic_endpoint.toString());
-  const fetchingError = new TextToImage_Config_Dynamic_Fetching_Error(TextToImage_Config_Dynamic_endpoint);
+  const fetchingError = new TextToImage_Config_Dynamic_Fetching.Error(TextToImage_Config_Dynamic_endpoint);
 
   if (
     !endpointResponse.ok
   ) return ends.inFailureDueTo(fetchingError);
 
-  const endpointResponseError = new TextToImage_Config_Dynamic_Fetching_ResponseError({
+  const endpointResponseError = new TextToImage_Config_Dynamic_Fetching.ResponseError({
     endpoint: TextToImage_Config_Dynamic_endpoint,
     response: endpointResponse,
   });
