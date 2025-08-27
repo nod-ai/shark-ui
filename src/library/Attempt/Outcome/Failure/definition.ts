3f3c2ba4a852a4abfff4243ec98f04f73ce628c5
Jacob Gordon <jacob.gordon@amd.com>
2025-08-27 15:09:51 +0000

    refactor(library/Attempt): extracts "/definition.ts" from "/dueTo.ts" in "/Outcome/Failure"

---
+++ src/library/Attempt/Outcome/Failure/definition.ts
@@ -0,0 +1,49 @@
+import type {
+  Attempt_Error_Actionable,
+} from '../../Error';
+
+import type {
+  Attempt_Outcome_Failure_SemanticallySugarfree,
+} from './SemanticallySugarfree';
+
+import type {
+  Attempt_Outcome_Failure_Transformer,
+} from './Transformer';
+
+interface Attempt_Outcome_Failure<
+  SomeActionableError extends Attempt_Error_Actionable<string>,
+> extends Attempt_Outcome_Failure_SemanticallySugarfree<
+    SomeActionableError
+  > {
+  /**
+   * Semantic sugar for `cause`; useful for juxtaposition against early exits:
+   * ```ts
+   * function safelyGetProductWhileHandlingErrors(
+   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
+   *   recoverFrom: (expectedError: CustomError) => void,
+   * ): CustomProduct {
+   *   if (
+   *     givenOutcome.isSuccess
+   *   ) return givenOutcome.unwrapped;
+   *
+   *   ...
+   *
+   *   recoverFrom(givenOutcome.causeOfFailure);
+   * }
+   * ```
+   */
+  readonly causeOfFailure: this['cause'];
+
+  rewrappedWith<
+    SomeTransformedActionableError extends Attempt_Error_Actionable<string> = SomeActionableError,
+  >(
+    given?: Attempt_Outcome_Failure_Transformer<
+      SomeActionableError,
+      SomeTransformedActionableError
+    >
+  ): Attempt_Outcome_Failure<SomeTransformedActionableError>;
+}
+
+export type {
+  Attempt_Outcome_Failure,
+};
--- src/library/Attempt/Outcome/Failure/dueTo.ts
+++ src/library/Attempt/Outcome/Failure/dueTo.ts
@@ -2,48 +2,14 @@ import type {
   Attempt_Error_Actionable,
 } from '../../Error';
 
-import type {
-  Attempt_Outcome_Failure_SemanticallySugarfree,
-} from './SemanticallySugarfree';
-
 import {
   type Attempt_Outcome_Failure_Transformer,
   Attempt_Outcome_Failure_Cause_Transformer_identity,
 } from './Transformer';
 
-interface Attempt_Outcome_Failure<
-  SomeActionableError extends Attempt_Error_Actionable<string>,
-> extends Attempt_Outcome_Failure_SemanticallySugarfree<
-    SomeActionableError
-  > {
-  /**
-   * Semantic sugar for `cause`; useful for juxtaposition against early exits:
-   * ```ts
-   * function safelyGetProductWhileHandlingErrors(
-   *   givenOutcome: Attempt.Outcome<CustomProduct, CustomError>,
-   *   recoverFrom: (expectedError: CustomError) => void,
-   * ): CustomProduct {
-   *   if (
-   *     givenOutcome.isSuccess
-   *   ) return givenOutcome.unwrapped;
-   *
-   *   ...
-   *
-   *   recoverFrom(givenOutcome.causeOfFailure);
-   * }
-   * ```
-   */
-  readonly causeOfFailure: this['cause'];
-
-  rewrappedWith<
-    SomeTransformedActionableError extends Attempt_Error_Actionable<string> = SomeActionableError,
-  >(
-    given?: Attempt_Outcome_Failure_Transformer<
-      SomeActionableError,
-      SomeTransformedActionableError
-    >
-  ): Attempt_Outcome_Failure<SomeTransformedActionableError>;
-}
+import type {
+  Attempt_Outcome_Failure,
+} from './definition.ts';
 
 const Attempt_Outcome_Failure_dueTo = <
   SomeActionableError extends Attempt_Error_Actionable<string>,
