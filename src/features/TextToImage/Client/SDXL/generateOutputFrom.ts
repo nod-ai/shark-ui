3e779d41dc38b212a93be4acc8d95420bd7f8272
Jacob Gordon <jacob.gordon@amd.com>
2025-08-28 14:46:56 +0000

    refactor(text-to-image): adds missing barrel files to "/toSharkUIOutput" module

--- src/features/TextToImage/Client/SDXL/generateOutputFrom.ts
+++ src/features/TextToImage/Client/SDXL/generateOutputFrom.ts
@@ -14,7 +14,7 @@ import * as TextToImage_Server from '../../Server';
 
 import {
   toSharkUIOutput_first,
-} from './toSharkUIOutput/first';
+} from './toSharkUIOutput';
 
 const TextToImage_Client_SDXL_initialize = async (): Promise<
   Attempt.Outcome<
