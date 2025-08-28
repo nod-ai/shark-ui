fe4e33a8a9c999c0e78eb5f75bc622bd356555e4
Jacob Gordon <jacob.gordon@amd.com>
2025-08-28 22:27:18 +0000

    refactor(library/ShimmedStabilityAIClient): aligns file structure with "SDXL" namespace

--- src/pages/TextToImagePage.vue
+++ src/pages/TextToImagePage.vue
@@ -26,7 +26,7 @@ import {
   VSkeletonLoader,
 } from 'vuetify/components/VSkeletonLoader';
 
-import SDXL_DiffusionStepCount from '@/library/ShimmedStabilityAIClient/models/SDXLDiffusionStepCount.ts';
+import SDXL_DiffusionStepCount from '@/library/ShimmedStabilityAIClient/SDXL/DiffusionStepCount';
 
 import DiscreteSlider from '@/components/DiscreteSlider.vue';
 import NavigationPanel from '@/components/NavigationPanel.vue';
