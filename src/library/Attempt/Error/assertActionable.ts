8e9a0a4f8b088d5773d126ab6a75b653ec261bea
Jacob Gordon <jacob.gordon@amd.com>
2025-08-28 16:21:31 +0000

    refactor(library/Attempt): moves "/Error/NonActionableBuiltInError.ts" to "/Error/NonActionableBuiltInError/definition.ts"

--- src/library/Attempt/Error/assertActionable.ts
+++ src/library/Attempt/Error/assertActionable.ts
@@ -8,7 +8,7 @@ import {
   default as Attempt_Error_NonActionable,
 } from './NonActionable';
 
-import NonActionableBuiltInError from './NonActionableBuiltInError';
+import NonActionableBuiltInError from './NonActionableBuiltInError/definition.ts';
 
 import {
   type AppropriatelyThrown,
