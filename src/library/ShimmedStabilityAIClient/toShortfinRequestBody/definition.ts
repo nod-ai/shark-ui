91ca6599358fa2abe6aab1629cd7d6f5e33012ea
Jacob Gordon <jacob.gordon@amd.com>
2025-08-28 20:55:49 +0000

    refactor(library/ShimmedStabilityAIClient): converts `toShortfinRequestBody` to a function declaration

--- src/library/ShimmedStabilityAIClient/toShortfinRequestBody/definition.ts
+++ src/library/ShimmedStabilityAIClient/toShortfinRequestBody/definition.ts
@@ -8,9 +8,9 @@ import {
   toShortfinRequestBody_Prompt,
 } from './Prompt';
 
-const toShortfinRequestBody = (
+function toShortfinRequestBody(
   givenRequestBody: GenerateFromTextRequest['textToImageRequestBody'],
-): Shortfin.TextToImage.SDXL.Client.Request.Body | null => {
+): Shortfin.TextToImage.SDXL.Client.Request.Body | null {
   if (
     (givenRequestBody.height === undefined)
     || (givenRequestBody.width === undefined)
@@ -34,7 +34,7 @@ const toShortfinRequestBody = (
   };
 
   return derivedShortfinRequestBody;
-};
+}
 
 export {
   toShortfinRequestBody,
