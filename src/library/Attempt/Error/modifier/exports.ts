58c97a4e6008b13c0a30e1ae86b596f05bab35ea
Jacob Gordon <jacob.gordon@amd.com>
2025-08-28 17:38:14 +0000

    refactor(library/Attempt): exposes relevant "/Error/assertions" through "/Error/modifier"

--- src/library/Attempt/Error/modifier/exports.ts
+++ src/library/Attempt/Error/modifier/exports.ts
@@ -2,6 +2,14 @@ export type {
   default as AppropriatelyThrown,
 } from './AppropriatelyThrown';
 
+export {
+  assertAppropriatelyThrown,
+} from '../assertions/assertAppropriatelyThrown';
+
 export type {
   default as PotentiallyActionable,
 } from './PotentiallyActionable';
+
+export {
+  assertPotentiallyActionable,
+} from '../assertions/assertPotentiallyActionable';
--- src/library/Attempt/Error/modifier/index.ts
+++ src/library/Attempt/Error/modifier/index.ts
@@ -1,1 +1,1 @@
-export type * from './exports.ts';
+export * from './exports.ts';
