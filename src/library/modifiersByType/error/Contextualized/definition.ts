7a69e5d80c6ca63c96d47734dddc2e5622b34e86
Jacob Gordon <jacob.gordon@amd.com>
2025-09-02 20:36:45 +0000

    refactor(library/modifiersByType): extracts "/cast.ts" from "/definition.ts" in "/error/Contextualized"

--- src/library/modifiersByType/error/Contextualized/definition.ts
+++ src/library/modifiersByType/error/Contextualized/definition.ts
@@ -1,10 +1,10 @@
-import type {
-  Instantiable,
-} from '@/library/typeUtilities';
-
 import {
   Contextualized_assume,
 } from './assume';
+
+import {
+  Contextualized_cast,
+} from './cast';
 
 import {
   Contextualized_describes,
@@ -19,25 +19,6 @@ type Contextualized<
     cause: SomeCause;
   }
 ;
-
-const Contextualized_cast = <
-  SomeError extends Error,
-  SomeCause extends Error,
->(
-  givenError: SomeError,
-  givenFallbackMessage: string,
-  GivenCause: Instantiable<SomeCause> | ErrorConstructor = Error,
-): Contextualized<SomeError, SomeCause> | Contextualized<Error, SomeError> => {
-  if (
-    Contextualized_describes<SomeError, SomeCause>(givenError, GivenCause)
-  ) return givenError;
-
-  const contextualizedError = new Error(givenFallbackMessage, {
-    cause: givenError,
-  });
-
-  return Contextualized_assume<Error, SomeError>(contextualizedError, Error);
-};
 
 /** Utilities for identifying and casting `Error` instances as "contextualized" */
 const Contextualized = {
