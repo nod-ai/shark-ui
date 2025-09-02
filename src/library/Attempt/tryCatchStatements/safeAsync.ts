b7f35cf485d210997d8d48fd7fa75d06d7d10e07
Jacob Gordon <jacob.gordon@amd.com>
2025-09-02 20:02:00 +0000

    refactor(library/Attempt): extracts "/safe.ts" from "/safeAsync.ts" in "/tryCatchStatements"

--- src/library/Attempt/tryCatchStatements/safeAsync.ts
+++ src/library/Attempt/tryCatchStatements/safeAsync.ts
@@ -1,34 +1,10 @@
-import {
-  asError,
-} from '@/library/utilitiesByType/error';
-
-import {
-  type AppropriatelyThrown,
-  AppropriatelyThrown_assume,
+import type {
+  AppropriatelyThrown,
 } from '../Error';
 
-const safe = <
-  SomeTryBlockOutput,
-  SomeCatchBlockOutput,
->(
-  {
-    try: getTryBlockOutput,
-    catch: catchBlockOutputFor,
-  }: {
-    try: () => SomeTryBlockOutput;
-    catch: ($0: AppropriatelyThrown<Error>) => SomeCatchBlockOutput;
-  },
-): SomeTryBlockOutput | SomeCatchBlockOutput => {
-  // eslint-disable-next-line no-restricted-syntax -- this is the implementation designed to help avoid use of raw try/catch
-  try {
-    return getTryBlockOutput();
-  }
-  catch (whateverThatWasThrown) {
-    const someError = asError(whateverThatWasThrown);
-    const someAppropriatelyThrownError = AppropriatelyThrown_assume(someError);
-    return catchBlockOutputFor(someAppropriatelyThrownError);
-  }
-};
+import {
+  safe,
+} from './safe';
 
 const safeAsync = async <
   SomeOutputOfResolvedPromise,
