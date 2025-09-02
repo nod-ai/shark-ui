8311078ca8e507951cfca799b0f5ce19fa63162c
Jacob Gordon <jacob.gordon@amd.com>
2025-09-02 17:22:12 +0000

    refactor(library/Attempt): extracts "/Cause" from "/Transformer.ts" in "/Outcome/Failure"

---
+++ src/library/Attempt/Outcome/Failure/Cause/Transformer/identity.ts
@@ -0,0 +1,24 @@
+import type {
+  Attempt_Error_Actionable,
+} from '@/library/Attempt/Error'; // eslint-disable-line import/no-internal-modules -- avoids lengthy relative paths
+
+type Attempt_Outcome_Failure_Cause_Transformer<
+  SomeTransformableActionableError extends Attempt_Error_Actionable<string>,
+  SomeTransformedActionableError extends Attempt_Error_Actionable<string>,
+> = (
+  transformableCause: SomeTransformableActionableError,
+) => SomeTransformedActionableError;
+
+const Attempt_Outcome_Failure_Cause_Transformer_identity = <
+  SomeTransformableActionableError extends Attempt_Error_Actionable<string>,
+  SomeTransformedActionableError extends Attempt_Error_Actionable<string>,
+>(
+  transformableCause: NoInfer<SomeTransformableActionableError>,
+): NoInfer<SomeTransformedActionableError> => {
+  return transformableCause as unknown as SomeTransformedActionableError;
+};
+
+export {
+  type Attempt_Outcome_Failure_Cause_Transformer,
+  /**/ Attempt_Outcome_Failure_Cause_Transformer_identity,
+};
--- src/library/Attempt/Outcome/Failure/Transformer.ts
+++ src/library/Attempt/Outcome/Failure/Transformer.ts
@@ -2,21 +2,10 @@ import type {
   Attempt_Error_Actionable,
 } from '../../Error';
 
-type Attempt_Outcome_Failure_Cause_Transformer<
-  SomeTransformableActionableError extends Attempt_Error_Actionable<string>,
-  SomeTransformedActionableError extends Attempt_Error_Actionable<string>,
-> = (
-  transformableCause: SomeTransformableActionableError,
-) => SomeTransformedActionableError;
-
-const Attempt_Outcome_Failure_Cause_Transformer_identity = <
-  SomeTransformableActionableError extends Attempt_Error_Actionable<string>,
-  SomeTransformedActionableError extends Attempt_Error_Actionable<string>,
->(
-  transformableCause: NoInfer<SomeTransformableActionableError>,
-): NoInfer<SomeTransformedActionableError> => {
-  return transformableCause as unknown as SomeTransformedActionableError;
-};
+import {
+  type Attempt_Outcome_Failure_Cause_Transformer,
+  /**/ Attempt_Outcome_Failure_Cause_Transformer_identity,
+} from './Cause';
 
 interface Attempt_Outcome_Failure_Transformer<
   SomeTransformableActionableError extends Attempt_Error_Actionable<string>,
