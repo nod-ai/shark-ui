dcad8dfe9592024b9a8b4eedf4d07c978e97838f
Jacob Gordon <jacob.gordon@amd.com>
2025-09-02 21:40:25 +0000

    refactor(utilities/Repository): inlines `Repository_Issue_Category` within "Issue/definition.ts"

--- src/utilities/Repository/Issue/definition.ts
+++ src/utilities/Repository/Issue/definition.ts
@@ -2,18 +2,16 @@ import type {
   Repository_Issue_Label,
 } from './Label';
 
-type Repository_Issue_Category =
-  | 'Feature'
-  | 'Bug'
-  | 'Task'
-;
-
 interface Repository_Issue {
   title: string;
   body: string;
   labels: Repository_Issue_Label[];
   /** a.k.a. "type" */
-  category: Repository_Issue_Category;
+  category:
+    | 'Feature'
+    | 'Bug'
+    | 'Task'
+  ;
 }
 
 export type {
