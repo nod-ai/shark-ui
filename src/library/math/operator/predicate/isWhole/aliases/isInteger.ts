c4ca4b575f0b5f68e3207d7696b0e9d26a88bb46
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 13:29:41 -0500

    refactor(library/math): extracts "/definition.ts" from "/aliases" in "/operator/predicate/isWhole" (#868)

--- src/library/math/operator/predicate/isWhole/aliases/isInteger.ts
+++ src/library/math/operator/predicate/isWhole/aliases/isInteger.ts
@@ -1,18 +1,6 @@
-import Attempt from '@/library/Attempt';
-
 import {
-  isFinite,
-} from '../../isFinite';
-
-const isWhole = (
-  givenOperand: number,
-): boolean => {
-  if (
-    !isFinite(givenOperand)
-  ) return Attempt.abandon('Operand must be finite.');
-
-  return Number.isInteger(givenOperand);
-};
+  isWhole,
+} from '../definition.ts';
 
 /** Alias for {@link isWhole} */
 const isInteger = isWhole;
---
+++ src/library/math/operator/predicate/isWhole/definition.ts
@@ -0,0 +1,19 @@
+import Attempt from '@/library/Attempt';
+
+import {
+  isFinite,
+} from '../isFinite';
+
+const isWhole = (
+  givenOperand: number,
+): boolean => {
+  if (
+    !isFinite(givenOperand)
+  ) return Attempt.abandon('Operand must be finite.');
+
+  return Number.isInteger(givenOperand);
+};
+
+export {
+  isWhole,
+};
