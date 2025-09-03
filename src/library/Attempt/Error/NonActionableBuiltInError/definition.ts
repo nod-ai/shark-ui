7859c19c862cf6e443f25e7c7bc7cdcb089a39e5
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 16:34:24 -0500

    refactor(library/Attempt): extracts sibling units from "NonActionableBuiltInError/definition.ts" (#907)
    * refactor(library/Attempt): extracts "/globalAugmentations.ts" from "/definition.ts" in "/Error/NonActionableBuiltInError"
    
    * refactor(library/Attempt): extracts "/describes.ts" from "/definition.ts" in "/Error/NonActionableBuiltInError"

--- src/library/Attempt/Error/NonActionableBuiltInError/definition.ts
+++ src/library/Attempt/Error/NonActionableBuiltInError/definition.ts
@@ -1,51 +1,8 @@
-import type {
-  Branded,
-} from '@/library/typeUtilities';
+import {
+  NonActionableBuiltInError_describes,
+} from './describes';
 
-// These augmentations allow TypeScript to distinguish these error types from `Error` at the annotation level.
-declare global {
-  interface ReferenceError
-    extends Branded<
-      'ReferenceError'
-    > {
-    readonly brand: this['name'];
-  }
-
-  interface TypeError
-    extends Branded<
-      'TypeError'
-    > {
-    readonly brand: this['name'];
-  }
-
-  interface RangeError
-    extends Branded<
-      'RangeError'
-    > {
-    readonly brand: this['name'];
-  }
-
-  interface URIError
-    extends Branded<
-      'URIError'
-    > {
-    readonly brand: this['name'];
-  }
-
-  interface EvalError
-    extends Branded<
-      'EvalError'
-    > {
-    readonly brand: this['name'];
-  }
-
-  interface SyntaxError
-    extends Branded<
-      'SyntaxError'
-    > {
-    readonly brand: this['name'];
-  }
-}
+import './globalAugmentations';
 
 /**
  * Error classes whose only plausible cause is a programmer bug or a violated language/runtime invariant.
@@ -60,17 +17,6 @@ type NonActionableBuiltInError =
   | EvalError // Something illegal was done with `eval` or `Function` constructor.
   | SyntaxError // The JS engine couldn't even parse the code.
 ;
-
-const NonActionableBuiltInError_describes = (
-  givenError: Error,
-): givenError is NonActionableBuiltInError => (
-  (givenError instanceof ReferenceError)
-  || (givenError instanceof TypeError)
-  || (givenError instanceof RangeError)
-  || (givenError instanceof URIError)
-  || (givenError instanceof EvalError)
-  || (givenError instanceof SyntaxError)
-);
 
 const NonActionableBuiltInError = {
   describes: NonActionableBuiltInError_describes,
