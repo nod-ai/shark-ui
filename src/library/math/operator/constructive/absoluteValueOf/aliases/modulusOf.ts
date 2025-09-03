428089f968a69629eb98858e5ac51c0f4b7465db
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 13:25:31 -0500

    refactor(library/math): extracts "/definition.ts" from "/aliases" in "/operator/constructive/absoluteValueOf" (#867)

--- src/library/math/operator/constructive/absoluteValueOf/aliases/modulusOf.ts
+++ src/library/math/operator/constructive/absoluteValueOf/aliases/modulusOf.ts
@@ -1,19 +1,6 @@
-import Attempt from '@/library/Attempt';
-
 import {
-  isOperable,
-} from '../../../predicate';
-
-/** The distance the given value is from the 1-dimensional origin */
-const absoluteValueOf = (
-  givenOperand: number,
-): number => {
-  if (
-    !isOperable(givenOperand)
-  ) return Attempt.abandon('Cannot determine distance of inoperable number from origin because it does not exist on the number line.');
-
-  return Math.abs(givenOperand);
-};
+  absoluteValueOf,
+} from '../definition.ts';
 
 /** Alias for {@link absoluteValueOf} */
 const unsigned = absoluteValueOf;
