e55dcb9e8b5d635082e9ad155b5c52e5b18ead89
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 16:27:20 -0500

    refactor(text-to-image): extracts "/plural.ts" from "/first.ts" in "/Client/SDXL/toSharkUIOutput" (#905)

--- src/features/TextToImage/Client/SDXL/toSharkUIOutput/first.ts
+++ src/features/TextToImage/Client/SDXL/toSharkUIOutput/first.ts
@@ -8,44 +8,14 @@ import {
   hasAtLeastOne,
 } from '@/library/utilitiesByType/array';
 
-import {
-  type Input as TextToImage_Pipeline_Input,
-  type Output as TextToImage_Pipeline_Output,
-  Output_Nullable_from as TextToImage_Pipeline_Output_Nullable_from,
+import type {
+  Input as TextToImage_Pipeline_Input,
+  Output as TextToImage_Pipeline_Output,
 } from '@/features/TextToImage/Pipeline'; // eslint-disable-line import/no-internal-modules -- more concise than relative import
 
 import {
-  toSharkUIOutput_Image,
-  toSharkUIOutput_Image_Description_all,
-} from './Image';
-
-const toSharkUIOutput_plural = (
-  {
-    in: givenResponse,
-    inferredFrom: givenInputText,
-  }: {
-    in: GenerateFromTextResponse;
-    inferredFrom: TextToImage_Pipeline_Input['text'];
-  },
-): (TextToImage_Pipeline_Output | null)[] | null => {
-  if (
-    !('artifacts' in givenResponse.result)
-  ) return Attempt.abandon('Expected response body rather than readable stream');
-
-  const inferredRawImages = givenResponse.result.artifacts;
-
-  if (
-    inferredRawImages === undefined
-  ) return null;
-
-  const inferredOutputs = inferredRawImages
-    .map($0 => toSharkUIOutput_Image($0, {
-      description: toSharkUIOutput_Image_Description_all(givenInputText),
-    }))
-    .map($0 => TextToImage_Pipeline_Output_Nullable_from($0));
-
-  return inferredOutputs;
-};
+  toSharkUIOutput_plural,
+} from './plural';
 
 const toSharkUIOutput_first = (
   {
