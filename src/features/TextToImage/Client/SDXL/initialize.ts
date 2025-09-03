107f8970a50522975c99f3a25b51c85d9ac84caf
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 16:12:34 -0500

    refactor(text-to-image): extracts "/initialize.ts" from "/generateOutputFrom.ts" in "/Client/SDXL" (#901)

---
+++ src/features/TextToImage/Client/SDXL/initialize.ts
@@ -0,0 +1,25 @@
+import Attempt from '@/library/Attempt';
+import ShimmedStabilityAIClient from '@/library/ShimmedStabilityAIClient';
+
+import * as TextToImage_Server from '../../Server';
+
+const TextToImage_Client_SDXL_initialize = async (): Promise<
+  Attempt.Outcome<
+    ShimmedStabilityAIClient,
+    TextToImage_Server.Error.Specification
+  >
+> => {
+  const outcomeOfRetrievingCurrentServer = await TextToImage_Server.Current_retrieve();
+
+  const outcomeOfInitializingClient = Attempt.Outcome.fromRewrapping(outcomeOfRetrievingCurrentServer, {
+    product: textToImageServer => new ShimmedStabilityAIClient({
+      serverURL: textToImageServer.origin,
+    }),
+  });
+
+  return outcomeOfInitializingClient;
+};
+
+export {
+  TextToImage_Client_SDXL_initialize,
+};
