4d1907383659fe226b96897dd44344880f702859
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 13:33:57 -0500

    refactor(library/math): extracts "/definition.ts" from "/abbreviations" in "/operator/constructive/greatestCommonDivisor" (#869)

--- src/library/math/operator/constructive/greatestCommonDivisor/abbreviations/gcf.ts
+++ src/library/math/operator/constructive/greatestCommonDivisor/abbreviations/gcf.ts
@@ -1,37 +1,6 @@
-import Attempt from '@/library/Attempt';
-
 import {
-  isWhole,
-} from '../../../predicate';
-
-import {
-  absoluteValueOf,
-} from '../../absoluteValueOf';
-
-const greatestCommonDivisor = (
-  leftHandOperand: number,
-  rightHandOperand: number,
-): number => {
-  if (
-    !isWhole(leftHandOperand)
-  ) return Attempt.abandon('Left operand must be whole.');
-
-  if (
-    !isWhole(rightHandOperand)
-  ) return Attempt.abandon('Right operand must be whole.');
-
-  const leftHandMagnitude = absoluteValueOf(leftHandOperand);
-  const rightHandMagnitude = absoluteValueOf(rightHandOperand);
-
-  if (
-    rightHandMagnitude > leftHandMagnitude
-  ) return greatestCommonDivisor(rightHandMagnitude, leftHandMagnitude);
-  else if (
-    rightHandMagnitude !== 0
-  ) return greatestCommonDivisor(rightHandMagnitude, leftHandMagnitude % rightHandMagnitude);
-
-  return leftHandMagnitude;
-};
+  greatestCommonDivisor,
+} from '../definition.ts';
 
 /** Alias for {@link greatestCommonDivisor} */
 const greatestCommonFactor = greatestCommonDivisor;
