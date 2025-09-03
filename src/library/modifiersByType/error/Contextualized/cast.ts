9943a0ed934f88b5c39810c2cff7e8320e478d73
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 14:38:28 -0500

    refactor(library/modifiersByType): extracts sibling units from "/error/contextualized/definition.ts" (#883)
    * refactor(library/modifiersByType): extracts "/describes.ts" from "/definition.ts" in "/error/Contextualized"
    
    * refactor(library/modifiersByType): extracts "/assume.ts" from "/definition.ts" in "/error/Contextualized"
    
    * refactor(library/modifiersByType): extracts "/cast.ts" from "/definition.ts" in "/error/Contextualized"

---
+++ src/library/modifiersByType/error/Contextualized/cast.ts
@@ -0,0 +1,36 @@
+import type {
+  Instantiable,
+} from '@/library/typeUtilities';
+
+import {
+  Contextualized_assume,
+} from './assume';
+
+import type Contextualized from './definition.ts';
+
+import {
+  Contextualized_describes,
+} from './describes';
+
+const Contextualized_cast = <
+  SomeError extends Error,
+  SomeCause extends Error,
+>(
+  givenError: SomeError,
+  givenFallbackMessage: string,
+  GivenCause: Instantiable<SomeCause> | ErrorConstructor = Error,
+): Contextualized<SomeError, SomeCause> | Contextualized<Error, SomeError> => {
+  if (
+    Contextualized_describes<SomeError, SomeCause>(givenError, GivenCause)
+  ) return givenError;
+
+  const contextualizedError = new Error(givenFallbackMessage, {
+    cause: givenError,
+  });
+
+  return Contextualized_assume<Error, SomeError>(contextualizedError, Error);
+};
+
+export {
+  Contextualized_cast,
+};
--- src/library/modifiersByType/error/Contextualized/definition.ts
+++ src/library/modifiersByType/error/Contextualized/definition.ts
@@ -1,8 +1,14 @@
-import Attempt from '@/library/Attempt';
+import {
+  Contextualized_assume,
+} from './assume';
 
-import type {
-  Instantiable,
-} from '@/library/typeUtilities';
+import {
+  Contextualized_cast,
+} from './cast';
+
+import {
+  Contextualized_describes,
+} from './describes';
 
 type Contextualized<
   SomeError extends Error,
@@ -13,49 +19,6 @@ type Contextualized<
     cause: SomeCause;
   }
 ;
-
-const Contextualized_describes = <
-  SomeError extends Error,
-  SomeCause extends Error,
->(
-  givenError: SomeError,
-  GivenCause: Instantiable<SomeCause> | ErrorConstructor = Error,
-): givenError is Contextualized<SomeError, SomeCause> => {
-  return givenError.cause instanceof GivenCause;
-};
-
-const Contextualized_assume = <
-  SomeError extends Error,
-  SomeCause extends Error,
->(
-  givenError: SomeError,
-  GivenCause: Instantiable<SomeCause> | ErrorConstructor = Error,
-): Contextualized<SomeError, SomeCause> => {
-  if (
-    Contextualized_describes<SomeError, SomeCause>(givenError, GivenCause)
-  ) return givenError;
-
-  return Attempt.abandon('Expected error to have a cause');
-};
-
-const Contextualized_cast = <
-  SomeError extends Error,
-  SomeCause extends Error,
->(
-  givenError: SomeError,
-  givenFallbackMessage: string,
-  GivenCause: Instantiable<SomeCause> | ErrorConstructor = Error,
-): Contextualized<SomeError, SomeCause> | Contextualized<Error, SomeError> => {
-  if (
-    Contextualized_describes<SomeError, SomeCause>(givenError, GivenCause)
-  ) return givenError;
-
-  const contextualizedError = new Error(givenFallbackMessage, {
-    cause: givenError,
-  });
-
-  return Contextualized_assume<Error, SomeError>(contextualizedError, Error);
-};
 
 /** Utilities for identifying and casting `Error` instances as "contextualized" */
 const Contextualized = {
