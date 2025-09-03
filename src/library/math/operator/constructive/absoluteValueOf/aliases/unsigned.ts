c492778df3de865403663663a9e32eed56cedcb0
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 14:43:45 -0500

    refactor(library/math): extracts "/unsigned.ts" from "/modulusOf.ts" in "/operator/constructive/absoluteValueOf/aliases" (#884)

--- src/library/math/operator/constructive/absoluteValueOf/aliases/unsigned.ts
+++ src/library/math/operator/constructive/absoluteValueOf/aliases/unsigned.ts
@@ -2,15 +2,9 @@ import {
   absoluteValueOf,
 } from '../definition.ts';
 
-import {
-  unsigned,
-} from './unsigned.ts';
-
 /** Alias for {@link absoluteValueOf} */
-const modulusOf = absoluteValueOf;
+const unsigned = absoluteValueOf;
 
 export {
-  absoluteValueOf,
-  modulusOf,
   unsigned,
 };
