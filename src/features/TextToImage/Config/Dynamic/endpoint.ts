0c6ede13e9f26d7855ec85bc660ae0d9422c04dd
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 16:17:16 -0500

    refactor(text-to-image): extracts "/endpoint.ts" from "/fetch.ts" in "/Config/Dynamic" (#902)

---
+++ src/features/TextToImage/Config/Dynamic/endpoint.ts
@@ -0,0 +1,9 @@
+import {
+  URLComponent_Path,
+} from '@/library/URLComponent';
+
+const TextToImage_Config_Dynamic_endpoint = URLComponent_Path.parsedFrom('/config/text-to-image').forciblyUnwrap();
+
+export {
+  TextToImage_Config_Dynamic_endpoint,
+};
