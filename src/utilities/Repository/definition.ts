9177a1bd6484d9f81f02bfd16ea2ec546fd9041d
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 13:56:47 -0500

    refactor(utilities/Repository): extracts "/Issue" from "/definition.ts" (#874)

--- src/utilities/Repository/definition.ts
+++ src/utilities/Repository/definition.ts
@@ -1,22 +1,6 @@
-type Repository_Issue_Label =
-  | 'bug'
-  | 'enhancement'
-  | 'documentation'
-;
-
-type Repository_Issue_Category =
-  | 'Feature'
-  | 'Bug'
-  | 'Task'
-;
-
-interface Repository_Issue {
-  title: string;
-  body: string;
-  labels: Repository_Issue_Label[];
-  /** a.k.a. "type" */
-  category: Repository_Issue_Category;
-}
+import type {
+  Repository_Issue,
+} from './Issue';
 
 const Repository = {
   get emptyDraftOfNewIssue(): URL {
