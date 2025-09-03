3f14fdf252cce0976eb1c793b688266c5c7a12d6
Jacob Gordon <jacob.gordon@amd.com>
2025-09-03 15:29:07 -0500

    refactor(library/Attempt): extracts sibling units from "/Outcome/Failure/dueTo.ts" (#892)
    * refactor(library/Attempt): extracts "/SemanticallySugarfree.ts" from "/dueTo.ts" in "/Outcome/Failure"
    
    * refactor(library/Attempt): extracts "/definition.ts" from "/dueTo.ts" in "/Outcome/Failure"

---
+++ src/library/Attempt/Outcome/Failure/SemanticallySugarfree.ts
@@ -0,0 +1,20 @@
+import type {
+  Attempt_Error_Actionable,
+} from '../../Error';
+
+import type {
+  Attempt_Outcome_Discriminable,
+} from '../Discriminable';
+
+interface Attempt_Outcome_Failure_SemanticallySugarfree<
+  SomeActionableError extends Attempt_Error_Actionable<string>,
+> extends Attempt_Outcome_Discriminable<
+    'failure',
+    SomeActionableError
+  > {
+  readonly cause: SomeActionableError;
+}
+
+export type {
+  Attempt_Outcome_Failure_SemanticallySugarfree,
+};
