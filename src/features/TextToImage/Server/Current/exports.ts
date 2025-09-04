f6bb9e0e9d21aab99b5119950682b3d4ecf8cdfa
Jacob Gordon <jacob.gordon@amd.com>
2025-09-04 22:05:17 +0000

    refactor(text-to-image): avoids pibling re-exports in "/Server"

--- src/features/TextToImage/Server/Current/exports.ts
+++ src/features/TextToImage/Server/Current/exports.ts
@@ -1,8 +1,4 @@
 export {
-  TextToImage_Server_Error as Error,
-} from '../Error';
-
-export {
   TextToImage_Server_Current_accordingToEnvironment as Current_accordingToEnvironment,
 } from './accordingToEnvironment';
 
