1fbba70a0ab4ae94e036ac68bec7fb0b15cb3824
Jacob Gordon <jacob.gordon@amd.com>
2025-09-04 20:07:40 +0000

    refactor(text-to-image): avoids passthrough re-exports in "/Config/Dynamic"

--- src/features/TextToImage/Config/Dynamic/exports.ts
+++ src/features/TextToImage/Config/Dynamic/exports.ts
@@ -1,1 +1,9 @@
+export {
+  TextToImage_Config_Dynamic_endpoint as endpoint,
+} from './endpoint';
+
 export * from './fetch';
+
+export {
+  TextToImage_Config_Dynamic_Fetching as Fetching,
+} from './Fetching';
--- src/features/TextToImage/Config/Dynamic/fetch.ts
+++ src/features/TextToImage/Config/Dynamic/fetch.ts
@@ -38,7 +38,5 @@ const TextToImage_Config_Dynamic_fetch = (): Promise<
 });
 
 export {
-  TextToImage_Config_Dynamic_endpoint as endpoint,
   TextToImage_Config_Dynamic_fetch as fetch,
-  TextToImage_Config_Dynamic_Fetching as Fetching,
 };
