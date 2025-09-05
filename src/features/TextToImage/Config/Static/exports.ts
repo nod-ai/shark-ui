4a759717829d3949395abeca47ec20f5ba8d715c
Jacob Gordon <jacob.gordon@amd.com>
2025-09-05 13:32:50 +0000

    refactor(text-to-image): reveals named re-exports in "/Config/Static"

--- src/features/TextToImage/Config/Static/exports.ts
+++ src/features/TextToImage/Config/Static/exports.ts
@@ -2,7 +2,9 @@ export {
   TextToImage_Config_Static_file as file,
 } from './file';
 
-export * from './read';
+export {
+  read,
+} from './read';
 
 export {
   TextToImage_Config_Static_Reading as Reading,
