d3230ca33d78bbc0d5d71900cbefc1829e104144
Jacob Gordon <jacob.gordon@amd.com>
2025-09-09 19:48:09 +0000

    refactor(text-to-image): nests `Nullable` under `TextToImage_Pipeline_Output`

--- src/features/TextToImage/Pipeline/Output/definition.ts
+++ src/features/TextToImage/Pipeline/Output/definition.ts
@@ -2,11 +2,19 @@ import type {
   TextToImage_Pipeline_Output_Image,
 } from './Image';
 
+import {
+  TextToImage_Pipeline_Output_Nullable,
+} from './Nullable';
+
 /** The resulting information after a text-to-image model has ingested some input with some configuration */
 interface TextToImage_Pipeline_Output {
   image: TextToImage_Pipeline_Output_Image;
 }
 
-export type {
+const TextToImage_Pipeline_Output = {
+  Nullable: TextToImage_Pipeline_Output_Nullable,
+};
+
+export {
   TextToImage_Pipeline_Output,
 };
--- src/features/TextToImage/Pipeline/namespaceMembers.ts
+++ src/features/TextToImage/Pipeline/namespaceMembers.ts
@@ -2,6 +2,5 @@ export type {
   TextToImage_Pipeline_Input as Input,
 } from './Input';
 export {
-  type TextToImage_Pipeline_Output as Output,
-  /**/ TextToImage_Pipeline_Output_Nullable as Output_Nullable,
+  TextToImage_Pipeline_Output as Output,
 } from './Output';
