ac2863ba5b9abce350082a851a8cf3e0a94964f7
Jacob Gordon <jacob.gordon@amd.com>
2025-09-11 10:04:25 -0500

    linter: enforces padding between exports (#976)
    - Each export statement is an "island"; they will never express an order-sensitive procedure
    - Hence, they should be padded to _look_ like an "island"

--- src/features/TextToImage/Config/namespaceMembers.ts
+++ src/features/TextToImage/Config/namespaceMembers.ts
@@ -5,9 +5,11 @@ export {
 export {
   TextToImage_Config_Dynamic as Dynamic,
 } from './Dynamic';
+
 export {
   TextToImage_Config_Static as Static,
 } from './Static';
+
 export {
   TextToImage_Config_empty as empty,
 } from './empty';
