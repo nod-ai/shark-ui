16b9a4fc31c85d9883096adac8dc02e5f88a5b57
Jacob Gordon <jacob.gordon@amd.com>
2025-09-12 14:38:42 +0000

    refactor(utilities/Repository): renames `given` to `givenIssue`

--- src/utilities/Repository/definition.ts
+++ src/utilities/Repository/definition.ts
@@ -3,16 +3,16 @@ import GitHub from '@/library/GitHub';
 const Repository = {
   /** Creates a URL that drafts a new issue with pre-populated fields  */
   draftIssue(
-    given: GitHub.Repository.Issue,
+    givenIssue: GitHub.Repository.Issue,
   ): URL {
     const SharkUIRepository = new GitHub.Repository('nod-ai', 'shark-ui');
     const mutableDraft = new URL(SharkUIRepository.Issue.site.toString().concat('/new'));
     const referencedParameters = mutableDraft.searchParams;
 
-    referencedParameters.set('title', given.title);
-    referencedParameters.set('body', given.body);
-    referencedParameters.set('labels', given.labels.join());
-    referencedParameters.set('type', given.category);
+    referencedParameters.set('title', givenIssue.title);
+    referencedParameters.set('body', givenIssue.body);
+    referencedParameters.set('labels', givenIssue.labels.join());
+    referencedParameters.set('type', givenIssue.category);
 
     return mutableDraft;
   },
