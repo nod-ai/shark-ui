9ffe744c00204166ee10033167a3eb80b945e006
Jacob Gordon <jacob.gordon@amd.com>
2025-09-12 13:21:22 +0000

    refactor(utilities/Repository): splits GitHub site from rest of path

--- src/utilities/Repository/definition.ts
+++ src/utilities/Repository/definition.ts
@@ -4,7 +4,7 @@ import type {
 
 const Repository = {
   get emptyDraftOfNewIssue(): URL {
-    const mutableURLForNewIssue = new URL('https://github.com/nod-ai/shark-ui/issues/new');
+    const mutableURLForNewIssue = new URL(('https://github.com').concat('/nod-ai/shark-ui/issues/new'));
     return mutableURLForNewIssue;
   },
   /** Creates a URL that drafts a new issue with pre-populated fields  */
