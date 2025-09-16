ca1d339b589123ac8b5e98121703b85b2f9dee56
Jacob Gordon <jacob.gordon@amd.com>
2025-09-16 14:01:55 +0000

    refactor(text-to-image): exposes `TextToImage.Pipeline.Output.Image`

--- src/features/TextToImage/Pipeline/Output/definition.declared.augmentation.ts
+++ src/features/TextToImage/Pipeline/Output/definition.declared.augmentation.ts
@@ -1,3 +1,7 @@
+import type {
+  TextToImage_Pipeline_Output_Image,
+} from './Image';
+
 import {
   TextToImage_Pipeline_Output_Nullable,
 } from './Nullable';
@@ -11,7 +15,8 @@ TextToImage_Pipeline_Output.Nullable = TextToImage_Pipeline_Output_Nullable;
 declare module './definition.declared.ts' {
   namespace TextToImage_Pipeline_Output {
     export {
-      TextToImage_Pipeline_Output_Nullable as Nullable,
+      /**/ TextToImage_Pipeline_Output_Nullable as Nullable,
+      type TextToImage_Pipeline_Output_Image/**/as Image,
     };
   }
 }
