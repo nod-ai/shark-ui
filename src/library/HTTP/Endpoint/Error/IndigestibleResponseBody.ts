e8f6ec26bc200307a562528e56f3a22f1e379a3f
Jacob Gordon <jacob.gordon@amd.com>
2025-09-17 20:24:56 +0000

    prefeature(library/HTTP): adds `HTTP.Endpoint.Error.IndigestibleResponseBody`

---
+++ src/library/HTTP/Endpoint/Error/IndigestibleResponseBody.ts
@@ -0,0 +1,19 @@
+import Attempt from '@/library/Attempt';
+
+class HTTP_Endpoint_Error_IndigestibleResponseBody
+  extends Attempt.Error.Actionable<
+    'HTTP_Endpoint_Error_IndigestibleResponseBody'
+  > {
+  public override name = 'HTTP_Endpoint_Error_IndigestibleResponseBody' as const;
+
+  public constructor(
+    public readonly endpoint: URL,
+    givenCause: Error,
+  ) {
+    super(`Could not digest body of response from "${endpoint.toString()}". ${givenCause.message}`);
+  }
+}
+
+export {
+  HTTP_Endpoint_Error_IndigestibleResponseBody,
+};
--- src/library/HTTP/Endpoint/Error/definition.assembled.members.ts
+++ src/library/HTTP/Endpoint/Error/definition.assembled.members.ts
@@ -6,6 +6,10 @@ export {
   HTTP_Endpoint_Error_Response as Response,
 } from './Response';
 
+export {
+  HTTP_Endpoint_Error_IndigestibleResponseBody as IndigestibleResponseBody,
+} from './IndigestibleResponseBody';
+
 export type {
   HTTP_Endpoint_Error_Any as Any,
 } from './Any';
