3917d46d08d1dd3a17ec7a47058ac16ea24f020f
Jacob Gordon <jacob.gordon@amd.com>
2025-09-17 16:04:39 +0000

    prefeature(library/ContentDescriptor): adds `ContentDescriptor.json`

---
+++ src/library/ContentDescriptor/json.ts
@@ -0,0 +1,9 @@
+import {
+  ContentDescriptor,
+} from './definition.declared.ts';
+
+const ContentDescriptor_json = new ContentDescriptor('application', null, 'json', null, null);
+
+export {
+  ContentDescriptor_json,
+};
