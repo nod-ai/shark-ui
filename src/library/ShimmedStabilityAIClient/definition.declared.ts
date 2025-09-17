8f9528e2535e5be624f97a8e43c2399ae78b2693
Jacob Gordon <jacob.gordon@amd.com>
2025-09-17 10:47:39 -0500

    refactor: separates concerns of default `Shortfin` headers from `ShimmedStabilityAIClient` (#1040)
    Leverages #1039

--- src/library/ShimmedStabilityAIClient/definition.declared.ts
+++ src/library/ShimmedStabilityAIClient/definition.declared.ts
@@ -12,20 +12,15 @@ class ShimmedStabilityAIClient
   }) {
     const serverOrigin = URLComponent.Origin.parsedFrom(given.serverURL).forciblyUnwrap(/* matches error propagation of actual StabilityAI client */);
 
-    const defaultHeaders = {
-      'Content-Type': 'application/json',
-    };
-
     super(
       serverOrigin,
-      defaultHeaders,
     );
   }
 
   private cachedClient?: ShimmedStabilityAIClient_Version1;
 
   public get version1(): ShimmedStabilityAIClient_Version1 {
-    this.cachedClient ??= new ShimmedStabilityAIClient_Version1(this.origin, this.headers);
+    this.cachedClient ??= new ShimmedStabilityAIClient_Version1(this.origin);
     return this.cachedClient;
   }
 }
