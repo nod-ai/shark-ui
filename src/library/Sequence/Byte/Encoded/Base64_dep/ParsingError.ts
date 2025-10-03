c7590c272676bd046133961e7f134f9f1e337bcb
Jacob Gordon <jacob.gordon@amd.com>
2025-10-03 21:37:37 +0000

    checkpoint: removes unused `.thatEscorts(...)` method

--- src/library/Sequence/Byte/Encoded/Base64_dep/ParsingError.ts
+++ src/library/Sequence/Byte/Encoded/Base64_dep/ParsingError.ts
@@ -1,13 +1,4 @@
-import Attempt from '@/library/Attempt';
 import ParsingError from '@/library/ParsingError';
-
-import {
-  Sequence_Base64,
-} from '../../../Base64';
-
-import {
-  Sequence_Byte_Encoded_Compatibility,
-} from '../Compatibility';
 
 class Sequence_Byte_Encoded_Base64_ParsingError
   extends ParsingError<
@@ -20,30 +11,6 @@ class Sequence_Byte_Encoded_Base64_ParsingError
     givenCause?: Error,
   ) {
     super(givenMessage, givenCause);
-  }
-
-  public static thatEscorts(
-    givenError:
-      | Sequence_Byte_Encoded_Compatibility.Error
-      | Sequence_Base64.Conformance.Error,
-  ): Sequence_Byte_Encoded_Base64_ParsingError {
-    const extraContext = (() => {
-      switch (true) {
-        case givenError instanceof Sequence_Byte_Encoded_Compatibility.Error:
-          return 'Could not use given characters to encode byte sequence.';
-        case givenError instanceof Sequence_Base64.Conformance.Error:
-          return 'Could not use given characters to encode byte sequence in Base64.';
-        default: return Attempt.abandon('Unexpected error type.');
-      }
-    })();
-
-    const expandedMessage = [
-      extraContext,
-      givenError.message,
-    ].join(' ');
-
-    const escortedError = new this(expandedMessage, givenError);
-    return escortedError;
   }
 }
 
