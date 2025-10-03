5c73dc67d0c4eca28797dcb7f468d4e5146d4eb2
Jacob Gordon <jacob.gordon@amd.com>
2025-10-03 16:54:39 +0000

    refactor(library/utilitiesByType): removes "/string/isString.ts"

--- src/library/utilitiesByType/string/exports.toolbox.ts
+++ src/library/utilitiesByType/string/exports.toolbox.ts
@@ -1,3 +1,1 @@
-export * from './isString';
-
 export * from './concatenated';
--- src/library/utilitiesByType/string/isString.ts
+++
@@ -1,9 +0,0 @@
-const isString = (
-  givenSubject: unknown,
-): givenSubject is string => {
-  return (typeof givenSubject === 'string');
-};
-
-export {
-  isString,
-};
