1012ab331da42c94a9751734a299253aa55da699
Jacob Gordon <jacob.gordon@amd.com>
2025-10-07 18:56:00 +0000

    checkpoint: removes `.parsedFrom(...)`

--- src/features/TextToImage/Config/definition.declared.ts
+++ src/features/TextToImage/Config/definition.declared.ts
@@ -2,7 +2,6 @@ import {
   Schema,
 } from 'effect';
 
-import Attempt from '@/library/Attempt';
 import WebAPI from '@/library/WebAPI';
 
 /** The user-provided settings for the text-to-image feature */
@@ -11,16 +10,6 @@ class TextToImage_Config
     /** The details of the server that's providing text-to-image generation */
     server: Schema.NullOr(WebAPI.Server),
   }) {
-  private static parsedFrom(
-    givenSubject: unknown,
-  ): Attempt.Outcome<
-    TextToImage_Config,
-    Attempt.Error.Actionable<'Pass'>
-  > {
-    return Attempt.abandon('Pass', {
-      cause: givenSubject,
-    });
-  }
 }
 
 export {
--- src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.declared.ts
+++ src/library/Shortfin/TextToImage/SDXL/Client/Response/Body/definition.declared.ts
@@ -2,7 +2,6 @@ import {
   Schema,
 } from 'effect';
 
-import Attempt from '@/library/Attempt';
 import Sequence from '@/library/Sequence';
 
 class Shortfin_TextToImage_SDXL_Client_Response_Body
@@ -13,16 +12,6 @@ class Shortfin_TextToImage_SDXL_Client_Response_Body
       ),
     ),
   }) {
-  private static parsedFrom(
-    givenSubject: unknown,
-  ): Attempt.Outcome<
-    Shortfin_TextToImage_SDXL_Client_Response_Body,
-    Attempt.Error.Actionable<'Pass'>
-  > {
-    return Attempt.abandon('Pass', {
-      cause: givenSubject,
-    });
-  }
 }
 
 export {
