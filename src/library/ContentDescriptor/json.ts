08ee0ca19d8eedab3482e1a5e20416d2e3b31552
Jacob Gordon <jacob.gordon@amd.com>
2025-10-08 14:43:00 +0000

    style: ensures multi-line construction in "ContentDescriptor/json.ts"

--- src/library/ContentDescriptor/json.ts
+++ src/library/ContentDescriptor/json.ts
@@ -2,7 +2,13 @@ import {
   ContentDescriptor,
 } from './definition.declared.ts';
 
-const ContentDescriptor_json = new ContentDescriptor('application', null, 'json', null, null);
+const ContentDescriptor_json = new ContentDescriptor(
+  'application',
+  null,
+  'json',
+  null,
+  null,
+);
 
 export {
   ContentDescriptor_json,
