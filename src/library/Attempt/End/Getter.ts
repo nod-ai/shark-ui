1efcd0cea7d1cd6ab83dc206187936592ca8f3d2
Jacob Gordon <jacob.gordon@amd.com>
2025-10-08 20:28:04 +0000

    checkpoint: removes handles

--- src/library/Attempt/End/Getter.ts
+++ src/library/Attempt/End/Getter.ts
@@ -6,15 +6,9 @@ import type {
   Attempt_Outcome,
 } from '../Outcome';
 
-import type {
-  Attempt_ended as handles,
-} from '../ended';
-
 type Attempt_End_Getter<
   SomeInferredOutcome extends Attempt_Outcome<unknown, Attempt_Error.Actionable<string>>,
-> = (
-  givenHandles: typeof handles
-) => SomeInferredOutcome;
+> = () => SomeInferredOutcome;
 
 export type {
   Attempt_End_Getter,
