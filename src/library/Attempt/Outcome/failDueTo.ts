227acf74a7e24a6647054f62921f8fe5970c09ca
Jacob Gordon <jacob.gordon@amd.com>
2025-10-09 11:17:37 -0500

    refactor: avoids aliases within units (#1130)
    Overlooked when introduced in #1126

--- src/library/Attempt/Outcome/failDueTo.ts
+++ src/library/Attempt/Outcome/failDueTo.ts
@@ -2,8 +2,8 @@ import {
   Attempt_Outcome_Failure,
 } from './Failure';
 
-const Attempt_failDueTo = Attempt_Outcome_Failure.dueTo;
+const Attempt_Outcome_failDueTo = Attempt_Outcome_Failure.dueTo;
 
 export {
-  Attempt_failDueTo as Attempt_Outcome_failDueTo,
+  Attempt_Outcome_failDueTo,
 };
