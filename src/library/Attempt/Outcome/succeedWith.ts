65b60b2c05c6a78c1665a73ab99590e3bf94df28
Jacob Gordon <jacob.gordon@amd.com>
2025-10-09 13:58:13 +0000

    refactor: avoids aliases within units

--- src/library/Attempt/Outcome/succeedWith.ts
+++ src/library/Attempt/Outcome/succeedWith.ts
@@ -2,8 +2,8 @@ import {
   Attempt_Outcome_Success,
 } from './Success';
 
-const Attempt_succeedWith = Attempt_Outcome_Success.thatYielded;
+const Attempt_Outcome_succeedWith = Attempt_Outcome_Success.thatYielded;
 
 export {
-  Attempt_succeedWith as Attempt_Outcome_succeedWith,
+  Attempt_Outcome_succeedWith,
 };
