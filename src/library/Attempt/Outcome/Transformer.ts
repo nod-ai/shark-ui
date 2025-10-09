aafc957a8899211205c7b9307a2d3d9ede0b4a7a
Jacob Gordon <jacob.gordon@amd.com>
2025-10-09 15:34:17 +0000

    checkpoint: simplifies `Attempt.Outcome.Transformer`

--- src/library/Attempt/Outcome/Transformer.ts
+++ src/library/Attempt/Outcome/Transformer.ts
@@ -10,25 +10,21 @@ import type {
   Attempt_Outcome_Success,
 } from './Success';
 
-type Attempt_Outcome_Transformer<
+interface Attempt_Outcome_Transformer<
   SomeTransformableProduct,
   SomeTransformableActionableError extends Attempt_Error.Actionable<string>,
   SomeTransformedProduct,
   SomeTransformedActionableError extends Attempt_Error.Actionable<string>,
-> =
-  & {
-    product: Attempt_Outcome_Success.Product.Transformer<
-      SomeTransformableProduct,
-      SomeTransformedProduct
-    >;
-  }
-  & {
-    cause: Attempt_Outcome_Failure.Cause.Transformer<
-      SomeTransformableActionableError,
-      SomeTransformedActionableError
-    >;
-  }
-;
+> {
+  product: Attempt_Outcome_Success.Product.Transformer<
+    SomeTransformableProduct,
+    SomeTransformedProduct
+  >;
+  cause: Attempt_Outcome_Failure.Cause.Transformer<
+    SomeTransformableActionableError,
+    SomeTransformedActionableError
+  >;
+}
 
 export type {
   Attempt_Outcome_Transformer,
