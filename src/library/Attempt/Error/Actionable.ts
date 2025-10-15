1aa243af04c2d66fd39076000a434c055b590766
Jacob Gordon <jacob.gordon@amd.com>
2025-10-15 22:23:02 +0000

    checkpoint: removes unused `Attempt.Error.Actionable.from(...)`

--- src/library/Attempt/Error/Actionable.ts
+++ src/library/Attempt/Error/Actionable.ts
@@ -1,24 +1,10 @@
 import {
   Data,
-  Option,
 } from 'effect';
-
-import type {
-  Attempt_Error_Interpreter,
-} from './Interpreter';
 
 import {
   Attempt_Error_NonActionable,
 } from './NonActionable';
-
-import {
-  NonActionableBuiltInError,
-} from './NonActionableBuiltInError';
-
-import {
-  type AppropriatelyThrown,
-  PotentiallyActionable,
-} from './modifiers';
 
 /** Extend this class to describe errors from which callers ought to recover */
 abstract class Attempt_Error_Actionable
@@ -34,32 +20,6 @@ abstract class Attempt_Error_Actionable
       thrower: this.throwAnyway,
     });
   };
-
-  public static from<
-    SomeActionableError extends Attempt_Error_Actionable,
-  >(
-    givenError: AppropriatelyThrown<Error>,
-    {
-      using: interpretationOf,
-    }: {
-      using: Attempt_Error_Interpreter<SomeActionableError>;
-    },
-  ): SomeActionableError {
-    const potentiallyActionableError = PotentiallyActionable.assume(givenError);
-    const definitelyActionableError = interpretationOf(potentiallyActionableError);
-
-    return Option.getOrThrowWith(
-      definitelyActionableError,
-      () => new Error(
-        NonActionableBuiltInError.describes(givenError)
-          ? 'Neglected to prevent built-in error'
-          : 'Neglected to interpret or prevent potentially actionable error',
-        {
-          cause: potentiallyActionableError,
-        },
-      ),
-    );
-  }
 }
 
 export {
