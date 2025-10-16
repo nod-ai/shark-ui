ce889a8a26dcb0b44bfcc87c8b4ad8f3d580fcf7
Jacob Gordon <jacob.gordon@amd.com>
2025-10-16 20:01:47 +0000

    checkpoint: avoids useless subclassing

--- src/library/Attempt/Error/Tagged.ts
+++ src/library/Attempt/Error/Tagged.ts
@@ -7,10 +7,10 @@ const Attempt_Error_Tagged = <
 >(
   givenBrand: SomeBrand,
 ) => { // eslint-disable-line @typescript-eslint/explicit-function-return-type
-  return class extends Data.TaggedError(givenBrand)<{
+  return Data.TaggedError(givenBrand)<{
     message: string;
     cause?: Error;
-  }> {};
+  }>;
 };
 
 export {
