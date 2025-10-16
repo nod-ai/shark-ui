nod-ai/shark-ui

commit 4a70d0cd58bf5ad8589e2c7afde78988e955bfd7

refactor: replaces `Attempt.Either` with `effect` equivalent (#1171)
Uses precedent established in #1167

src/library/ShimmedStabilityAIClient/Version1/Image.ts
@@ -1,3 +1,7 @@
+import {
+  Effect,
+} from 'effect';
+
 import type {
   Image as StabilityAI_TextToImage_Pipeline_Output,
 } from 'stabilityai-client-typescript/models/components';
@@ -7,7 +11,6 @@ import type {
   GenerateFromTextResponse,
 } from 'stabilityai-client-typescript/models/operations';
 
-import Attempt from '@/library/Attempt';
 import HTTP from '@/library/HTTP';
 import Shortfin from '@/library/Shortfin';
 
@@ -27,7 +30,7 @@ class ShimmedStabilityAIClient_Version1_Image
     const textToImageSDXLShortfinClient = new Shortfin.TextToImage.SDXL.Client(this.origin);
 
     const exitFromGeneratingImage = await textToImageSDXLShortfinClient.generateImageFrom(derivedBatchedRequestBody);
-    const generatedImage = Attempt.Either.getOrThrow(exitFromGeneratingImage); // matches error propagation of actual StabilityAI Client
+    const generatedImage = Effect.runSync(exitFromGeneratingImage); // matches error propagation of actual StabilityAI Client
 
     const soleGeneratedArtifact: StabilityAI_TextToImage_Pipeline_Output = {
       base64      : generatedImage.toString(),

src/features/TextToImage/Server/Current/retrieve.ts
@@ -1,4 +1,5 @@
 import {
+  Effect,
   Exit,
   Option,
 } from 'effect';
@@ -32,13 +33,15 @@ const TextToImage_Server_Current_retrieve = (): Promise<
     Option.isSome(TextToImage_Server_Current_accordingToEnvironment)
   ) return Exit.succeed(Option.getOrThrow(TextToImage_Server_Current_accordingToEnvironment));
 
-  const staticConfig = Attempt.Either.getOrElse(await TextToImage_Config.Static.read(), () => TextToImage_Config.empty);
+  const exitFromReadingStaticConfig = await TextToImage_Config.Static.read();
+  const staticConfig = Effect.runSync(Effect.orElseSucceed(exitFromReadingStaticConfig, () => TextToImage_Config.empty));
 
   if (
     Option.isSome(staticConfig.server)
   ) return Exit.succeed(Option.getOrThrow(staticConfig.server));
 
-  const dynamicConfig = Attempt.Either.getOrElse(await TextToImage_Config.Dynamic.fetch(), () => TextToImage_Config.empty);
+  const exitFromFetchingDynamicConfig = await TextToImage_Config.Dynamic.fetch();
+  const dynamicConfig = Effect.runSync(Effect.orElseSucceed(exitFromFetchingDynamicConfig, () => TextToImage_Config.empty));
 
   if (
     Option.isSome(dynamicConfig.server)

src/library/Attempt/Either/definition.assembled.members.ts
@@ -1,7 +1,0 @@
-export {
-  Attempt_Either_getOrElse as getOrElse,
-} from './getOrElse';
-
-export {
-  Attempt_Either_getOrThrow as getOrThrow,
-} from './getOrThrow';

src/library/Attempt/Either/definition.assembled.ts
@@ -1,1 +1,0 @@
-export * as Attempt_Either from './definition.assembled.members.ts';

src/library/Attempt/Either/exports.object.primary.ts
@@ -1,1 +1,0 @@
-export * from './definition.assembled.ts';

src/library/Attempt/Either/getOrElse.ts
@@ -1,24 +1,0 @@
-import {
-  Exit,
-} from 'effect';
-
-import type {
-  Attempt_Error,
-} from '../Error';
-
-const Attempt_Either_getOrElse = <
-  SomeProduct,
-  SomeActionableError extends Attempt_Error.Actionable,
-  SomeFallbackProduct,
->(
-  givenExit: Exit.Exit<SomeProduct, SomeActionableError>,
-  givenFallbackGetter: () => SomeFallbackProduct,
-): SomeProduct | SomeFallbackProduct => {
-  return Exit.isSuccess(givenExit)
-    ? givenExit.value
-    : givenFallbackGetter();
-};
-
-export {
-  Attempt_Either_getOrElse,
-};

src/library/Attempt/Either/getOrThrow.ts
@@ -1,30 +1,0 @@
-import {
-  Cause,
-  Effect,
-  Exit,
-} from 'effect';
-
-import type {
-  Attempt_Error,
-} from '../Error';
-
-const Attempt_Either_getOrThrow = <
-  SomeProduct,
-  SomeActionableError extends Attempt_Error.Actionable,
->(
-  givenExit: Exit.Exit<SomeProduct, SomeActionableError>,
-): SomeProduct => {
-  if (
-    Exit.isSuccess(givenExit)
-  ) return givenExit.value;
-
-  if (
-    !Cause.isFailType(givenExit.cause)
-  ) return Effect.runSync(Effect.dieMessage(`Expected failure, got "${givenExit.cause._tag}" instead`));
-
-  return givenExit.cause.error.throwAnyway('Expected product, got failure instead');
-};
-
-export {
-  Attempt_Either_getOrThrow,
-};

src/library/Attempt/Either/index.ts
@@ -1,1 +1,0 @@
-export * from './exports.object.primary.ts';

commit 6ac8318e7b900b7e9d04f01d6ae0c70cbf340ebd

fix: prevents `effect` from wrapping errors thrown from `ShimmedStabilityAIClient` (#1177)
- this shim must simulate the actual StabilityAIClient, which doesn't wrap thrown errors in any way.

Regression introduced in #1171

src/library/ShimmedStabilityAIClient/Version1/Image.ts
@@ -1,5 +1,6 @@
 import {
   Effect,
+  Either,
 } from 'effect';
 
 import type {
@@ -30,7 +31,14 @@ class ShimmedStabilityAIClient_Version1_Image
     const textToImageSDXLShortfinClient = new Shortfin.TextToImage.SDXL.Client(this.origin);
 
     const exitFromGeneratingImage = await textToImageSDXLShortfinClient.generateImageFrom(derivedBatchedRequestBody);
-    const generatedImage = Effect.runSync(exitFromGeneratingImage); // matches error propagation of actual StabilityAI Client
+    const resultOfGeneratingImage = Effect.runSync(Effect.either(exitFromGeneratingImage));
+
+    const generatedImage = Either.getOrThrowWith(
+      resultOfGeneratingImage,
+      (someFailure) => {
+        return someFailure.throwAnyway('matches error propagation of actual StabilityAI Client');
+      },
+    );
 
     const soleGeneratedArtifact: StabilityAI_TextToImage_Pipeline_Output = {
       base64      : generatedImage.toString(),