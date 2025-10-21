9e789a6e23b70da4c4a60b67fa4c882f9cfefcdc
Jacob Gordon <jacob.gordon@amd.com>
2025-10-21 08:24:25 -0500

    refactor: decouples mapping of successes and failures (#1211)
    - using the `yield` syntax, transforming the success payload need not be done within a `.map*` function
    - these changes decouple these from failure transformations ahead of time to prep for this syntax
    
    Facilitates #1203

--- src/features/TextToImage/Config/Dynamic/fetch.ts
+++ src/features/TextToImage/Config/Dynamic/fetch.ts
@@ -21,17 +21,18 @@ import {
 const TextToImage_Config_Dynamic_fetch = (): Promise<
   TextToImage_Config_Dynamic_Fetching.Exit
 > => Effect.runPromise(Effect.promise(async () => {
-  const exitFromFetchingRawConfigFromEndpoint = await HTTP.Client.local.fetchResource({
+  const exitFromFetchingRawConfigFromEndpoint = (await HTTP.Client.local.fetchResource({
     from: TextToImage_Config_Dynamic_endpoint,
-  });
-
-  const exitFromDecodingConfigFromEndpoint = Exit.mapBoth(exitFromFetchingRawConfigFromEndpoint, {
-    onSuccess: $0 => Schema.decodeUnknownSync(TextToImage_Config)($0),
-    onFailure: $0 => new TextToImage_Config_Dynamic_Fetching.Error({
+  })).pipe(
+    Exit.mapError($0 => new TextToImage_Config_Dynamic_Fetching.Error({
       endpoint: TextToImage_Config_Dynamic_endpoint,
       cause   : $0,
-    }),
-  });
+    })),
+  );
+
+  const exitFromDecodingConfigFromEndpoint = exitFromFetchingRawConfigFromEndpoint.pipe(
+    Exit.map($0 => Schema.decodeUnknownSync(TextToImage_Config)($0)),
+  );
 
   return exitFromDecodingConfigFromEndpoint;
 }));
--- src/features/TextToImage/Config/Static/read.ts
+++ src/features/TextToImage/Config/Static/read.ts
@@ -21,17 +21,18 @@ import {
 const TextToImage_Config_Static_read = (): Promise<
   TextToImage_Config_Static_Reading.Exit
 > => Effect.runPromise(Effect.promise(async () => {
-  const exitFromFetchingRawConfigFromFile = await HTTP.Client.local.fetchResource({
+  const exitFromFetchingRawConfigFromFile = (await HTTP.Client.local.fetchResource({
     from: TextToImage_Config_Static_file,
-  });
-
-  const exitFromDecodingConfigFromFile = Exit.mapBoth(exitFromFetchingRawConfigFromFile, {
-    onSuccess: $0 => Schema.decodeUnknownSync(TextToImage_Config)($0),
-    onFailure: $0 => new TextToImage_Config_Static_Reading.Error({
-      cause   : $0,
+  })).pipe(
+    Exit.mapError($0 => new TextToImage_Config_Static_Reading.Error({
       filePath: TextToImage_Config_Static_file,
-    }),
-  });
+      cause   : $0,
+    })),
+  );
+
+  const exitFromDecodingConfigFromFile = exitFromFetchingRawConfigFromFile.pipe(
+    Exit.map($0 => Schema.decodeUnknownSync(TextToImage_Config)($0)),
+  );
 
   return exitFromDecodingConfigFromFile;
 }));
