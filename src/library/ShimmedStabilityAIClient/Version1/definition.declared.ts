9a408a01379cd47963e2eb4f3909da05c2349484
Jacob Gordon <jacob.gordon@amd.com>
2025-10-21 21:02:12 +0000

    refactor: removes use of `HTTP` from `ShimmedStabilityAIClient`

--- src/library/ShimmedStabilityAIClient/Version1/definition.declared.ts
+++ src/library/ShimmedStabilityAIClient/Version1/definition.declared.ts
@@ -1,15 +1,17 @@
-import HTTP from '@/library/HTTP';
+import {
+  ClientSDK,
+} from 'stabilityai-client-typescript/lib/sdks';
 
 import {
   ShimmedStabilityAIClient_Version1_Image,
 } from './Image';
 
 class ShimmedStabilityAIClient_Version1
-  extends HTTP.Client {
+  extends ClientSDK {
   private cachedClient?: ShimmedStabilityAIClient_Version1_Image;
 
   public get image(): ShimmedStabilityAIClient_Version1_Image {
-    this.cachedClient ??= new ShimmedStabilityAIClient_Version1_Image(this.origin);
+    this.cachedClient ??= new ShimmedStabilityAIClient_Version1_Image(this._options);
     return this.cachedClient;
   }
 }
--- src/library/ShimmedStabilityAIClient/definition.declared.ts
+++ src/library/ShimmedStabilityAIClient/definition.declared.ts
@@ -1,26 +1,17 @@
-import HTTP from '@/library/HTTP';
-import URLComponent from '@/library/URLComponent';
+import {
+  ClientSDK,
+} from 'stabilityai-client-typescript/lib/sdks';
 
 import {
   ShimmedStabilityAIClient_Version1,
 } from './Version1';
 
 class ShimmedStabilityAIClient
-  extends HTTP.Client {
-  public constructor(given: {
-    serverURL: string;
-  }) {
-    const serverOrigin = URLComponent.Origin(given.serverURL);
-
-    super(
-      serverOrigin,
-    );
-  }
-
+  extends ClientSDK {
   private cachedClient?: ShimmedStabilityAIClient_Version1;
 
   public get version1(): ShimmedStabilityAIClient_Version1 {
-    this.cachedClient ??= new ShimmedStabilityAIClient_Version1(this.origin);
+    this.cachedClient ??= new ShimmedStabilityAIClient_Version1(this._options);
     return this.cachedClient;
   }
 }
