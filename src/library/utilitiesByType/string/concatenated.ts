3c1a4280121e8c024fc76437ad594c854b20c513
Jacob Gordon <jacob.gordon@amd.com>
2025-10-22 16:52:23 +0000

    checkpoint: adds `Option<string>` support to `concatenated`

--- src/library/utilitiesByType/string/concatenated.ts
+++ src/library/utilitiesByType/string/concatenated.ts
@@ -1,15 +1,22 @@
 import {
   Array,
+  Option,
   pipe,
 } from 'effect';
 
+import {
+  isString,
+} from 'effect/Predicate';
+
 const concatenated = (
   ...givenOperands: (
-    | string
+    | /*         */ string
+    | Option.Option<string>
   )[]
 ): string => pipe(
   givenOperands,
-  Array.map($0 => $0),
+  Array.map($0 => isString($0) ? Option.some($0) : $0),
+  Array.getSomes,
   Array.join(''),
 );
 
