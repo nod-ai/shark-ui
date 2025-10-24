36e3f12d68171204b11db98bcccf77a4f3c49a4c
Jacob Gordon <jacob.gordon@amd.com>
2025-10-24 22:00:19 +0000

    checkpoint: splits `Effect.orDieWith` into `Effect.orElseFail` and `Effect.orDie

--- src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
+++ src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
@@ -29,16 +29,16 @@ const toSharkUIOutput_plural = (
   ) return yield* Effect.fail(new Error('Expected response body rather than readable stream')).pipe(Effect.orDie);
 
   const inferredRawImages = yield* Option.fromNullable(givenResponse.result.artifacts).pipe(
-    Effect.orDieWith(() => new Error('Expected text-to-image output in response result')),
-  );
+    Effect.orElseFail(() => new Error('Expected text-to-image output in response result')),
+  ).pipe(Effect.orDie);
 
   const potentialOutputImages = inferredRawImages.map(($0) => toSharkUIOutput_Image($0, {
     description: toSharkUIOutput_Image.Description.all(givenInputText),
   }));
 
   const inferredOutputImages = yield* Effect.all(potentialOutputImages).pipe(
-    Effect.orDieWith(() => new Error('Failed to convert one or more raw images to Shark UI output image.')),
-  );
+    Effect.orElseFail(() => new Error('Failed to convert one or more raw images to Shark UI output image.')),
+  ).pipe(Effect.orDie);
 
   const inferredOutputs = inferredOutputImages.map(($0) => new TextToImage_Pipeline.Output({
     image: $0,
