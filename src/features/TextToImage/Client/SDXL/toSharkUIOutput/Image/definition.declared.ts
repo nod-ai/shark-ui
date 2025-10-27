883b5bf014b41e08e4052a72ddecfd84f71589fe
Jacob Gordon <jacob.gordon@amd.com>
2025-10-27 14:35:45 -0500

    refactor(text-to-image): ensures image conversion errors originate from failure points (#1260)
    Pre-requisite for #1246

--- src/features/TextToImage/Client/SDXL/toSharkUIOutput/Image/definition.declared.ts
+++ src/features/TextToImage/Client/SDXL/toSharkUIOutput/Image/definition.declared.ts
@@ -17,17 +17,20 @@ function toSharkUIOutput_Image(
     description: TextToImage_Pipeline.Output['image']['description'];
   },
 ): Option.Option<TextToImage_Pipeline.Output['image']> {
-  return Option.gen(function* () {
-    const rawBase64Data = yield* Option.fromNullable(givenImage.base64);
-    const base64DataOfRawImage = Sequence.Byte.Encoded.Base64(rawBase64Data);
+  const rawBase64Data = Option.fromNullable(givenImage.base64).pipe(
+    Option.getOrThrowWith(() => new Error('Data for Stability AI image was not present.')),
+  );
 
-    const derivedImage = {
-      uri        : new URI.Image('png', 'base64', base64DataOfRawImage),
-      description: given.description,
-    };
+  const base64DataOfRawImage = Sequence.Byte.Encoded.Base64.option(rawBase64Data).pipe(
+    Option.getOrThrowWith(() => new Error('Data for Stability AI image was not base64-encoded.')),
+  );
 
-    return derivedImage;
-  });
+  const derivedImage = {
+    uri        : new URI.Image('png', 'base64', base64DataOfRawImage),
+    description: given.description,
+  };
+
+  return Option.some(derivedImage);
 }
 
 export {
