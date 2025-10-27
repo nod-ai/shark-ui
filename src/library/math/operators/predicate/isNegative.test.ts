9e80294095d9097687c68a3b41f31b59fb194489
Jacob Gordon <jacob.gordon@amd.com>
2025-10-27 21:56:39 +0000

    checkpoint: switches from 'vitest' to '@effect/vitest'

--- src/library/math/operators/predicate/isNegative.test.ts
+++ src/library/math/operators/predicate/isNegative.test.ts
@@ -1,12 +1,14 @@
+/* eslint-disable vitest/prefer-importing-vitest-globals */
+/* eslint-disable vitest/require-hook */
+import {
+  describe,
+  expect,
+  it,
+} from '@effect/vitest';
+
 import {
   Effect,
 } from 'effect';
-
-import {
-  describe,
-  it,
-  expect,
-} from 'vitest';
 
 import {
   numberTaxonomy,
@@ -35,29 +37,29 @@ describe(isNegative, () => {
 
   describe('the sad outcomes', () => {
     describe('when generated due to inoperable operands', () => {
-      it('should reject the operand', () => {
+      it.effect('should reject the operand', () => Effect.gen(function* () {
         expect.assertions(1);
 
         const inoperableInputDidResultInFailure = Effect.isFailure(isNegative(soleInoperableNumber)).pipe(Effect.runSync);
 
         expect(inoperableInputDidResultInFailure).toBe(true);
-      });
+      }));
 
-      it('should safely propagate the rejection', () => {
+      it.effect('should safely propagate the rejection', () => Effect.gen(function* () {
         expect.assertions(1);
 
         const soleFailure = Effect.flip(isNegative(soleInoperableNumber)).pipe(Effect.runSync);
 
         expect(soleFailure).toBeInstanceOf(Error);
-      });
+      }));
 
-      it('should clearly communicate the rejection to developers', () => {
+      it.effect('should clearly communicate the rejection to developers', () => Effect.gen(function* () {
         expect.assertions(1);
 
         const soleFailure = Effect.flip(isNegative(soleInoperableNumber)).pipe(Effect.runSync);
 
         expect(soleFailure.message).toBe('Operand must be operable');
-      });
+      }));
     });
   });
 
@@ -93,44 +95,44 @@ describe(isNegative, () => {
       ...positiveFiniteNumbers,
     ];
 
-    it.each(operableNumbers)('should accept valid operands', (eachOperableNumber) => {
+    it.effect.each(operableNumbers)('should accept valid operands', (eachOperableNumber) => Effect.gen(function* () {
       expect.assertions(1);
 
       const eachOperationDidSucceed = Effect.isSuccess(isNegative(eachOperableNumber)).pipe(Effect.runSync);
 
       expect(eachOperationDidSucceed).toBe(true);
-    });
+    }));
 
-    it.each(infiniteAssertions)('should support infinite operands', (eachInfiniteNumber, eachExpectedOutput) => {
+    it.effect.each(infiniteAssertions)('should support infinite operands', ([eachInfiniteNumber, eachExpectedOutput]) => Effect.gen(function* () {
       expect.assertions(1);
 
       const eachActualOutput = isNegative(eachInfiniteNumber).pipe(Effect.runSync);
 
       expect(eachActualOutput).toBe(eachExpectedOutput);
-    });
+    }));
 
-    it.each(neutralFiniteNumbers)('should detect neutral operands', (eachNeutralNumber) => {
+    it.effect.each(neutralFiniteNumbers)('should detect neutral operands', (eachNeutralNumber) => Effect.gen(function* () {
       expect.assertions(1);
 
       const outputForEachNeutralNumber = isNegative(eachNeutralNumber).pipe(Effect.runSync);
 
       expect(outputForEachNeutralNumber).toBe(false);
-    });
+    }));
 
-    it.each(positiveFiniteNumbers)('should detect positive operands', (eachPositiveNumber) => {
+    it.effect.each(positiveFiniteNumbers)('should detect positive operands', (eachPositiveNumber) => Effect.gen(function* () {
       expect.assertions(1);
 
       const outputForEachPositiveNumber = isNegative(eachPositiveNumber).pipe(Effect.runSync);
 
       expect(outputForEachPositiveNumber).toBe(false);
-    });
+    }));
 
-    it.each(negativeFiniteNumbers)('should detect negative operands', (eachNegativeNumber) => {
+    it.effect.each(negativeFiniteNumbers)('should detect negative operands', (eachNegativeNumber) => Effect.gen(function* () {
       expect.assertions(1);
 
       const outputForEachNegativeNumber = isNegative(eachNegativeNumber).pipe(Effect.runSync);
 
       expect(outputForEachNegativeNumber).toBe(true);
-    });
+    }));
   });
 });
