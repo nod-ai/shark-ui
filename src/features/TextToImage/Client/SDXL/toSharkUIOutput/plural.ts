3392854cb33840f87a5be16babffc0f98178648b
Jacob Gordon <jacob.gordon@amd.com>
2025-10-27 12:02:19 -0500

    refactor(text-to-image): moves "failed to convert" error closer to source of failure (#1257)
    Facilitates #1258

--- src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
+++ src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
@@ -36,11 +36,12 @@ const toSharkUIOutput_plural = (
     .map(($0) => toSharkUIOutput_Image($0, {
       description: toSharkUIOutput_Image.Description.all(givenInputText),
     }))
-    .map(($0) => TextToImage_Pipeline.Output.Option.from($0));
+    .map(($0) => TextToImage_Pipeline.Output.Option.from($0))
+    .map(
+      Option.getOrThrowWith(() => new Error('Failed to convert one or more raw images to Shark UI output')),
+    );
 
-  return Option.all(inferredOutputs).pipe(
-    Option.getOrThrowWith(() => new Error('Expected at least one well-formed text-to-image output in response')),
-  );
+  return inferredOutputs;
 };
 
 export {
