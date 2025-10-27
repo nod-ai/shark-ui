e2b3c3ee95b65197ff5f6deda552ab18f413970b
Jacob Gordon <jacob.gordon@amd.com>
2025-10-27 15:25:57 +0000

    checkpoint: leverages `Option.all` in `toSharkUIOutput_plural`

--- src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
+++ src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
@@ -36,7 +36,7 @@ const toSharkUIOutput_plural = (
     description: toSharkUIOutput_Image.Description.all(givenInputText),
   }));
 
-  const inferredOutputImages = potentialOutputImages.map(
+  const inferredOutputImages = Option.all(potentialOutputImages).pipe(
     Option.getOrThrowWith(() => new Error('Failed to convert one or more raw images to Shark UI output image.')),
   );
 
