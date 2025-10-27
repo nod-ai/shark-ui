55fd435ac0080cce2f8fe4deb3783c03da082635
Jacob Gordon <jacob.gordon@amd.com>
2025-10-27 18:44:40 +0000

    checkpoint: transfers `Option.some` from `toSharkUIOutput_Image` to `toSharkUIOutput_plural`

--- src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
+++ src/features/TextToImage/Client/SDXL/toSharkUIOutput/plural.ts
@@ -34,7 +34,8 @@ const toSharkUIOutput_plural = (
 
   const potentialOutputImages = inferredRawImages.map(($0) => toSharkUIOutput_Image($0, {
     description: toSharkUIOutput_Image.Description.all(givenInputText),
-  }));
+  }))
+    .map(Option.some);
 
   const inferredOutputImages = Option.all(potentialOutputImages).pipe(
     Option.getOrThrowWith(() => new Error('Failed to convert one or more raw images to Shark UI output image.')),
