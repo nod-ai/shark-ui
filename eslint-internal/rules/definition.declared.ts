caf30d93b23d63925005d9417f5b1b5cb73fa8bb
Jacob Gordon <Jacob.Gordon@amd.com>
2025-11-04 16:47:27 +0000

    checkpoint: adds "/eslint-internal" module

---
+++ eslint-internal/rules/definition.declared.ts
@@ -0,0 +1,11 @@
+import {
+  preferNewLinesAroundConditionOfBlocklessIfStatements,
+} from './preferNewLinesAroundConditionOfBlocklessIfStatements';
+
+const pluginInternal_rules = {
+  'prefer-new-lines-around-condition-of-blockless-if-statements': preferNewLinesAroundConditionOfBlocklessIfStatements,
+};
+
+export {
+  pluginInternal_rules,
+};
