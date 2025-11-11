f2b4c141b7592858fb28ccb6e3cbb5e24f753a71
Jacob Gordon <Jacob.Gordon@amd.com>
2025-11-11 17:35:55 +0000

    checkpoint: adds subcommand for generating test companions for a single unit

--- vitest/test-companion-cli/definition.declared.ts
+++ vitest/test-companion-cli/definition.declared.ts
@@ -2,10 +2,17 @@ import {
   Command,
 } from '@effect/cli';
 
+import {
+  TestCompanionCLI_generateForUnit,
+} from './generate-for-unit';
+
 const TestCompanionCLI = Command.make(
   'test-companion',
 ).pipe(
   Command.withDescription('Manage the companion test files for testable units.'),
+  Command.withSubcommands([
+    TestCompanionCLI_generateForUnit,
+  ]),
   Command.run({
     name   : 'Test Companion CLI',
     version: '0.0.0',
