9179f1e92a1d56184dfc78eb2a417a43cae40687
Jacob Gordon <jacob.gordon@amd.com>
2025-11-11 11:23:31 -0600

    linter: enforces new lines between named imports (#1325)
    - makes changes to named imports are easier to combine during merge conflicts

--- eslint-internal/rules/definition.declared.ts
+++ eslint-internal/rules/definition.declared.ts
@@ -2,8 +2,13 @@ import {
   preferNewLinesAroundConditionOfBlocklessIfStatements,
 } from './preferNewLinesAroundConditionOfBlocklessIfStatements';
 
+import {
+  preferNewLinesBetweenNamedImports,
+} from './preferNewLinesBetweenNamedImports';
+
 const pluginInternal_rules = {
   'prefer-new-lines-around-condition-of-blockless-if-statements': preferNewLinesAroundConditionOfBlocklessIfStatements,
+  'prefer-new-lines-between-named-imports'                      : preferNewLinesBetweenNamedImports,
 };
 
 export {
