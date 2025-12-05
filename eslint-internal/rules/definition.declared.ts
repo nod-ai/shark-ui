380b119e43d748a3497ede2b1f1dcb835de73aff
Jacob Gordon <Jacob.Gordon@amd.com>
2025-12-05 23:03:51 +0000

    linter(internal): defines rule for requiring extension for ts modules

--- eslint-internal/rules/definition.declared.ts
+++ eslint-internal/rules/definition.declared.ts
@@ -6,9 +6,14 @@ import {
   preferNewLinesBetweenNamedImports,
 } from './preferNewLinesBetweenNamedImports';
 
+import {
+  requireExtensionForTSModules,
+} from './requireExtensionForTSModules';
+
 const pluginInternal_rules = {
   'prefer-new-lines-around-condition-of-blockless-if-statements': preferNewLinesAroundConditionOfBlocklessIfStatements,
   'prefer-new-lines-between-named-imports'                      : preferNewLinesBetweenNamedImports,
+  'require-extension-for-ts-modules'                            : requireExtensionForTSModules,
 };
 
 export {
