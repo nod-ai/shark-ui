nod-ai/shark-ui

commit f442790463b1ca938a9ec7d596bc28e764176528

Migration to AMDShark (#1362)
Signed-off-by: pdhirajkumarprasad <dhirajp@amd.com>

eslint.config.ts
@@ -30,7 +30,7 @@ import pluginStylistic from './eslint.stylistic';
 import pluginVitest from './eslint.vitest';
 
 const extraConfigForESLint: ConfigWithExtends = {
-  name : 'shark-ui/eslint',
+  name : 'amdshark-ui/eslint',
   rules: {
     'curly': [
       'error',
@@ -71,7 +71,7 @@ const extraConfigForESLint: ConfigWithExtends = {
 };
 
 const extraConfigForTypeScriptESLint: ConfigWithExtends = {
-  name : 'shark-ui/@typescript-eslint',
+  name : 'amdshark-ui/@typescript-eslint',
   rules: {
     '@typescript-eslint/consistent-type-exports': [
       'error',
@@ -109,7 +109,7 @@ const extraConfigForTypeScriptESLint: ConfigWithExtends = {
 };
 
 const VueTSConfig_overridesForAugmentationsToModuleDefinitions: ConfigWithExtends = {
-  name : 'shark-ui/module-definition-augmentations',
+  name : 'amdshark-ui/module-definition-augmentations',
   files: [
     '**/definition.declared.augmentation.ts',
   ],

eslint.import.ts
@@ -5,7 +5,7 @@ import type {
 } from 'typescript-eslint';
 
 const extendedConfig: ConfigWithExtends = {
-  name : 'shark-ui/import',
+  name : 'amdshark-ui/import',
   files: [
     '**/*.{ts,vue}',
   ],

eslint.internal.ts
@@ -6,7 +6,7 @@ import eslintInternal from './eslint-internal';
 
 const pluginInternal: ConfigWithExtends[] = [
   {
-    name   : 'shark-ui/internal',
+    name   : 'amdshark-ui/internal',
     plugins: {
       internal: eslintInternal,
     },

eslint.jsonc.ts
@@ -5,7 +5,7 @@ import type {
 import pluginJsonc from 'eslint-plugin-jsonc';
 
 const rulesForPackageJSON: Linter.Config = {
-  name : 'shark-ui/jsonc/package-json',
+  name : 'amdshark-ui/jsonc/package-json',
   files: [
     '**/package.json',
   ],
@@ -40,7 +40,7 @@ const jsonPatterns = [
 const pluginJSON: Linter.Config[] = [
   ...pluginJsonc.configs['flat/recommended-with-json'],
   {
-    name   : 'shark-ui/jsonc/ignore-generated',
+    name   : 'amdshark-ui/jsonc/ignore-generated',
     ignores: [
       '**/package-lock.json',
       '**/coverage/**',
@@ -48,7 +48,7 @@ const pluginJSON: Linter.Config[] = [
     ],
   },
   {
-    name : 'shark-ui/jsonc/rules',
+    name : 'amdshark-ui/jsonc/rules',
     files: jsonPatterns,
     rules: {
       'jsonc/array-bracket-newline': [

eslint.stylistic.ts
@@ -5,7 +5,7 @@ import type {
 } from 'typescript-eslint';
 
 const extraConfig: ConfigWithExtends = {
-  name   : 'shark-ui/extra-stylistic',
+  name   : 'amdshark-ui/extra-stylistic',
   plugins: {
     '@stylistic': stylistic,
   },

src/features/Reporting/promptUserWith.ts
@@ -30,9 +30,9 @@ const Reporting_promptUserWith = (givenIssue: unknown): void => {
     !userDidPermitDraftingNewIssue
   ) return;
 
-  const SharkUIRepository = new GitHub.Repository('nod-ai', 'shark-ui');
+  const AMDSharkUIRepository = new GitHub.Repository('nod-ai', 'amdshark-ui');
 
-  const newIssue = SharkUIRepository.Issue.from({
+  const newIssue = AMDSharkUIRepository.Issue.from({
     title : `[Unexpected Error]: can't <some task> when <some context>`,
     body  : `### Details\n${formattedErrorDetails}`.replaceAll('\n', '\n> '),
     labels: [

index.html
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/logo/favicon-16x16.png">
     <link rel="apple-touch-icon" sizes="180x180" href="/logo/apple-touch-icon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SHARK UI</title>
+    <title>AMD-SHARK UI</title>
   </head>
   <body>
     <div id="app"></div>

commit 06a718e851c21b20ed8da458bba900fce449af18

fix: corrects kebab-case repo names (#1392)
- a hyphen was missing, which broke many hyperlinks

Introduced in #1362

eslint.config.ts
@@ -30,7 +30,7 @@ import pluginStylistic from './eslint.stylistic';
 import pluginVitest from './eslint.vitest';
 
 const extraConfigForESLint: ConfigWithExtends = {
-  name : 'amdshark-ui/eslint',
+  name : 'amd-shark-ui/eslint',
   rules: {
     'curly': [
       'error',
@@ -71,7 +71,7 @@ const extraConfigForESLint: ConfigWithExtends = {
 };
 
 const extraConfigForTypeScriptESLint: ConfigWithExtends = {
-  name : 'amdshark-ui/@typescript-eslint',
+  name : 'amd-shark-ui/@typescript-eslint',
   rules: {
     '@typescript-eslint/consistent-type-exports': [
       'error',
@@ -109,7 +109,7 @@ const extraConfigForTypeScriptESLint: ConfigWithExtends = {
 };
 
 const VueTSConfig_overridesForAugmentationsToModuleDefinitions: ConfigWithExtends = {
-  name : 'amdshark-ui/module-definition-augmentations',
+  name : 'amd-shark-ui/module-definition-augmentations',
   files: [
     '**/definition.declared.augmentation.ts',
   ],

eslint.import.ts
@@ -5,7 +5,7 @@ import type {
 } from 'typescript-eslint';
 
 const extendedConfig: ConfigWithExtends = {
-  name : 'amdshark-ui/import',
+  name : 'amd-shark-ui/import',
   files: [
     '**/*.{ts,vue}',
   ],

eslint.internal.ts
@@ -6,7 +6,7 @@ import eslintInternal from './eslint-internal';
 
 const pluginInternal: ConfigWithExtends[] = [
   {
-    name   : 'amdshark-ui/internal',
+    name   : 'amd-shark-ui/internal',
     plugins: {
       internal: eslintInternal,
     },

eslint.jsonc.ts
@@ -5,7 +5,7 @@ import type {
 import pluginJsonc from 'eslint-plugin-jsonc';
 
 const rulesForPackageJSON: Linter.Config = {
-  name : 'amdshark-ui/jsonc/package-json',
+  name : 'amd-shark-ui/jsonc/package-json',
   files: [
     '**/package.json',
   ],
@@ -40,7 +40,7 @@ const jsonPatterns = [
 const pluginJSON: Linter.Config[] = [
   ...pluginJsonc.configs['flat/recommended-with-json'],
   {
-    name   : 'amdshark-ui/jsonc/ignore-generated',
+    name   : 'amd-shark-ui/jsonc/ignore-generated',
     ignores: [
       '**/package-lock.json',
       '**/coverage/**',
@@ -48,7 +48,7 @@ const pluginJSON: Linter.Config[] = [
     ],
   },
   {
-    name : 'amdshark-ui/jsonc/rules',
+    name : 'amd-shark-ui/jsonc/rules',
     files: jsonPatterns,
     rules: {
       'jsonc/array-bracket-newline': [

eslint.stylistic.ts
@@ -5,7 +5,7 @@ import type {
 } from 'typescript-eslint';
 
 const extraConfig: ConfigWithExtends = {
-  name   : 'amdshark-ui/extra-stylistic',
+  name   : 'amd-shark-ui/extra-stylistic',
   plugins: {
     '@stylistic': stylistic,
   },

src/features/Reporting/promptUserWith.ts
@@ -30,7 +30,7 @@ const Reporting_promptUserWith = (givenIssue: unknown): void => {
     !userDidPermitDraftingNewIssue
   ) return;
 
-  const AMDSharkUIRepository = new GitHub.Repository('nod-ai', 'amdshark-ui');
+  const AMDSharkUIRepository = new GitHub.Repository('nod-ai', 'amd-shark-ui');
 
   const newIssue = AMDSharkUIRepository.Issue.from({
     title : `[Unexpected Error]: can't <some task> when <some context>`,