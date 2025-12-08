4243ba5c957600cde94d1a4911d55c1a7eb30d3d
Jacob Gordon <Jacob.Gordon@amd.com>
2025-12-08 16:16:35 +0000

    linter: enforces same-name shorthand for all v-binds

--- eslint.vue.ts
+++ eslint.vue.ts
@@ -6,6 +6,17 @@ import type {
 
 const pluginVue: InfiniteDepthConfigWithExtends[] = [
   uncustomizedPluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      'vue/v-bind-style': [
+        'error',
+        'shorthand',
+        {
+          sameNameShorthand: 'always', // Makes it easier to see when a prop is just being passed through to a child component
+        },
+      ],
+    },
+  },
 ];
 
 export {
