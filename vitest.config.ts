d012fa96c50c092d37d79829a67d583fd8e7293f
Jacob Gordon <jacob.gordon@amd.com>
2025-12-09 14:36:20 -0600

    tests(unit): outputs coverage summary (#1415)
    - the coverage summary can indicate which units have no coverage at all
    - from there, the list can be narrowed down to those that don't even have a companion test file

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ import {
   mergeConfig,
   defineConfig,
   configDefaults,
+  coverageConfigDefaults,
 } from 'vitest/config';
 
 import viteConfig from './vite.config';
@@ -19,7 +20,26 @@ const vitestConfig = mergeConfig(
         ...configDefaults.exclude,
         'e2e/**',
       ],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+      root    : fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        enabled: true,
+        include: [
+          'src/library/**/*.ts',
+          'src/features/**/*.ts',
+        ],
+        exclude: [
+          ...coverageConfigDefaults.exclude,
+          'src/**/index.ts', // barrel files as entry points
+          'src/**/exports.*.ts', // barrel files as export control
+          'src/**/definition.assembled*.ts', // barrel files as object assemblers
+          'src/**/definition.declared.augmentation.ts', // side-effect files for module augmentation
+          'src/**/definition.declared.withAugmentation.ts', // barrel files as object augmenters
+          'src/**/external.ts', // facades for external libraries
+        ],
+        reporter: [
+          'json-summary', // "json" to make it machine readable, "summary" for a schema that's easier to parse
+        ],
+      },
     },
   }),
 );
