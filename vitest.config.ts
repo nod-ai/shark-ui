19fc87ca68763ad8b531985b9c3e88e0149cccb1
Jacob Gordon <Jacob.Gordon@amd.com>
2025-12-09 20:16:27 +0000

    checkpoint: narrows coverage by excluding non-units

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ import {
   mergeConfig,
   defineConfig,
   configDefaults,
+  coverageConfigDefaults,
 } from 'vitest/config';
 
 import viteConfig from './vite.config';
@@ -25,6 +26,15 @@ const vitestConfig = mergeConfig(
         include: [
           'src/library/**/*.ts',
           'src/features/**/*.ts',
+        ],
+        exclude: [
+          ...coverageConfigDefaults.exclude,
+          'src/**/index.ts', // barrel files as entry points
+          'src/**/exports.*.ts', // barrel files as export control
+          'src/**/definition.assembled*.ts', // barrel files as object assemblers
+          'src/**/definition.declared.augmentation.ts', // side-effect files for module augmentation
+          'src/**/definition.declared.withAugmentation.ts', // barrel files as object augmenters
+          'src/**/external.ts', // facades for external libraries
         ],
       },
     },
